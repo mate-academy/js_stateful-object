1becee7541834b6925c3830a311c57516344c044
Sosibona <empty.tara@gmail.com>
2019-10-01 11:40:53 +0300

    fix

--- src/transformState.js
+++ src/transformState.js
@@ -49,18 +49,18 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  for (let i = 0; i < transforms.length; i++) {
-    if (transforms[i].operation === 'addProperties') {
-      for (const key in transforms[i].properties) {
-        state[key] = transforms[i].properties[key];
+  for (const i of transforms) {
+    if (i.operation === 'addProperties') {
+      for (const key in i.properties) {
+        state[key] = i.properties[key];
       }
     }
-    if (transforms[i].operation === 'removeProperties') {
-      for (let j = 0; j < transforms[i].properties.length; j++) {
-        delete state[transforms[i].properties[j]];
+    if (i.operation === 'removeProperties') {
+      for (let j = 0; j < i.properties.length; j++) {
+        delete state[i.properties[j]];
       }
     }
-    if (transforms[i].operation === 'clear') {
+    if (i.operation === 'clear') {
       for (const key in state) {
         delete state[key];
       }
