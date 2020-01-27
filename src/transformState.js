b208c0348e85b88fe5006055d82bf17ad3ba3034
ded-git <sakarailko@gmail.com>
2020-01-27 15:34:46 +0200

    hardy...

--- src/transformState.js
+++ src/transformState.js
@@ -46,7 +46,31 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  // write code here
+  for (let i = 0; i < transforms.length; i++) {
+    if (transforms[i]['operation'] === 'addProperties') {
+      for (const key in transforms[i]['properties']) {
+        state[key] = transforms[i]['properties'][key];
+      }
+    }
+
+    if (transforms[i]['operation'] === 'removeProperties') {
+      for (const key in transforms[i]['properties']) {
+        for (const prop in state) {
+          if (transforms[i]['properties'][key] === prop) {
+            delete state[prop];
+          }
+        }
+      }
+    }
+
+    if (transforms[i]['operation'] === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
