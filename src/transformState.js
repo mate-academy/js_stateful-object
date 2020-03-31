81a2b088a82d25fc153afb6f8d72dc7dcff33582
Yarik1993 <makarov1603@icloud.com>
2020-03-31 13:11:52 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -47,6 +47,26 @@
  */
 function transformState(state, transforms) {
   // write code here
+  for (let i = 0; i < transforms.length; i++) {
+    switch (transforms[i].operation) {
+      case 'addProperties':
+        for (const key in transforms[i].properties) {
+          state[key] = transforms[i].properties[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key in transforms[i].properties) {
+          delete state[transforms[i].properties[key]];
+        }
+        break;
+      case 'clear':
+        // eslint-disable-next-line no-param-reassign
+        state = {};
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
