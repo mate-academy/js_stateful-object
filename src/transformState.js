ac10569e5af00738e7c2faaf1a5d663d98ed3fce
Nastia-rosy <nastiiarosy@gmail.com>
2020-04-01 13:22:19 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -47,34 +47,6 @@
  */
 function transformState(state, transforms) {
   // write code here
-  for (let i = 0; i < transforms.length; i++) {
-    const operation = transforms[i].operation;
-    const property = transforms[i].properties;
-
-    switch (operation) {
-      case 'addProperties':
-        for (const key in property) {
-          state[key] = property[key];
-        }
-        break;
-
-      case 'removeProperties':
-        for (const key in property) {
-          if (state.hasOwnProperty(property[key])) {
-            delete state[property[key]];
-          }
-        }
-        break;
-
-      case 'clear':
-        for (const key in state) {
-          delete state[key];
-        }
-        break;
-    }
-  }
-
-  return state;
 }
 
 module.exports = transformState;
