226527f59ed9403183f83c956f7101edf620898e
DimaDP <dimadp95@gmail.com>
2020-05-27 13:28:24 +0300

    replace__if_to_switch

--- src/transformState.js
+++ src/transformState.js
@@ -48,24 +48,26 @@
 function transformState(state, transforms) {
   // write code here
   for (let i = 0; i < transforms.length; i++) {
-    if (transforms[i].operation === 'addProperties') {
-      for (const key in transforms[i].properties) {
-        state[key] = transforms[i].properties[key];
-      }
-    }
 
-    if (transforms[i].operation === 'removeProperties') {
-      for (const key in transforms[i].properties) {
-        delete state[transforms[i].properties[key]];
-      }
-    }
+    switch (transforms[i].operation) {
+      case 'addProperties':
+        for (const key in transforms[i].properties) {
+          state[key] = transforms[i].properties[key];
+        }
+        break;
 
-    if (transforms[i].operation === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'removeProperties':
+        for (const key in transforms[i].properties) {
+          delete state[transforms[i].properties[key]];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
-
 module.exports = transformState;
