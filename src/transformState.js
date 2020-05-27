32c367c30b0be0094fde9700f676e00ba45f52fa
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-05-27 22:36:46 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -46,7 +46,29 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  // write code here
+  for (const transform of transforms) {
+    const operation = transform.operation;
+
+    switch (operation) {
+      case 'addProperties':
+        for (const key in transform.properties) {
+          state[key] = transform.properties[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of transform.properties) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
