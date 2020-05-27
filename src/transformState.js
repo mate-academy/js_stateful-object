ca93288a5df4fc6cb0342f962369125c6843fccf
Glib Rulev <velzepooz@gmail.com>
2020-05-27 12:52:09 +0300

    Fixed

--- src/transformState.js
+++ src/transformState.js
@@ -47,18 +47,22 @@
  */
 function transformState(state, transforms) {
   for (let i = 0; i < transforms.length; i++) {
-    if (transforms[i].operation === 'addProperties') {
-      for (const key in transforms[i].properties) {
-        state[key] = transforms[i].properties[key];
-      }
-    } else if (transforms[i].operation === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      };
-    } else if (transforms[i].operation === 'removeProperties') {
-      for (const key in transforms[i].properties) {
-        delete state[transforms[i].properties[key]];
-      };
+    switch (transforms[i].operation) {
+      case 'addProperties':
+        for (const key in transforms[i].properties) {
+          state[key] = transforms[i].properties[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+      case 'removeProperties':
+        for (const key in transforms[i].properties) {
+          delete state[transforms[i].properties[key]];
+        };
+        break;
     }
   }
 
