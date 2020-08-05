728583d9f79d5f51ec3457a27ebfc530589b33d8
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-08-05 14:12:52 +0300

    with changes

--- src/transformState.js
+++ src/transformState.js
@@ -46,27 +46,25 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  for (let i = 0; i < transforms.length; i++) {
-    const step = transforms[i];
+  for (const transform of transforms) {
+    switch (transform.operation) {
+      case 'addProperties':
+        for (const key in transform.properties) {
+          state[key] = transform.properties[key];
+        }
+        break;
 
-    if (step.operation === 'addProperties') {
-      const prop = step.properties;
+      case 'removeProperties':
+        for (const property of transform.properties) {
+          delete state[property];
+        }
+        break;
 
-      for (const key in prop) {
-        state[key] = prop[key];
-      }
-    }
-
-    if (step.operation === 'removeProperties') {
-      for (let j = 0; j < step.properties.length; j++) {
-        delete state[step.properties[j]];
-      }
-    }
-
-    if (step.operation === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 
