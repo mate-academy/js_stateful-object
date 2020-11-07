eeca7ccefec66e86a10b70fbf407116d556da067
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2020-11-07 09:46:08 +0200

    Solved transformState

--- src/transformState.js
+++ src/transformState.js
@@ -63,6 +63,28 @@
  */
 function transformState(state, transforms) {
   // write code here
+
+  for (let i = 0; i < transforms.length; i++) {
+    if (transforms[i]['operation'] === 'addProperties') {
+      Object.assign(state, transforms[i]['properties']);
+    }
+
+    if (transforms[i]['operation'] === 'removeProperties') {
+      for (let j = 0; j < transforms[i]['properties'].length; j++) {
+        delete state[transforms[i]['properties'][j]];
+      }
+    }
+
+    if (transforms[i]['operation'] === 'clear') {
+      // const keysState = Object.keys(state)
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  // console.log(state)
+  return state;
 }
 
 module.exports = transformState;
