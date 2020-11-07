019ddf09e25ec666a35ed98a099cfc9db2d2696e
DominoSl <artemenko.mik@gmail.com>
2020-11-07 12:27:28 +0200

    MySolution

--- src/transformState.js
+++ src/transformState.js
@@ -50,9 +50,7 @@
  * Then after calling
  *
  * transformState(state, [
- *   { operation: 'addProperties', properties: { yet: 'another property' } }
  *   { operation: 'clear' },
- *   { operation: 'addProperties', properties: { foo: 'bar', name: 'Jim' } }
  * ])
  *
  * the `state` variable must contain
@@ -62,7 +60,34 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  // write code here
-}
+  // Going through the oprations to be performed
 
+  for (const element in transforms) {
+    // In case of 'clear' - deleting every property in state
+
+    if (transforms[element].operation === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    // In case of 'addProperties' - assigning the properties to the state
+
+    if (transforms[element].operation === 'addProperties') {
+      Object.assign(state, transforms[element].properties);
+    }
+
+    // In case of 'removeProperties'-looking for and deleting the matching keys
+
+    if (transforms[element].operation === 'removeProperties') {
+      for (const propertie in transforms[element].properties) {
+        for (const key in state) {
+          if (key === transforms[element].properties[propertie]) {
+            delete state[key];
+          }
+        }
+      }
+    }
+  }
+}
 module.exports = transformState;
