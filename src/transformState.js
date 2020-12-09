b3004f6993b09e4037f77f3c23ef1fa178588983
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-09 17:43:13 +0200

    completed

--- src/transformState.js
+++ src/transformState.js
@@ -62,7 +62,25 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  // write code here
+  for (let i = 0; i < transforms.length; i++) {
+    if (transforms[i]['operation'] === `addProperties`) {
+      Object.assign(state, transforms[i]['properties']);
+    }
+
+    if (transforms[i]['operation'] === `removeProperties`) {
+      for (const key of transforms[i]['properties']) {
+        if (state.hasOwnProperty(key)) {
+          delete state[key];
+        }
+      }
+    }
+
+    if (transforms[i]['operation'] === `clear`) {
+      for (const prop in state) {
+        delete state[prop];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
