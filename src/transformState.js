f94fab03af1c3562370c1cbc3662b46f70543ea7
Andron <blackdog337.ab@gmail.com>
2021-04-21 12:41:18 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,13 +4,13 @@
  * Implement a function accepting 2 arguments `state` and `transforms`. It
  * should change the `state` basing on the given `transforms`
  *
- * `state` is an object. You are supposed to add, change, or delete its
- *  properties instead of creating a new object
+ * `state` is an objectect. You are supposed to add, change, or delete its
+ *  properties instead of creating a new objectect
  *
- * `transforms` is an array of objects having the following properties:
+ * `transforms` is an array of objectects having the following properties:
  * `operation`: either `addProperties`, `removeProperties` or `clear`;
  * `properties`:
- *   - if `operation` is `addProperties`, this property contains an object
+ *   - if `operation` is `addProperties`, this property contains an objectect
  *   with `key: value` pairs to add to the state;
  *   - if `operation` is `removeProperties`, this property contains an array
  *   with the list of property names to remove from the state; (Not existing
@@ -40,7 +40,7 @@
  *   }
  * ])
  *
- * should modify the `state` object so after the call it becomes
+ * should modify the `state` objectect so after the call it becomes
  * {
  *   foo: 'bar',
  *   name: 'Jim',
@@ -58,11 +58,36 @@
  * the `state` variable must contain
  * { foo: 'bar', name: 'Jim' }
  *
- * @param {Object} state
- * @param {Object[]} transforms
+ * @param {objectect} state
+ * @param {objectect[]} transforms
  */
 function transformState(state, transforms) {
   // write code here
+  for (const object of transforms) {
+    // ADD
+
+    if (object.operation === 'addProperties') {
+      for (const key in object.properties) {
+        state[key] = object.properties[key];
+      };
+    };
+    // REMOVE
+
+    if (object.operation === 'removeProperties') {
+      for (const element of object.properties) {
+        if (state.hasOwnProperty(element)) {
+          delete state[element];
+        };
+      };
+    };
+    // CLEAR
+
+    if (object.operation === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      };
+    };
+  };
 }
 
 module.exports = transformState;
