0684a200c693c23f2dd3aee688b456e6df29ad23
victor <victorarytionowmail@gmail.com>
2021-04-22 11:55:00 +0300

    change if else to switch

--- src/transformState.js
+++ src/transformState.js
@@ -63,16 +63,20 @@
  */
 function transformState(state, transforms) {
   for (const transform of transforms) {
-    if (transform.operation === 'addProperties') {
-      Object.assign(state, transform.properties);
-    } else if (transform.operation === 'removeProperties') {
-      for (const key of transform.properties) {
-        delete state[key];
-      }
-    } else if (transform.operation === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (true) {
+      case transform.operation === 'addProperties':
+        Object.assign(state, transform.properties);
+        break;
+      case transform.operation === 'removeProperties':
+        for (const key of transform.properties) {
+          delete state[key];
+        }
+        break;
+      case transform.operation === 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
