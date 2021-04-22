0e9847172e383a40ec53068b8a6fa43f75993aaf
iryna-vys <vys.iryna@gmail.com>
2021-04-22 13:32:41 +0300

    Rewrited to switch

--- src/transformState.js
+++ src/transformState.js
@@ -62,25 +62,23 @@
  * @param {Object[]} transforms
  */
 function transformState(state, transforms) {
-  for (let i = 0; i < transforms.length; i++) {
-    const { operation, properties } = transforms[i];
+  for (const { operation, properties } of transforms) {
+    switch (operation) {
+      case 'addProperties':
+        Object.assign(state, properties);
+        break;
 
-    if (operation === 'addProperties') {
-      for (const key in properties) {
-        state[key] = properties[key];
-      }
-    }
+      case 'removeProperties':
+        for (const property of properties) {
+          delete state[property];
+        }
+        break;
 
-    if (operation === 'removeProperties') {
-      for (let n = 0; n < properties.length; n++) {
-        delete state[properties[n]];
-      }
-    }
-
-    if (operation === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
