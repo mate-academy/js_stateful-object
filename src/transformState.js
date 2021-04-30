f78d9080ae9e8ddc4f5c71e47bd34baba5153d2c
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-04-30 15:37:57 +0300

    Changes after review

--- src/transformState.js
+++ src/transformState.js
@@ -63,6 +63,25 @@
  */
 function transformState(state, transforms) {
   // write code here
+  for (const { operation, properties } of transforms) {
+    switch (operation) {
+      case 'addProperties':
+        Object.assign(state, properties);
+        break;
+
+      case 'removeProperties':
+        for (const remove of properties) {
+          delete state[remove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
