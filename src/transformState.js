7d8fd48f3790e20c8b55a32c23f7b920fddaf855
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-05-23 02:05:14 +0300

    solution v.1

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,35 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const i of actions) {
+    switch (true) {
+      case i['type'] === 'addProperties' :
+
+        /* It works, but linter isn't allow and some tests falls. Why? */
+
+        // state = {
+        //   ...state, ...i['extraData'],
+        // };
+
+        for (const key in i['extraData']) {
+          state[key] = i['extraData'][key];
+        }
+        break;
+
+      case i['type'] === 'removeProperties' :
+        for (const property of i['keysToRemove']) {
+          delete state[property];
+        }
+        break;
+
+      default :
+        for (const key in state) {
+          delete state[key];
+        }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
