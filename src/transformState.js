05323856198d76084a2a75f79295994539e83480
Sasha Pavlenko <380971520240@gmail.com>
2021-06-09 22:42:40 +0300

    The if,else statement was changed to switch,case

--- src/transformState.js
+++ src/transformState.js
@@ -6,16 +6,20 @@
  */
 function transformState(state, actions) {
   for (const key of actions) {
-    if (key.type === 'addProperties') {
-      Object.assign(state, key.extraData);
-    } else if (key.type === 'removeProperties') {
-      for (const keys of key.keysToRemove) {
-        delete state[keys];
-      }
-    } else if (key.type === 'clear') {
-      for (const k in state) {
-        delete state[k];
-      }
+    switch (key.type) {
+      case 'addProperties':
+        Object.assign(state, key.extraData);
+        break;
+      case 'removeProperties':
+        for (const keys of key.keysToRemove) {
+          delete state[keys];
+        }
+        break;
+      case 'clear':
+        for (const k in state) {
+          delete state[k];
+        }
+        break;
     }
   }
 }
