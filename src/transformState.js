4a666535b54d25c8652fee739e47d763a2231b12
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-06-17 16:26:51 +0300

    feat: completed tast stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const el of actions) {
+    if (el.type === 'addProperties') {
+      Object.assign(state, el.extraData);
+    }
+
+    if (el.type === 'removeProperties') {
+      const arr = [...el.keysToRemove];
+
+      for (const everyEl of arr) {
+        delete state[everyEl];
+      }
+    }
+
+    if (el.type === 'clear') {
+      Object.keys(state).forEach((item) => delete state[item]);
+    }
+  }
 }
 
 module.exports = transformState;
