984ad42125360eedfacd927c9726bfa54849a0e4
korzh <korzh.alexey@yahoo.com>
2021-06-17 22:28:16 +0300

    added switch

--- src/transformState.js
+++ src/transformState.js
@@ -7,24 +7,24 @@
 
 function transformState(state, actions) {
   actions.forEach(val => {
-    if (val.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      };
-    };
-
-    if (val.type === 'addProperties') {
-      Object.assign(state, val.extraData);
-    };
-
-    if (val.type === 'removeProperties') {
-      for (const key in state) {
-        val.keysToRemove.forEach(item => {
-          if (item === key) {
-            delete state[key];
-          };
-        });
-      };
+    switch (val.type) {
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+      case 'addProperties':
+        Object.assign(state, val.extraData);
+        break;
+      case 'removeProperties':
+        for (const key in state) {
+          val.keysToRemove.forEach(item => {
+            if (item === key) {
+              delete state[key];
+            };
+          });
+        };
+        break;
     };
   });
 
