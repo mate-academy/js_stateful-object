5f3562aca726ef4d97ae9ee61c01b69479156442
Roman <romakelemen@gmail.com>
2021-06-17 14:28:47 +0300

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const { type, extraData, keysToRemove } = actions[i];
+
+    switch (type) {
+      case 'addProperties': Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties': for (const key in keysToRemove) {
+        delete state[keysToRemove[key]];
+        continue;
+      }
+        break;
+
+      case 'clear': for (const property in state) {
+        delete state[property];
+        continue;
+      }
+        break;
+      default: break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
