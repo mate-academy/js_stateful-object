7efaa501c05151a3aca1488932308b8137c8d38a
John <jozaman96@gmail.com>
2021-06-17 21:05:30 +0300

    added switch statement

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,22 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      (actions[i].keysToRemove).map(key => {
-        delete state[key];
-      });
-    }
-
-    if (actions[i].type === 'clear') {
-      Object.keys(state).map(key => {
-        delete state[key];
-      });
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+      case 'removeProperties':
+        (actions[i].keysToRemove).map(key => {
+          delete state[key];
+        });
+        break;
+      case 'clear':
+        Object.keys(state).map(key => {
+          delete state[key];
+        });
+        break;
+      default:
+        break;
     }
   }
 }
