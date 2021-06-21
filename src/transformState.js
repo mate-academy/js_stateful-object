3010b885a5b24437a292f13d1f93f1cef27f1633
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-06-21 18:22:37 +0300

    solution on switch case

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,6 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    // if (actions[i].type === 'clear') {
-    //   for (const element in state) {
-    //     delete state[element];
-    //   }
-    // }
-
-    // if (actions[i].type === 'addProperties') {
-    //   Object.assign(state, actions[i].extraData);
-    // }
-
-    // if (actions[i].type === 'removeProperties') {
-    //   for (let index = 0; index < actions[i].keysToRemove.length; index++) {
-    //     delete state[actions[i].keysToRemove[index]];
-    //   }
-    // }
-
     switch (actions[i].type) {
       case 'clear':
         for (const element in state) {
