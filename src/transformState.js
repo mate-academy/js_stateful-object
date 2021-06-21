6cc71b79039f214255ab0a717d6e4f5e474e2584
David Dohru <tronsmartmy@gmail.com>
2021-06-21 13:13:03 +0300

    done

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,34 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
+
+function transformState(state, action) {
+  const obj = state;
+
+  for (let i = 0; i < action.length; i++) {
+    if (action[i].type === 'addProperties') {
+      for (const key in action[i].extraData) {
+        obj[key] = action[i].extraData[key];
+      }
+    }
+
+    if (action[i].type === 'removeProperties') {
+      for (let j = 0; j < action[i].keysToRemove.length; j++) {
+        for (let key in action[i].keysToRemove[j]) {
+          key = [key];
+          delete obj[action[i].keysToRemove[j]];
+        }
+      }
+    }
+
+    if (action[i].type === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      }
+    }
+  }
+
+  return obj;
 }
 
 module.exports = transformState;
