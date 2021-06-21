178003462b0d795de81d61de0cee3932bedb929d
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-06-21 18:21:47 +0300

    booth solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,43 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    // if (actions[i].type === 'clear') {
+    //   for (const element in state) {
+    //     delete state[element];
+    //   }
+    // }
+
+    // if (actions[i].type === 'addProperties') {
+    //   Object.assign(state, actions[i].extraData);
+    // }
+
+    // if (actions[i].type === 'removeProperties') {
+    //   for (let index = 0; index < actions[i].keysToRemove.length; index++) {
+    //     delete state[actions[i].keysToRemove[index]];
+    //   }
+    // }
+
+    switch (actions[i].type) {
+      case 'clear':
+        for (const element in state) {
+          delete state[element];
+        }
+        break;
+
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (let index = 0; index < actions[i].keysToRemove.length; index++) {
+          delete state[actions[i].keysToRemove[index]];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
