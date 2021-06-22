fab75c7f6a92b23293a8c8bab07ba8e1198fb784
Vitaliy Martynyuk <vitaliymartiniuk16@gmail.com>
2021-06-22 19:20:06 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    for (const firstLevelKey in actions[i]) {
+      if (actions[i][firstLevelKey] === 'clear') {
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+      }
+
+      if (firstLevelKey === 'extraData') {
+        for (const secondLevelKey in actions[i][firstLevelKey]) {
+          state[secondLevelKey] = actions[i][firstLevelKey][secondLevelKey];
+        }
+      }
+
+      if (firstLevelKey === 'keysToRemove') {
+        for (let j = 0; j < actions[i][firstLevelKey].length; j++) {
+          for (const stateKey in state) {
+            if (stateKey === actions[i][firstLevelKey][j]) {
+              delete state[stateKey];
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
