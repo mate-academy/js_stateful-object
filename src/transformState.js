4e18e5481051fbc2e0890526d1497834c0718fb2
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-06-22 14:41:29 +0300

    stateful obj version 1.0

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const char of actions) {
+    if (char.type === 'addProperties') {
+      Object.assign(state, char.extraData);
+    }
+
+    if (char.type === 'removeProperties') {
+      for (const key of char.keysToRemove) {
+        delete state[key];
+      }
+    }
+
+    if (char.type === 'clear') {
+      for (const key of Object.keys(state)) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
