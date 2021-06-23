74f78c2dd5713abb0ffbdf76c52ba2784d2a6535
Yaroslav <gso218695@gmail.com>
2021-06-23 19:19:59 +0300

    test

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,26 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
+function transformState(state, transforms) {
+  for (const transform of transforms) {
+    switch (transform.operation) {
+      case 'addProperties':
+        Object.assign(state, transform.properties);
+        break;
+
+      case 'removeProperties':
+        for (const key of transform.properties) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
