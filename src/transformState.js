7699d32b4a8efbd55d82225db04fa1748c57c03e
Sasha Kozlovskyy <alex33507@gmail.com>
2021-06-23 17:19:26 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -2,10 +2,28 @@
 
 /**
  * @param {Object} state
- * @param {Object[]} actions
+ * @param {Object[]} is
  */
-function transformState(state, actions) {
-  // write code here
+function transformState(state, is) {
+  for (const i of is) {
+    switch (i.type) {
+      case 'addProperties':
+        Object.assign(state, i.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of i.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
