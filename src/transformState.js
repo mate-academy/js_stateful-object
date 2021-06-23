739422f0077398e6d1083363a7d702261de4a7db
Artem <temaqwerty15@gmail.com>
2021-06-23 16:31:02 +0300

    fix1

--- src/transformState.js
+++ src/transformState.js
@@ -5,47 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const i in actions) {
-    let act = '';
+  for (const action of actions) {
+    switch (action.type) {
+      case ('addProperties'):
+        Object.assign(state, action.extraData);
+        break;
 
-    for (const j in actions[i]) {
-      const action = (actions[i])[j];
-
-      if (act === 'add') {
-        for (const e in action) {
-          state[e] = action[e];
+      case ('removeProperties'):
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
-      }
+        break;
 
-      if (act === 'rem') {
+      case ('clear'):
         for (const key in state) {
-          for (const e in action) {
-            if (action[e] === key) {
-              delete state[key];
-            }
-          }
+          delete state[key];
         }
-      }
-
-      if (typeof (action) === typeof ('a')) {
-        if (action === 'addProperties') {
-          act = 'add';
-        }
-
-        if (action === 'removeProperties') {
-          act = 'rem';
-        }
-
-        if (action === 'clear') {
-          for (const e in state) {
-            delete state[e];
-          }
-        }
-      }
+        break;
     }
   }
-
-  return state;
 };
 
 module.exports = transformState;
