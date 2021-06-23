a9aaa84a325b98fe4ae3ed0d73c0c3774d055db1
Artem <temaqwerty15@gmail.com>
2021-06-23 16:05:23 +0300

    fin

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,47 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (const i in actions) {
+    let act = '';
+
+    for (const j in actions[i]) {
+      const action = (actions[i])[j];
+
+      if (act === 'add') {
+        for (const e in action) {
+          state[e] = action[e];
+        }
+      }
+
+      if (act === 'rem') {
+        for (const key in state) {
+          for (const e in action) {
+            if (action[e] === key) {
+              delete state[key];
+            }
+          }
+        }
+      }
+
+      if (typeof (action) === typeof ('a')) {
+        if (action === 'addProperties') {
+          act = 'add';
+        }
+
+        if (action === 'removeProperties') {
+          act = 'rem';
+        }
+
+        if (action === 'clear') {
+          for (const e in state) {
+            delete state[e];
+          }
+        }
+      }
+    }
+  }
+
+  return state;
+};
 
 module.exports = transformState;
