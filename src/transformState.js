c715666bdb98aac4988acbce7179c65266d1dbe0
Andrii Balitskyi <10balian10@gmail.com>
2021-06-23 16:35:47 +0200

    parenthesis removed

--- src/transformState.js
+++ src/transformState.js
@@ -7,17 +7,17 @@
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
-      case ('addProperties'):
+      case 'addProperties':
         Object.assign(state, action.extraData);
         break;
 
-      case ('removeProperties'):
+      case 'removeProperties':
         for (const propertyToRemove of action.keysToRemove) {
           delete state[propertyToRemove];
         }
         break;
 
-      case ('clear'):
+      case 'clear':
         for (const property in state) {
           delete state[property];
         }
