1a3fe0f07b77a3537317064359f63253eea19976
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-06-24 15:11:35 +0300

    fix

--- src/transformState.js
+++ src/transformState.js
@@ -7,28 +7,18 @@
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     const actionsValues = Object.values(actions[i]);
-    let actionsValuesKeys = [];
     let actionsValuesValues = [];
 
     switch (actionsValues[0]) {
       case 'addProperties' :
-        actionsValuesKeys = Object.keys(actionsValues[1]);
-        actionsValuesValues = Object.values(actionsValues[1]);
-
-        for (let q = 0; q < actionsValuesKeys.length; q++) {
-          state[actionsValuesKeys[q]] = actionsValuesValues[q];
-        }
+        Object.assign(state, actions[i].extraData);
         break;
 
       case 'removeProperties' :
         actionsValuesValues = Object.values(actionsValues[1]);
 
-        for (const key in state) {
-          for (let q = 0; q < actionsValuesValues.length; q++) {
-            if (key === actionsValuesValues[q]) {
-              delete state[key];
-            }
-          }
+        for (const key of actionsValuesValues) {
+          delete state[key];
         }
         break;
 
