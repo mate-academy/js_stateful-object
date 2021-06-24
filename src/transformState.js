317a9def44e2737c9fd705644c80616c724dd423
tkachukione <tkachukione@gmail.com>
2021-06-24 17:38:13 +0300

    switch added

--- src/transformState.js
+++ src/transformState.js
@@ -6,23 +6,67 @@
  */
 function transformState(state, actions) {
   for (const key in actions) {
-    if (actions[key].type === 'addProperties') {
-      Object.assign(state, actions[key].extraData);
-    }
+    switch (actions[key].type) {
+      case 'addProperties':
+        Object.assign(state, actions[key].extraData);
+        break;
 
-    if (actions[key].type === 'removeProperties') {
-      const remove = Object.assign([], actions[key].keysToRemove);
+      case 'removeProperties':
+        for (const keyRemove of actions[key].keysToRemove) {
+          delete state[keyRemove];
+        }
+        break;
 
-      for (const removeKey of remove) {
-        delete state[removeKey];
-      }
-    }
-
-    if (actions[key].type === 'clear') {
-      for (const stateKey in state) {
-        delete state[stateKey];
-      }
+      case 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
     }
   }
 }
+// function transformState(state, actions) {
+//   for (const key in actions) {
+//     if (actions[key].type === 'addProperties') {
+//       Object.assign(state, actions[key].extraData);
+//     }
+
+//     if (actions[key].type === 'removeProperties') {
+//       const remove = Object.assign([], actions[key].keysToRemove);
+
+//       for (const removeKey of remove) {
+//         delete state[removeKey];
+//       }
+//     }
+
+//     if (actions[key].type === 'clear') {
+//       for (const stateKey in state) {
+//         delete state[stateKey];
+//       }
+//     }
+//   }
+// }
+
+// const actionsHistory = [];
+//   const copy = { ...state };
+// function transformState(state, actions) {
+//   for (const key in actions) {
+//     if (actions[key].type === 'addProperties') {
+//       Object.assign(copy, actions[key].extraData);
+//     }
+
+//     if (actions[key].type === 'removeProperties') {
+//       const remove = Object.assign([], actions[key].keysToRemove);
+
+//       for (const removeKey of remove) {
+//         delete copy[removeKey];
+//       }
+//     }
+
+//     if (actions[key].type === 'clear') {
+//       for (const stateKey in copy) {
+//         delete copy[stateKey];
+//       }
+//     }
+//   }
+// }
 module.exports = transformState;
