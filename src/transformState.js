4a87f421e4535a372e357e32890cff8a5c0b7313
Ihor Nahnybida <igor653279@gmail.com>
2021-08-02 22:45:19 +0300

    Transformed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        for (const i in obj.extraData) {
+          state[i] = obj.extraData[i];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const i in obj.keysToRemove) {
+          delete state[obj.keysToRemove[i]];
+        }
+        break;
+
+      default:
+        for (const i in state) {
+          delete state[i];
+        }
+    }
+  }
 }
 
 module.exports = transformState;
