f7ccf9f8ac2909277af3392cb6c5d0a08061130a
NastyaVovk <winc4ester@gmail.com>
2021-08-03 21:10:28 +0300

    initial

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const oneAction of actions) {
+    const { type, extraData, keysToRemove } = oneAction;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const oneToRemove of keysToRemove) {
+          delete state[oneToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
