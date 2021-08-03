32324de70eb52f91c370236fe3616dc45e0ff32a
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-03 15:52:48 +0300

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const item of actions) {
+    if (item.type === 'addProperties') {
+      for (const key in item.extraData) {
+        state[key] = item.extraData[key];
+      }
+    } else if (item.type === 'removeProperties') {
+      for (const keyRemove of item.keysToRemove) {
+        if (keyRemove in state) {
+          delete state[keyRemove];
+        }
+      }
+    } else if (item.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
