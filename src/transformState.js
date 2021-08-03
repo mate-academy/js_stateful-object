8893ffc4de0bc20cd81d8f62689a41724289e0de
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-03 17:16:02 +0300

    change to switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,24 @@
  */
 function transformState(state, actions) {
   for (const item of actions) {
-    if (item.type === 'addProperties') {
-      for (const key in item.extraData) {
-        state[key] = item.extraData[key];
-      }
-    } else if (item.type === 'removeProperties') {
-      for (const keyRemove of item.keysToRemove) {
-        if (keyRemove in state) {
-          delete state[keyRemove];
+    switch (item.type) {
+      case 'addProperties':
+        for (const key in item.extraData) {
+          state[key] = item.extraData[key];
         }
-      }
-    } else if (item.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+      case 'removeProperties':
+        for (const keyRemove of item.keysToRemove) {
+          if (keyRemove in state) {
+            delete state[keyRemove];
+          }
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
