a56d017f3e7b2780d43593e779b92abe4aa40b1b
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-08-03 14:52:10 +0300

    Fixes after review

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,28 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
 
-    if (action.type === 'removeProperties') {
-      for (const keyToRemove of action.keysToRemove) {
-        if (state[keyToRemove]) {
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
           delete state[keyToRemove];
         }
-      }
-    }
 
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+
+        break;
+
+      default:
+        break;
     }
   }
 }
