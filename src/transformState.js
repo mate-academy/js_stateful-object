43588fd512d0d8ac56357b751434742cee91688c
VeronikaLushchik <loaaary212@gmail.com>
2021-08-03 13:59:47 +0300

    fixes

--- src/transformState.js
+++ src/transformState.js
@@ -5,21 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  const clearAction = 'clear';
+  const addPropertiesAction = 'addProperties';
+  const removePropertiesAction = 'removeProperties';
+
   for (const action of actions) {
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
-
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
+    switch (action.type) {
+      case clearAction:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      case addPropertiesAction:
+        Object.assign(state, action.extraData);
+        break;
+      case removePropertiesAction:
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
