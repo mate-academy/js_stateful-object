0ac120e3363d0e14ca7c93cfd4083d1c4ec10ba3
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-08-04 16:51:42 +0300

    third commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,32 +5,28 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  actions.forEach(element => {
-    for (const key in element) {
-      switch (element[key]) {
-        case 'addProperties':
-          for (const key2 in element.extraData) {
-            state[key2] = element.extraData[key2];
-          };
-          break;
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-        case 'removeProperties':
-          for (const key2 in element.keysToRemove) {
-            delete state[element.keysToRemove[key2]];
-          };
-          break;
+      case 'removeProperties':
+        for (const key2 in action.keysToRemove) {
+          delete state[action.keysToRemove[key2]];
+        };
+        break;
 
-        case 'clear':
-          for (const key2 in state) {
-            delete state[key2];
-          }
-          break;
+      case 'clear':
+        for (const key2 in state) {
+          delete state[key2];
+        }
+        break;
 
-        default:
-          break;
-      }
+      default:
+        break;
     }
-  });
+  }
 
   return state;
 }
