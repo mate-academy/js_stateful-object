12383a08298b8c16c9a53b13fd05be8bd2672f27
Polina Razumna <poliandiy@gmail.com>
2021-08-04 17:39:21 +0300

    added switch

--- src/transformState.js
+++ src/transformState.js
@@ -8,20 +8,24 @@ function transformState(state, actions) {
   // write code here
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (let j = 0; j < action.keysToRemove.length; j++) {
-        for (const key in state) {
-          if (key === action.keysToRemove[j]) {
-            delete state[key];
-          }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
-      }
-    } else {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        break;
     }
   }
 }
