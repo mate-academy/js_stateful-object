9153977ea53b342fa942745cc86724cc6be4b66b
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-08-04 19:38:04 +0300

    first try

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const toDelete of action.keysToRemove) {
+          if (Object.prototype.hasOwnProperty.call(state, toDelete)) {
+            delete state[toDelete];
+          }
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
