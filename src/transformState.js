afe227eec32e4217ea4177e817390fbed297b413
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-08-04 15:17:11 +0300

    first commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(element => {
+    for (const key in element) {
+      if (element[key] === 'addProperties') {
+        for (const key2 in element.extraData) {
+          state[key2] = element.extraData[key2];
+        }
+      }
+
+      if (element[key] === 'removeProperties') {
+        for (const key2 in element.keysToRemove) {
+          delete state[element.keysToRemove[key2]];
+        }
+      }
+
+      if (element[key] === 'clear') {
+        for (const key2 in state) {
+          delete state[key2];
+        }
+      }
+    }
+  });
+
+  return state;
 }
 
 module.exports = transformState;
