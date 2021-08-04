07ce6a7b0729449e4025739d46e9b8cc2faea9db
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-08-04 23:26:24 +0300

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (const object of actions) {
+    for (const types in object) {
+      if (object[types] === 'addProperties') {
+        Object.assign(state, object.extraData);
+      }
 
+      if (object[types] === 'removeProperties') {
+        for (const key of object.keysToRemove) {
+          if (!state.key) {
+            delete state[key];
+          }
+        }
+      }
+
+      if (object[types] === 'clear') {
+        for (const keys in state) {
+          delete state[keys];
+        }
+      }
+    }
+  }
+}
 module.exports = transformState;
