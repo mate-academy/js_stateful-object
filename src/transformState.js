d665c581e0bac0fca3d0118727b89dc60fed735b
karolina-nadDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD <westlinka20@gmail.com>
2021-08-04 15:27:51 +0300

    task done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const properties of actions) {
+    if (properties.type === 'addProperties') {
+      Object.assign(state, properties.extraData);
+    }
+
+    if (properties.type === 'removeProperties') {
+      for (const keys in properties.keysToRemove) {
+        delete state[properties.keysToRemove[keys]];
+      }
+    }
+
+    if (properties.type === 'clear') {
+      for (const properties2 in state) {
+        delete state[properties2];
+      };
+    }
+  }
 }
 
 module.exports = transformState;
