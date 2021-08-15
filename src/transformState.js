e06e1ad62ace1012f7a7e281d7477f2fd1cd1e6a
Illia Ostrovskiy <aleanrenuar@gmail.com>
2021-08-15 19:27:49 +0300

    Initial commit

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,31 @@
  */
 function transformState(state, actions) {
   // write code here
+
+  for (const i in actions) {
+    if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+      continue;
+    }
+
+    if (actions[i].type === 'addProperties') {
+      for (const value in actions[i].extraData) {
+        state[value] = actions[i].extraData[value];
+      }
+      continue;
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        if (state[key]) {
+          delete state[key];
+        }
+      }
+      continue;
+    }
+  }
 }
 
 module.exports = transformState;
