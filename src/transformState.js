4c74984ebbc6771a887254c4e4c51a6c64afadaf
Rak Oleg <rakole86@gmail.com>
2021-09-01 17:09:14 +0300

    js_stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (const actionKey of actions) {
+    for (const key in actionKey) {
+      if (actionKey[key] === 'addProperties') {
+        Object.assign(state, actionKey.extraData);
+      }
 
+      if (actionKey[key] === 'clear') {
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+      }
+
+      if (actionKey[key] === 'removeProperties') {
+        for (const prop of actionKey.keysToRemove) {
+          delete state[prop];
+        }
+      }
+    }
+  }
+
+  return state;
+}
 module.exports = transformState;
