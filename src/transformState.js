aed228c693115b097b2c0fa9d19ffb0b1e365dd0
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-09-10 18:00:24 +0300

    solution js_stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,31 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const properties of actions) {
+    const { type, extraData, keysToRemove } = properties;
+
+    if (type === 'addProperties') {
+      for (const value in extraData) {
+        state[value] = extraData[value];
+      }
+    } else if (type === 'removeProperties') {
+      for (const value of keysToRemove) {
+        for (const key in state) {
+          if (value === key) {
+            delete state[key];
+          }
+        }
+      }
+    } else {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
