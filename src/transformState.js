a3ced1e57fc401580afc57e6c4ce8f220f6a4f09
PresidentComanch <ap.dissembler@gmail.com>
2021-09-15 17:55:47 +0300

    --done

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,31 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action['type'] === 'addProperties') {
+      for (const key in action['extraData']) {
+        state[key] = action['extraData'][key];
+      }
+    }
+
+    if (action['type'] === 'removeProperties') {
+      for (const rem of action.keysToRemove) {
+        delete state[rem];
+      }
+    }
+
+    if (action['type'] === 'clear') {
+      for (const key in state) {
+        if (state.hasOwnProperty(key)) {
+          delete state[key];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
