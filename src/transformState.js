cc351265f51c4535b2b1c7e83c5d51b0733d0424
Victor Shevchuk <callmevitok@gmail.com>
2021-09-15 13:28:46 +0300

    transformState2

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const element of actions) {
-    if (element.type === 'addProperties') {
-      for (const key in element.extraData) {
-        state[key] = element.extraData[key];
-      }
-    } else if (element.type === 'removeProperties') {
-      for (const item of element.keysToRemove) {
-        delete state[item];
-      }
-    } else if (element.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          state[key] = action.extraData[key];
+        };
+        break;
+
+      case 'removeProperties' :
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        };
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
     }
   }
 }
