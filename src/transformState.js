ff7df66a68943c938fe7c5eb033242fee000d5bf
ruslan-bondarenko <ruslan21bondarenko@gmail.com>
2021-09-15 14:22:13 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,53 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
   // write code here
-}
+  for (const i in actions) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+      case 'removeProperties':
+        for (const j in actions[i]) {
+          for (const k in actions[i][j]) {
+            delete state[actions[i][j][k]];
+          }
+        }
+        break;
+      case 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  return state;
+};
 
 module.exports = transformState;
+
+// transformState({foo: 'bar', bar: 'foo'}, [
+//   {
+//     type: 'addProperties', // --если *, то экстра добавляется до state
+//     extraData: {
+//       name: 'Jim',
+//       hello: 'world',
+//     }
+//   },
+//   {
+//     type: 'removeProperties', // --если *, то кейс удаляет с state
+//     keysToRemove: ['bar', 'hello'],
+//   },
+//   {
+//     type: 'addProperties',
+//     extraData: { another: 'one' },
+//   },
+//   {
+//     type: 'clear',
+//   }
+// ]);
