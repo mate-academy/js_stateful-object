0fe79bdf1f0b67fe30fd9bfb4a94733e505b514e
Vitalii Strohush <vitalikstrog2003v@gmail.com>
2021-09-15 19:24:11 +0300

    transformState soulution

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+// eslint-disable-next-line no-shadow
 function transformState(state, actions) {
-  // write code here
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      Object.assign(state, i.extraData);
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const b of i.keysToRemove) {
+        delete state[b];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
