49159503a71ba3f01a4f1b6f5a101a535993691e
Dmytro Baltak <dmitrybaltak@gmail.com>
2021-09-15 14:40:54 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.map(({ type, extraData, keysToRemove }) => {
+    switch (type) {
+      case 'addProperties':
+        for (const key in extraData) {
+          state[key] = extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        Object.keys(state).map(key => {
+          delete state[key];
+        });
+        break;
+
+      default:
+        throw new Error('Invalid action type.');
+    }
+  });
 }
 
 module.exports = transformState;
