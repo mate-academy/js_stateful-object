689e99f92aeb7e0d753ddbe32abd20d709eb0a8c
caelum29 <caelum29@gmail.com>
2021-09-16 11:49:33 +0300

    changes after review

--- src/transformState.js
+++ src/transformState.js
@@ -6,15 +6,19 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.keys(action.extraData)
-        .forEach(key => (state[key] = action.extraData[key]));
-    } else if (action.type === 'removeProperties') {
-      for (const keyToDelete of action.keysToRemove) {
-        delete state[keyToDelete];
-      }
-    } else if (action.type === 'clear') {
-      Object.keys(state).forEach(key => (delete state[key]));
+    switch (action.type) {
+      case 'addProperties':
+        Object.keys(action.extraData)
+          .forEach(key => (state[key] = action.extraData[key]));
+        break;
+      case 'removeProperties':
+        for (const keyToDelete of action.keysToRemove) {
+          delete state[keyToDelete];
+        }
+        break;
+      case 'clear':
+        Object.keys(state).forEach(key => (delete state[key]));
+        break;
     }
   }
 }
