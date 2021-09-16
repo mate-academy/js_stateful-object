38648b8a56ac8ae09955d2f3c0281e97aa1017c6
anastasiia <theyashkindzhus@gmail.com>
2021-09-16 09:37:16 +0300

    done

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(state, key.extraData);
+    }
+
+    if (key.type === 'clear') {
+      for (const stateKey in state) {
+        delete state[stateKey];
+      }
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const removeKey of key.keysToRemove) {
+        delete state[removeKey];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
