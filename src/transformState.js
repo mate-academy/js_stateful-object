2d5fc92909d6ef637730804228681663efedd73f
Michael Strazhevski <work.acc.com@gmail.com>
2021-09-20 13:13:27 +0300

    Made over on switch\case

--- src/transformState.js
+++ src/transformState.js
@@ -5,18 +5,38 @@
  * @param {Object[]} actions
  */
 
+// function transformState1(state, actions) {
+//   for (const action of actions) {
+//     if (action.type === 'addProperties') {
+//       Object.assign(state, action.extraData);
+//     } else if (action.type === 'removeProperties') {
+//       for (const key of action.keysToRemove) {
+//         delete state[key];
+//       }
+//     } else if (action.type === 'clear') {
+//       for (const prop in state) {
+//         delete state[prop];
+//       }
+//     }
+//   }
+// }
+
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const prop in state) {
-        delete state[prop];
-      }
+    switch (true) {
+      case (action.type === 'addProperties'):
+        Object.assign(state, action.extraData);
+        break;
+      case (action.type === 'removeProperties'):
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        };
+        break;
+      case (action.type === 'clear'):
+        for (const prop in state) {
+          delete state[prop];
+        };
+        break;
     }
   }
 }
