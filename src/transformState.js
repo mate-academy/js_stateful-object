3d6ab3eebac8e6e70d2b51ae7a8f6e23d319758b
Michael Strazhevski <work.acc.com@gmail.com>
2021-09-20 13:15:07 +0300

    Switch\case — clear code

--- src/transformState.js
+++ src/transformState.js
@@ -5,22 +5,6 @@
  * @param {Object[]} actions
  */
 
-// function transformState1(state, actions) {
-//   for (const action of actions) {
-//     if (action.type === 'addProperties') {
-//       Object.assign(state, action.extraData);
-//     } else if (action.type === 'removeProperties') {
-//       for (const key of action.keysToRemove) {
-//         delete state[key];
-//       }
-//     } else if (action.type === 'clear') {
-//       for (const prop in state) {
-//         delete state[prop];
-//       }
-//     }
-//   }
-// }
-
 function transformState(state, actions) {
   for (const action of actions) {
     switch (true) {
