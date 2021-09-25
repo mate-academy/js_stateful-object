275f9f844e45fbdc03fef32e00ef6c1e3567a248
Vitaly <vitaly.pasechnik.11@gmail.com>
2021-09-25 21:02:27 +0300

    saved 1 25/09

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,23 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- */
 function transformState(state, actions) {
-  // write code here
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      Object.assign(state, i.extraData);
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const x in i.keysToRemove) {
+        delete state[i.keysToRemove[x]];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const x in state) {
+        delete state[x];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
