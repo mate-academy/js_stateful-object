c00baf6ba9ed9c2645ddcdecf7096f343a43afef
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-06 21:40:03 +0300

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,33 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const key of actions) {
+    for (const char in key) {
+      if (key[char] === 'addProperties') {
+        for (const a in key.extraData) {
+          state[a] = key.extraData[a];
+        }
+      }
+
+      if (key[char] === 'removeProperties') {
+        for (const a in state) {
+          if (key.keysToRemove.includes(a)) {
+            delete state[a];
+          }
+        }
+      }
+
+      if (key[char] === 'clear') {
+        for (const clear in state) {
+          delete state[clear];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
