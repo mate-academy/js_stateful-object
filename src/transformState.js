a8551acf7b8f386fbf6ee183b14a373d6742f08f
Eugene Chornyi <winheisbox@gmail.com>
2021-10-14 18:51:18 +0300

    HomeWork

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,33 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        for (const key of keysToRemove) {
+          delete state[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const element in state) {
+          delete state[element];
+        }
+
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
