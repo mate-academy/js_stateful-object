28fad03a658e21ae1aa13da6c1eac5f0f7b5314e
Oleg Kozak <kozakoleg77@ukr.net>
2021-10-16 14:31:44 +0300

    Fixes after code review

--- src/transformState.js
+++ src/transformState.js
@@ -6,18 +6,22 @@
  */
 function transformState(state, actions) {
   for (const key of actions) {
-    for (const lit in key) {
-      if (key[lit] === 'addProperties') {
+    switch (key.type) {
+      case 'addProperties':
         Object.assign(state, key.extraData);
-      } else if (key[lit] === 'removeProperties') {
+        break;
+
+      case 'removeProperties':
         for (const y of key.keysToRemove) {
           delete state[y];
         }
-      } else if (key[lit] === 'clear') {
+        break;
+
+      case 'clear':
         for (const part in state) {
           delete state[part];
         }
-      }
+        break;
     }
   }
 }
