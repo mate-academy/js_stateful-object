cb4ae5aa4a7ee44b467231c9ec867f8c406934b4
Robert Krukau <robert.grodno@mail.ru>
2021-11-04 10:07:09 +0100

    Fixed to Switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,18 +6,20 @@
  */
 function transformState(state, actions) {
   for (const item of actions) {
-    if (item.type === 'clear') {
-      for (const cleared in state) {
-        delete state[cleared];
-      }
-    } else if (item.type === 'addProperties') {
-      Object.assign(state, item.extraData);
-    } else if (item.type === 'removeProperties') {
-      for (const key of item.keysToRemove) {
-        delete state[key];
-      }
-    } else {
-      continue;
+    switch (item.type) {
+      case 'clear':
+        for (const cleared in state) {
+          delete state[cleared];
+        }
+        break;
+      case 'addProperties':
+        Object.assign(state, item.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of item.keysToRemove) {
+          delete state[key];
+        }
+        break;
     }
   }
 
