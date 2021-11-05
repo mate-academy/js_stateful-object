4c55f1cdb44fe1fb41266f7a09938f09741bb16a
Andriy Bahayev <andrbah2015@gmail.com>
2021-11-05 18:56:05 +0100

    1)  action in actionObjects loop was deleted, 2) if/elseif  was changed to switch statements, 3) bracket notation was changed to dot notation in actionObjects.extraData loop, 4) unnecessary loop was deleted from removeProperties case

--- src/transformState.js
+++ src/transformState.js
@@ -6,24 +6,24 @@
  */
 function transformState(state, actions) {
   for (const actionObjects of actions) {
-    for (const action in actionObjects) {
-      if (actionObjects[action] === 'addProperties') {
-        for (const data in actionObjects['extraData']) {
-          state[data] = actionObjects['extraData'][data];
+    switch (actionObjects.type) {
+      case 'addProperties':
+        for (const data in actionObjects.extraData) {
+          state[data] = actionObjects.extraData[data];
         }
-      } else if (actionObjects[action] === 'removeProperties') {
-        for (const aKeyToRemove of actionObjects['keysToRemove']) {
-          for (const sts in state) {
-            if (sts === aKeyToRemove) {
-              delete state[sts];
-            }
+        break;
+      case 'removeProperties':
+        for (const aKeyToRemove of actionObjects.keysToRemove) {
+          if (aKeyToRemove in state) {
+            delete state[aKeyToRemove];
           }
         }
-      } else if (actionObjects[action] === 'clear') {
-        for (const sts in state) {
-          delete state[sts];
+        break;
+      case 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
         }
-      }
+        break;
     }
   }
 }
