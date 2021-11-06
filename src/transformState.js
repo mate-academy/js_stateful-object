26476a9d85f4c3f8814d6e0777b9f1fed0e6b234
Vasiliy <voronvasia@gmail.com>
2021-11-06 22:06:19 +0200

    task complite

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,34 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
+function transformState(state, ...actions) {
+  for (const action of actions) {
+    for (const obj in action) {
+      if (action[obj].type === 'clear') {
+        for (const key in state) {
+          delete state[key];
+        }
+      }
+
+      if (action[obj].type === 'addProperties') {
+        for (const key in action[obj].extraData) {
+          const value = action[obj].extraData[key];
+
+          state[key] = value;
+        }
+      }
+
+      if (action[obj].type === 'removeProperties') {
+        for (const key in action[obj].keysToRemove) {
+          const removeKeys = action[obj].keysToRemove[key];
+
+          delete (state[removeKeys]);
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
