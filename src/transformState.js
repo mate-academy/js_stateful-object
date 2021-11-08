ab52e707eddcd12412ada468724cefb496335558
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-08 16:05:09 +0200

    Create a state transformer

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,24 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+
+      default:
+        throw new Error('Performing unknown action');
+    }
+  });
 }
 
 module.exports = transformState;
