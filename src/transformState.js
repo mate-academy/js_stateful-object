40e2b5743206bb81438efcd57d30356de4778041
Ivan Stanko <ivan_stanko@ukr.net>
2021-11-09 13:36:53 +0200

    add stateful obj

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,26 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
-}
+const transformState = (state, actions) => {
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const type of action.keysToRemove) {
+          delete state[type];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
+};
 
 module.exports = transformState;
