62c30197669fac0dfe6be55eebecf02433f4501a
Dariia Romanova <dariia.romanova@gmail.com>
2021-11-09 15:36:33 +0200

    Fixes

--- src/transformState.js
+++ src/transformState.js
@@ -6,13 +6,15 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    switch (action.type) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
       case 'addProperties':
-        Object.assign(state, action.extraData);
+        Object.assign(state, extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
+        for (const key of keysToRemove) {
           delete state[key];
         }
         break;
@@ -21,7 +23,6 @@ function transformState(state, actions) {
         for (const key in state) {
           delete state[key];
         }
-        break;
     }
   }
 }
