e036830036d1ac00ee8870cf516189bde8a81e44
WorkAccCom <work.acc.com@gmail.com>
2021-11-09 14:29:50 +0200

    Second solution: on switch case

--- src/transformState.js
+++ src/transformState.js
@@ -4,18 +4,23 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
   for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      Object.assign(state, obj.extraData);
-    } else if (obj.type === 'removeProperties') {
-      for (const key of obj.keysToRemove) {
-        delete state[key];
-      }
-    } else if (obj.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (obj.type) {
+      case 'addProperties':
+        Object.assign(state, obj.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of obj.keysToRemove) {
+          delete state[key];
+        };
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
     }
   }
 }
