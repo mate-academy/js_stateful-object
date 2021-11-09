135272dcd497a4c347c905eeb693c80339bdf54d
Ivan Meleganych <imi92@ukr.net>
2021-11-09 15:32:38 +0200

    First solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const direct of actions) {
+    if (direct.type === 'addProperties') {
+      Object.assign(state, direct.extraData);
+    }
+
+    if (direct.type === 'removeProperties') {
+      for (const key of direct.keysToRemove) {
+        if (key in state) {
+          delete state[key];
+        }
+      }
+    }
+
+    if (direct.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      };
+    }
+  }
 }
 
 module.exports = transformState;
