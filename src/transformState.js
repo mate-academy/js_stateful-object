79855e52120efa58f00b16eb710501625428895b
Lera Permiakova <vv.permiakova@gmail.com>
2021-11-09 20:00:43 +0200

    transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const prop of actions) {
+    if (prop.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    if (prop.type === 'addProperties') {
+      for (const add in prop.extraData) {
+        state[add] = prop.extraData[add];
+      }
+    }
+
+    if (prop.type === 'removeProperties') {
+      for (const rem of prop.keysToRemove) {
+        for (const key in state) {
+          if (key === rem) {
+            delete state[key];
+          }
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
