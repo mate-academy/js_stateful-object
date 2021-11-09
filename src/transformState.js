37a7e88acbf71cb7dd5d24f78ca32eac5c610a60
Ihor Poliakov <junglqq@gmail.com>
2021-11-09 15:17:47 +0200

    init

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      for (const add in key.extraData) {
+        state[add] = key.extraData[add];
+      }
+    }
+
+    if (key.type === 'clear') {
+      for (const del in state) {
+        delete state[del];
+      }
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const remove of key.keysToRemove) {
+        delete state[remove];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
