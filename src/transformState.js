89027684feb03a87f4e91c49d451b61721a845e0
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-11-09 16:05:56 +0200

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,35 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const obj in actions) {
+    const type = actions[obj].type;
+
+    if (type === 'addProperties') {
+      const addProp = actions[obj].extraData;
+
+      for (const prop in addProp) {
+        state[prop] = addProp[prop];
+      }
+    }
+
+    if (type === 'removeProperties') {
+      const keys = actions[obj].keysToRemove;
+
+      for (const key in keys) {
+        delete state[keys[key]];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const prop in state) {
+        delete state[prop];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
