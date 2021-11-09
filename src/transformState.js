e81f032dae84bfe6d25e856abea51f4fb91345c0
Yevhen Khmelnytskyi <summerssscrystal@gmail.com>
2021-11-09 13:38:03 +0200

    removed unneccesary condition

--- src/transformState.js
+++ src/transformState.js
@@ -7,17 +7,17 @@
 function transformState(state, actions) {
   for (const obj of actions) {
     for (const key in obj) {
-      if (key === 'type' && obj[key] === 'addProperties') {
+      if (obj[key] === 'addProperties') {
         Object.assign(state, obj.extraData);
       }
 
-      if (key === 'type' && obj[key] === 'removeProperties') {
+      if (obj[key] === 'removeProperties') {
         for (const props of obj.keysToRemove) {
           delete state[props];
         }
       }
 
-      if (key === 'type' && obj[key] === 'clear') {
+      if (obj[key] === 'clear') {
         for (const keys in state) {
           delete state[keys];
         }
