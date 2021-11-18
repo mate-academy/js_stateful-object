00a863f1a995decf5cfdfd34bbf9acd6632efc21
Skidan Alexander(notebook) <xpromt@gmail.com>
2021-11-18 13:22:47 +0200

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      Object.assign(state, act.extraData);
+    } else if (act.type === 'removeProperties') {
+      for (const key in act.keysToRemove) {
+        delete (state[act.keysToRemove[key]]);
+      }
+    } else if (act.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
