78d0f740cdb6f3f2ab5d8b7810a0c36d70f8554e
Ihor Barban <barbanihor@gmial.com>
2021-11-23 18:23:51 +0200

    Task completed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    if (obj['type'] === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    } else if (obj['type'] === 'addProperties') {
+      Object.assign(state, obj.extraData);
+    } else if (obj['type'] === 'removeProperties') {
+      ;
+
+      for (const key of obj['keysToRemove']) {
+        if (key in state) {
+          delete state[key];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
