54b038a72eda70c8d85e411209fad3308e726e8a
Vova Alputov <alputov1998@gmail.com>
2021-11-25 21:10:35 +0200

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,27 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const i in actions) {
-    const { type, extraData, keysToRemove } = actions[i];
+  for (const action of actions) {
 
-    if (type === 'addProperties') {
-      Object.assign(state, extraData);
-    } else if (type === 'removeProperties') {
-      if (keysToRemove.length > 1) {
-        for (const key of keysToRemove) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties':
+        if (keysToRemove.length > 1) {
+          for (const key of keysToRemove) {
+            delete state[key];
+          }
+        } else {
+          delete state[keysToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
           delete state[key];
         }
-      } else {
-        delete state[keysToRemove];
-      }
-    } else if (type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
     }
   }
 
   return state;
 }
-
 module.exports = transformState;
