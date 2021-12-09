8589fd6906e493e8f24b6e8397ae2a0e6c42edaa
Anton Ustavshchykov <unton84@gmail.com>
2021-12-09 17:11:41 +0200

    fixed comments

--- src/transformState.js
+++ src/transformState.js
@@ -30,29 +30,4 @@ function transformState(state, actions) {
   return newState;
 }
 
-//   for (let i = 0; i < actions.length; i++) {
-//     if (actions[i].type === 'addProperties') {
-//       newState = Object.assign(state, actions[i].extraData);
-//     }
-
-//     if (actions[i].type === 'removeProperties') {
-//       for (const j in state) {
-//         for (let k = 0; k < actions[i].keysToRemove.length; k++) {
-//           if (actions[i].keysToRemove[k] === j) {
-//             delete state[j];
-//           }
-//         }
-//       }
-//     }
-
-//     if (actions[i].type === 'clear') {
-//       for (const j in state) {
-//         delete state[j];
-//       }
-//     }
-//   }
-
-//   return newState;
-// }
-
 module.exports = transformState;
