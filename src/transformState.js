4514d03925b6f8014b58cdd6186c0e431e3034b7
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-12 17:22:23 +0200

    commit added

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const element of actions) {
+    for (const key in element) {
+      if (element[key] === 'removeProperties') {
+        for (let i = 0; i < element.keysToRemove.length; i++) {
+          delete state[element.keysToRemove[i]];
+        }
+      }
+
+      if (element[key] === 'addProperties') {
+        Object.assign(state, element.extraData);
+      }
+
+      if (element[key] === 'clear') {
+        for (const item in state) {
+          delete state[item];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
