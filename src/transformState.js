09c82df96cbc1f69f89a11a1d75c1ac2bda207bc
Samson Sim <samson.sim@ukr.net>
2021-12-13 20:13:50 +0200

    develop

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(state, actions[i].extraData);
+    } else if (actions[i].type === 'removeProperties') {
+      for (const key of actions[i].keysToRemove) {
+        delete state[key];
+      }
+    } else {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
