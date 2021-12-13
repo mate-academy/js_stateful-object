9d6550f2ac8bedd0fd3d60113fba5090d1fb8009
Niki <tes.kolia@gmail.com>
2021-12-13 19:12:49 +0200

    done

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,19 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(state, key.extraData);
+    } else if (key.type === 'removeProperties') {
+      for (const delKey of key.keysToRemove) {
+        delete state[delKey];
+      }
+    } else if (key.type === 'clear') {
+      for (const i in state) {
+        delete state[i];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
