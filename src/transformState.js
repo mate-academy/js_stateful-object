e2277bd21865add0db40cbeea67741f289976b3b
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-13 12:13:03 +0200

    another commit

--- src/transformState.js
+++ src/transformState.js
@@ -7,20 +7,38 @@
 function transformState(state, actions) {
   for (const element of actions) {
     for (const key in element) {
-      if (element[key] === 'removeProperties') {
-        for (let i = 0; i < element.keysToRemove.length; i++) {
-          delete state[element.keysToRemove[i]];
-        }
-      }
+      // if (element[key] === 'removeProperties') {
+      //   for (let i = 0; i < element.keysToRemove.length; i++) {
+      //     delete state[element.keysToRemove[i]];
+      //   }
+      // }
 
-      if (element[key] === 'addProperties') {
-        Object.assign(state, element.extraData);
-      }
+      // if (element[key] === 'addProperties') {
+      //   Object.assign(state, element.extraData);
+      // }
 
-      if (element[key] === 'clear') {
-        for (const item in state) {
-          delete state[item];
-        }
+      // if (element[key] === 'clear') {
+      //   for (const item in state) {
+      //     delete state[item];
+      //   }
+      // }
+
+      switch (element[key]) {
+        case 'removeProperties':
+          for (let i = 0; i < element.keysToRemove.length; i++) {
+            delete state[element.keysToRemove[i]];
+          }
+          break;
+
+        case 'addProperties':
+          Object.assign(state, element.extraData);
+          break;
+
+        case 'clear':
+          for (const item in state) {
+            delete state[item];
+          }
+          break;
       }
     }
   }
