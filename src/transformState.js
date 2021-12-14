f93597bf7a55ab2371c0aeb8bae0d7d8e2b9fb3c
michael valiahmetov <michael.valiahmetov@gmail.com>
2021-12-14 23:58:35 +0200

    Transform-State

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        const keysAdd = Object.keys(actions[i].extraData);
+        const valuesAdd = Object.values(actions[i].extraData);
+
+        for (let y = 0; y < keysAdd.length; y++) {
+          state[keysAdd[y]] = valuesAdd[y];
+        };
+        break;
+
+      case 'removeProperties':
+        const valuesRemove = Object.values(actions[i].keysToRemove);
+
+        for (let j = 0; j < valuesRemove.length; j++) {
+          delete state[valuesRemove[j]];
+        };
+        break;
+
+      case 'clear':
+        for (const keyClear in state) {
+          delete state[keyClear];
+        };
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
