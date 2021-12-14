c7f9f3d1e426d0661a26fe09f48e948b0ea395d0
Taras Zherebetkiy <tarasj76@gmail.com>
2021-12-14 16:12:06 +0200

    all test passed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(state, obj.extraData);
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const item of obj.keysToRemove) {
+        delete state[item];
+      }
+    }
+
+    if (obj.type === 'clear') {
+      for (const item in state) {
+        delete state[item];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
