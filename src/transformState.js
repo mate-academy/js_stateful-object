6302a1f5414455ac6da351650ea3a87aeae5a676
michael valiahmetov <michael.valiahmetov@gmail.com>
2021-12-15 12:33:26 +0200

    Stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,22 +5,15 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    switch (actions[i].type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        const keysAdd = Object.keys(actions[i].extraData);
-        const valuesAdd = Object.values(actions[i].extraData);
-
-        for (let y = 0; y < keysAdd.length; y++) {
-          state[keysAdd[y]] = valuesAdd[y];
-        };
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        const valuesRemove = Object.values(actions[i].keysToRemove);
-
-        for (let j = 0; j < valuesRemove.length; j++) {
-          delete state[valuesRemove[j]];
+        for (const keyRemove of action.keysToRemove) {
+          delete state[keyRemove];
         };
         break;
 
