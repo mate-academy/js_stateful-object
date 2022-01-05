2a987993c5bc9f01a7e1a8fc6d01c967f35b2a92
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-01-06 00:42:54 +0200

    done

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const properties of actions) {
+    if (properties.type === 'addProperties') {
+      Object.assign(state, properties.extraData);
+    }
+
+    if (properties.type === 'removeProperties') {
+      for (const removeKey of properties.keysToRemove) {
+        delete state[removeKey];
+      }
+    }
+
+    if (properties.type === 'clear') {
+      for (const clear in state) {
+        delete state[clear];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
