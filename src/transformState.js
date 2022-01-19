345a36d0aa5b206d1ea03bcafd417ad082649a4b
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-19 21:19:39 +0200

    fone

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        for (const prop in act.extraData) {
+          state[prop] = act.extraData[prop];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of act.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
