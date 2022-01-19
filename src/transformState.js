fa737b9ecc948623547768656459281a43254658
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-19 13:59:50 +0200

    nice one...

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const i in actions) {
+    switch (actions[i].type) {
+      case 'addProperties': {
+        for (const ent of Object.entries(actions[i].extraData)) {
+          state[ent[0]] = ent[1];
+        }
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const key of actions[i].keysToRemove) {
+          delete state[key];
+        }
+        break;
+      }
+
+      case `clear`: {
+        for (const key in state) {
+          delete state[key];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
