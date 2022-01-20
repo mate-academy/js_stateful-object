983fb0bab43409c496a76852b22fd6207fd5df37
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-20 19:03:09 +0200

    ()(())

--- src/transformState.js
+++ src/transformState.js
@@ -5,17 +5,17 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const i in actions) {
-    switch (actions[i].type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties': {
-        for (const ent of Object.entries(actions[i].extraData)) {
+        for (const ent of Object.entries(action.extraData)) {
           state[ent[0]] = ent[1];
         }
         break;
       }
 
       case 'removeProperties': {
-        for (const key of actions[i].keysToRemove) {
+        for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
@@ -25,6 +25,11 @@ function transformState(state, actions) {
         for (const key in state) {
           delete state[key];
         }
+        break;
+      }
+
+      default: {
+        break;
       }
     }
   }
