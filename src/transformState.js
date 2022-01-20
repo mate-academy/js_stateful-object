6f6f7de6c934b087f31ae4b802309f3a6debc707
Vadym Suslov <vsuslov2906@gmail.com>
2022-01-20 16:46:43 +0200

    fixes...

--- src/transformState.js
+++ src/transformState.js
@@ -5,18 +5,24 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
   actions.forEach(action => {
-    const type = action.type;
+    const { type } = action;
 
-    if (type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (type === 'removeProperties') {
-      action.keysToRemove.forEach(key => delete state[key]);
-    } else if (type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        break;
     }
   });
 }
