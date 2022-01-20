340d0026b4556938485d2f4865ab0fa5656e0a2e
Kostya Kovbel <dovkerdota@gmail.com>
2022-01-20 17:51:17 +0200

    sol

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,29 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(element => {
+          delete state[element];
+        });
+        break;
+
+      case 'clear' :
+        for (const properties in state) {
+          delete state[properties];
+        }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
