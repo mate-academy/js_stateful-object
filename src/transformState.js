854fdc33f1b0a555958d88a8697d2fdb689c3b05
Roman Kozhukhovskyi <4kroman@gmail.com>
2022-01-20 16:38:25 +0200

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,19 +1,78 @@
 'use strict';
 
+/**
+ * Implement a function accepting 2 arguments `state` and `transforms`. It
+ * should change the `state` basing on the given `transforms`
+ *
+ * `state` is an object. You are supposed to add, change, or delete its
+ *  properties instead of creating a new object
+ *
+ * `transforms` is an array of objects having the following properties:
+ * `operation`: either `addProperties`, `removeProperties` or `clear`;
+ * `properties`:
+ *   - if `operation` is `addProperties`, this property contains an object
+ *   with `key: value` pairs to add to the state;
+ *   - if `operation` is `removeProperties`, this property contains an array
+ *   with the list of property names to remove from the state; (Not existing
+ *   properties should be ignored)
+ *   - if `operation is `clear` you should remove all the properties from the
+ *   state
+ *
+ * Sample usage:
+ *
+ * If `state` is {foo: 'bar', bar: 'foo'}, then
+ *
+ * transformState(state, [
+ *   {
+ *     operation: 'addProperties',
+ *     properties: {
+ *       name: 'Jim',
+ *.      hello: 'world',
+ *.    }
+ *.  },
+ *   {
+ *     operation: 'removeProperties',
+ *     properties: ['bar', 'hello'],
+ *   },
+ *   {
+ *     operation: 'addProperties',
+ *     properties: { another: 'one' },
+ *   }
+ * ])
+ *
+ * should modify the `state` object so after the call it becomes
+ * {
+ *   foo: 'bar',
+ *   name: 'Jim',
+ *   another: 'one',
+ * }
+ *
+ * Then after calling
+ *
+ * transformState(state, [
+ *   { operation: 'addProperties', properties: { yet: 'another property' } }
+ *   { operation: 'clear' },
+ *   { operation: 'addProperties', properties: { foo: 'bar', name: 'Jim' } }
+ * ])
+ *
+ * the `state` variable must contain
+ * { foo: 'bar', name: 'Jim' }
+ *
+ * @param {Object} state
+ * @param {Object[]} transforms
+ */
 function transformState(state, transforms) {
   // write code here
-  const transform = transforms;
-
-  for (let i = 0; i < transform.length; i++) {
-    switch (transform[i].operation) {
+  for (let i = 0; i < transforms.length; i++) {
+    switch (transforms[i].operation) {
       case 'addProperties':
-        for (const [key1, value] of Object.entries(transform[i].properties)) {
+        for (const [key1, value] of Object.entries(transforms[i].properties)) {
           state[key1] = value;
         }
         break;
       case 'removeProperties':
-        for (let j = 0; j < transform[i].properties.length; j++) {
-          delete state[transform[i].properties[j]];
+        for (let j = 0; j < transforms[i].properties.length; j++) {
+          delete state[transforms[i].properties[j]];
         }
         break;
       case 'clear':
