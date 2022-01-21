30426cff4c7ff442dd7feb7e08091154db265b15
Heorhii Plugoyi <gladiator943work@gmail.com>
2022-01-21 17:14:52 +0200

    save

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case ('addProperties') :
+        Object.assign(state, extraData);
+        break;
+
+      case ('removeProperties') :
+        for (const key of keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case ('clear') :
+        Object.keys(state).map(key => delete state[key]);
+        // for (const key in state) {
+        //   delete state[key];
+        // }
+        break;
+
+      default : ;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
