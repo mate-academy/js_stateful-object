ec248733d4d422f3dd89636d10d6ac86a8251b61
Maksym Mishchanchuk <yssam.nn@gmail.com>
2022-01-21 17:52:19 +0200

    first comit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const object of actions) {
+    const typeOfObj = object.type;
+
+    switch (typeOfObj) {
+      case 'addProperties':
+        Object.assign(state, object.extraData);
+        break;
+      case 'removeProperties':
+        for (const deleteElement of object.keysToRemove) {
+          delete state[deleteElement];
+        }
+        break;
+      case 'clear':
+        for (const property in state) {
+          delete state[property];
+        }
+        break;
+      default:
+        return ('Err');
+    }
+  }
 }
 
 module.exports = transformState;
