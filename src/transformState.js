be0cb25f1ea42063deaf4776877d779ed6ac73ca
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-01-30 21:56:31 +0200

    Function transformState was created

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const modifiedState = state;
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'clear':
+        for (const key in modifiedState) {
+          delete modifiedState[key];
+        }
+        break;
+
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          if (actions[i].keysToRemove[j] in modifiedState) {
+            delete modifiedState[actions[i].keysToRemove[j]];
+          }
+        }
+    }
+  }
+
+  return modifiedState;
 }
 
 module.exports = transformState;
