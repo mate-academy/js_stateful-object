a04f952787e7ccfabadc862283e47e72314b0733
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-02-03 19:26:14 +0200

    Moroz v0.1

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const item of actions) {
+    switch (item.type) {
+      case 'addProperties':
+        Object.assign(state, item.extraData);
+        break;
+      case 'removeProperties':
+        for (const iterator of item.keysToRemove) {
+          delete state[iterator];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
