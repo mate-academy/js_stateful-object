d2e46eaa23857114a11671700888ded0ff9d9c24
Dmytro Pelovych <falcon.patron@gmail.com>
2022-02-08 16:46:38 +0200

    solution for task #transformState

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,33 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
+/*
+const state = {
+  foo: 'bar', name: 'Jim', another: 'one',
+};
+
+transformState(state, [
+  { type: 'clear' },
+]);
+
+console.log(state)
+*/
+
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      Object.keys(state).forEach(key => delete state[key]);
+    } else if (action.type === 'removeProperties') {
+      for (const K of Object.values(action.keysToRemove)) {
+        delete state[K];
+      }
+    } else {
+      for (const [K, V] of Object.entries(action.extraData)) {
+        state[K] = V;
+      }
+    }
+  }
 }
 
 module.exports = transformState;
