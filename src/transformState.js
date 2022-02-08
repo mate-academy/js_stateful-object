04aee8f21c108a7dae3670ae12bf4e90c7d5e344
Dmytro Pelovych <falcon.patron@gmail.com>
2022-02-08 16:58:59 +0200

    redundant comments removed

--- src/transformState.js
+++ src/transformState.js
@@ -4,19 +4,6 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-
-/*
-const state = {
-  foo: 'bar', name: 'Jim', another: 'one',
-};
-
-transformState(state, [
-  { type: 'clear' },
-]);
-
-console.log(state)
-*/
-
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
