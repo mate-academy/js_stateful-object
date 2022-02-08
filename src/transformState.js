2197085ae9edf81feb475d823b0827b5cbf28e43
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-08 14:34:12 +0200

    transforming object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      const arr = action.keysToRemove;
+
+      for (const key of arr) {
+        delete state[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
