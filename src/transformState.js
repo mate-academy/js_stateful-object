b65d195d20e4ce04abd5d05b8c56d41a32ab4390
Max Shlikhta <maxok.vibe@gmail.com>
2022-02-09 15:59:57 +0200

    Transform

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,28 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const prop of actions) {
+    switch (prop['type']) {
+      case 'addProperties':
+        Object.assign(state, prop['extraData']);
+        break;
+
+      case 'removeProperties':
+        for (const key of prop['keysToRemove']) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const char in state) {
+          delete state[char];
+        }
+        break;
+
+      default:
+        return null;
+    }
+  }
 }
 
 module.exports = transformState;
