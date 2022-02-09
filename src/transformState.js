eba47083daeba01d1508a11a11e45846a661a365
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-02-09 16:08:32 +0200

    rewrote on switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,24 +6,23 @@
  */
 function transformState(state, actions) {
   for (const elem of actions) {
-    if (elem.type === 'addProperties') {
-      for (const keys in elem.extraData) {
-        state[keys] = elem.extraData[keys];
-      }
-    }
+    switch (elem.type) {
+      case 'addProperties':
+        for (const keys in elem.extraData) {
+          state[keys] = elem.extraData[keys];
+        }
+        break;
+      case 'removeProperties':
+        for (const elem1 of elem.keysToRemove) {
+          delete state[elem1];
+        }
+        break;
 
-    if (elem.type === 'removeProperties') {
-      for (const elem1 of elem.keysToRemove) {
-        delete state[elem1];
-      }
-    }
-
-    if (elem.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 }
-
 module.exports = transformState;
