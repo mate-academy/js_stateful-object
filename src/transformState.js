e59299f36bf281d31bdc84403c7bc406d75fabc9
Danylo Kotliarenko <kotlyar099@gmail.com>
2022-02-10 20:29:22 +0200

    function action was created

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (const change of actions) {
+    switch (change.type) {
+      case 'addProperties':
+        Object.assign(state, change.extraData);
+        break;
 
+      case 'clear':
+        for (const action in state) {
+          delete state[action];
+        }
+        break;
+
+      case 'removeProperties':
+
+        for (const action of change.keysToRemove) {
+          delete state[action];
+        }
+        break;
+    }
+  }
+}
 module.exports = transformState;
