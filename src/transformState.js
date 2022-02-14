8cd4ec6dc5d1d2e81292b265758cffe1534afa69
Yevhen Veselov <veselovyevhen@gmail.com>
2022-02-14 12:10:24 +0200

    1st commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const param of actions) {
+    if (param['type'] === 'addProperties') {
+      Object.assign(state, param['extraData']);
+    }
+
+    if (param['type'] === 'removeProperties') {
+      for (const i of param['keysToRemove']) {
+        delete state[i];
+      }
+    }
+
+    if (param['type'] === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
