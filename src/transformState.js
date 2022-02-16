2edb829350cfff11a611bef8a7b0bc2e2edb1c38
Alex Nastychenko <nastychenko1993@gmail.com>
2022-02-16 11:23:59 +0200

    fix remark

--- src/transformState.js
+++ src/transformState.js
@@ -3,42 +3,30 @@
 /**
  * @param {Object} state
  * @param {Object[]} actions
- * transformState(state, [
-   {
-     type: 'addProperties',
-     extraData: {
-       name: 'Jim',
-       hello: 'world',
-     }
-   },
-   {
-     type: 'removeProperties',
-     keysToRemove: ['bar', 'hello'],
-   },
-   {
-     type: 'addProperties',
-     extraData: { another: 'one' },
-   }
- ])
  */
 function transformState(state, actions) {
   for (const obj of actions) {
-    if (obj.type === 'removeProperties') {
-      for (const prop of obj.keysToRemove) {
-        delete state[prop];
-      }
-    }
+    switch (obj.type) {
+      case 'addProperties':
+        for (const [prop, value] of Object.entries(obj.extraData)) {
+          state[prop] = value;
+        }
 
-    if (obj.type === 'addProperties') {
-      for (const [prop, value] of Object.entries(obj.extraData)) {
-        state[prop] = value;
-      }
-    }
+        return state;
 
-    if (obj.type === 'clear') {
-      for (const prop in state) {
-        delete state[prop];
-      }
+      case 'removeProperties':
+        for (const prop of obj.keysToRemove) {
+          delete state[prop];
+        };
+
+        return state;
+
+      case 'clear':
+        for (const prop in state) {
+          delete state[prop];
+        }
+
+        return state;
     }
   }
 }
