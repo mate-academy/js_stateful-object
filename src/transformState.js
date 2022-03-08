3ae4fef8e8ce409c36513e4ba1f7e6e3b299f316
Max Shylenko <maxshylenko@gmail.com>
2022-03-08 22:01:32 +0100

    js_inverse_robot is done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (true) {
+      case action.type === 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case action.type === 'removeProperties':
+        for (let i = 0; i < action['keysToRemove'].length; i++) {
+          delete state[action.keysToRemove[i]];
+        }
+        break;
+
+      case action.type === 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
