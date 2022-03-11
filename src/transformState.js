6a144f57f2dbc4148992fe9d2ad7221c43761c11
Roman Nikitenko <Lomkaaa@ukr.net>
2022-03-11 17:50:15 +0200

    Task complete

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.keys(action.extraData).forEach(dataKey => {
+          state[dataKey] = action.extraData[dataKey];
+        });
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(keyRemove => {
+          delete state[keyRemove];
+        });
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 module.exports = transformState;
