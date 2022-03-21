c728cfcc29c8a97108704fa24c047526da7d2dac
Dima <d.maik2178@gmail.com>
2022-03-21 14:07:54 +0200

    First commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+
+  for (const key of actions) {
+
+    switch (key.type) {
+      
+      case 'clear': {
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+        break;
+      }
+
+      case 'addProperties': {
+        Object.assign(state, key.extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        key.keysToRemove.forEach(element => {
+          delete state[element];
+        });
+        break;
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
