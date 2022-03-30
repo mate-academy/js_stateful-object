583b63e76ba673ba7de40709a13dcac09bda0742
unknown <avicostov2@gmail.com>
2022-03-30 15:40:11 +0200

    solut

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,27 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const i of actions) {
+    if (i.type === 'addProperties') {
+      for (const b in i.extraData) {
+        state[b] = i.extraData[b];
+      }
+    }
+
+    if (i.type === 'removeProperties') {
+      for (const b of i.keysToRemove) {
+        delete state[b];
+      }
+    }
+
+    if (i.type === 'clear') {
+      for (const b in state) {
+        delete state[b];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
