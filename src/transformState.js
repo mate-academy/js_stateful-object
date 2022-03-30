b637b0f19460d9ffdc4e2f577ccff8864ffbf843
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-03-30 12:46:57 +0300

    transformState function was created

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const item of actions) {
+    if (item.type === 'addProperties') {
+      const { extraData } = item;
+
+      Object.assign(state, extraData);
+    } else if (item.type === 'removeProperties') {
+      const { keysToRemove } = item;
+
+      for (const key in state) {
+        if (keysToRemove.includes(key)) {
+          delete state[key];
+        }
+      }
+    } else if (item.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
