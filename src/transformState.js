37a072be6a5e21b483351475f6fa613a38e1cbc9
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-03-30 20:27:24 +0300

    Switch/case was used

--- src/transformState.js
+++ src/transformState.js
@@ -5,27 +5,37 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const item of actions) {
-    if (item.type === 'addProperties') {
-      const { extraData } = item;
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        const { extraData } = action;
 
-      Object.assign(state, extraData);
-    } else if (item.type === 'removeProperties') {
-      const { keysToRemove } = item;
+        Object.assign(state, extraData);
 
-      for (const key in state) {
-        if (keysToRemove.includes(key)) {
+        break;
+      }
+
+      case 'removeProperties': {
+        const { keysToRemove } = action;
+
+        for (const key in state) {
+          if (keysToRemove.includes(key)) {
+            delete state[key];
+          }
+        }
+
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
           delete state[key];
         }
-      }
-    } else if (item.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
+
+        break;
       }
     }
   }
-
-  return state;
 }
 
 module.exports = transformState;
