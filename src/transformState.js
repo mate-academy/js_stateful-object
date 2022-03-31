7883b6dbf7ec4b4aec16baa20e75ebc46c7d42c9
Helen Petrovska <paznachka@gmail.com>
2022-03-31 16:56:27 +0300

    default case was changed

--- src/transformState.js
+++ src/transformState.js
@@ -18,10 +18,13 @@ function transformState(state, actions) {
         };
         break;
 
-      default:
+      case 'clear':
         for (const key in state) {
           delete state[key];
         };
+        break;
+
+      default:
         break;
     }
   }
