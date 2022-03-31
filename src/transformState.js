ffbb11c7b25876d589dbba4bd87fc78a5ac12b5d
Serhii Naumenko <s.naumenko.miy@gmail.com>
2022-03-31 16:20:46 +0300

    first variant

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  let keys = [];
+  let result = state;
+
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        result = Object.assign(result, act.extraData);
+        break;
+
+      case 'removeProperties':
+        keys = act.keysToRemove;
+
+        for (const key of keys) {
+          delete result[key];
+        }
+        break;
+
+      default: for (const st in state) {
+        delete result[st];
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformState;
