12f61368cd08bdf7457670108c371b4bc9ff24bb
SeKaM0 <polozhentsew02@gmail.com>
2022-03-31 19:50:20 +0300

    Finish task

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(state, actions[i].extraData);
+    }
 
+    if (actions[i].type === 'removeProperties') {
+      for (const key in state) {
+        if (actions[i].keysToRemove.includes(key)) {
+          delete state[key];
+        }
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
+}
 module.exports = transformState;
