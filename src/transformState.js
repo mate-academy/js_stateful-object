40e09de48378a63d1e69886273ba909690aa7d6e
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-03-31 16:19:58 +0300

    add task

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const a of actions) {
+    switch (a.type) {
+      case 'addProperties':
+        Object.assign(state, a.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const m of a.keysToRemove) {
+          delete state[m];
+        };
+        break;
+
+      default:
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
