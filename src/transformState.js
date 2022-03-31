108fc41dc76ca37d8a0549179f5e6afc206ea827
Dima Kotovich <kotovih950@gmail.com>
2022-04-01 01:18:19 +0300

    finish

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,24 @@
  */
 function transformState(state, actions) {
   // write code here
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      for (const key in actions[i].extraData) {
+        state[key] = actions[i].extraData[key];
+      }
+    } else if (actions[i].type === 'removeProperties') {
+      for (const remove in actions[i].keysToRemove) {
+        delete state[actions[i].keysToRemove[remove]];
+      }
+    } else if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
