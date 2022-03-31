71bed20a61176925ba4e1b5790d230381889e030
Vlad Skoromnyi <VectraSton@gmail.com>
2022-03-31 16:29:06 +0300

    remake with switch case

--- src/transformState.js
+++ src/transformState.js
@@ -6,18 +6,24 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const key in actions[i].extraData) {
-        state[key] = actions[i].extraData[key];
-      }
-    } else if (actions[i].type === 'removeProperties') {
-      for (const key of actions[i].keysToRemove) {
-        delete state[key];
-      }
-    } else if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (true) {
+      case actions[i].type === 'addProperties':
+        for (const key in actions[i].extraData) {
+          state[key] = actions[i].extraData[key];
+        }
+        break;
+
+      case actions[i].type === 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case actions[i].type === 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
