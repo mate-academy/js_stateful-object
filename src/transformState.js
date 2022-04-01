f923f79a1d3d7ee6579b775f2941859bf6ee1e6a
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-04-01 14:38:38 +0200

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,37 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const actionType = action.type;
+    const actionExtraDate = action.extraData;
+
+    switch (actionType) {
+      case 'addProperties': {
+        Object.assign(state, actionExtraDate);
+        break;
+      }
+
+      case 'removeProperties': {
+        for (let index = 0; index < action.keysToRemove.length; index++) {
+          delete state[action.keysToRemove[index]];
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      }
+
+      default: {
+        throw new Error(`Unsopported action type: ${actionType}`);
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
