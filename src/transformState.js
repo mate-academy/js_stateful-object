223f5b4e1777c6da51398cc47effae478a159808
Max Nikiforov <maxxnikiforov@gmail.com>
2022-04-01 19:22:44 +0700

    switch was added

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,25 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i]['type'] === 'addProperties') {
-      Object.assign(state, actions[i]['extraData']);
-    }
+    switch (actions[i]['type']) {
+      case 'addProperties':
+        Object.assign(state, actions[i]['extraData']);
+        break;
 
-    if (actions[i]['type'] === 'removeProperties') {
-      for (let x = 0; x < actions[i]['keysToRemove'].length; x++) {
-        delete state[actions[i]['keysToRemove'][x]];
-      }
-    }
+      case 'removeProperties':
+        for (let x = 0; x < actions[i]['keysToRemove'].length; x++) {
+          delete state[actions[i]['keysToRemove'][x]];
+        }
+        break;
 
-    if (actions[i]['type'] === `clear`) {
-      for (const key in state) {
-        delete state[key];
-      }
+      case `clear`:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        continue;
     }
   }
 
