ccefa7d373707e19ad0e72f014da82145b809421
Kostiantyn <havryshkostya@gmail.com>
2022-04-04 17:10:58 +0400

    switch as you have asked

--- src/transformState.js
+++ src/transformState.js
@@ -5,26 +5,48 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  // for (const act of actions) {
+  //   if (act.type === 'clear') {
+  //     for (const props in state) {
+  //       delete state[props];
+  //     }
+  //   }
+
+  //   if (act.type === 'addProperties') {
+  //     Object.assign(state, act.extraData);
+  //   }
+
+  //   if (act.type === 'removeProperties') {
+  //     for (const removeItem of act.keysToRemove) {
+  //       for (const item in state) {
+  //         if (removeItem === item) {
+  //           delete state[item];
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   for (const act of actions) {
-    if (act.type === 'clear') {
-      for (const props in state) {
-        delete state[props];
-      }
-    }
+    switch (act.type) {
+      case 'clear':
+        for (const props in state) {
+          delete state[props];
+        }
+        break;
 
-    if (act.type === 'addProperties') {
-      Object.assign(state, act.extraData);
-    }
+      case 'addProperties' :
+        Object.assign(state, act.extraData);
+        break;
 
-    if (act.type === 'removeProperties') {
-      for (const removeItem of act.keysToRemove) {
-        for (const item in state) {
-          if (removeItem === item) {
-            delete state[item];
+      case 'removeProperties' :
+        for (const removeItem of act.keysToRemove) {
+          for (const item in state) {
+            if (removeItem === item) {
+              delete state[item];
+            }
           }
         }
-      }
     }
   }
 
