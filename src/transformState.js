af16afb26ae30a7b508b466cfe3c72d630b2a432
Александр Погребняк <pogrebnyak19999@gmail.com>
2022-04-04 15:02:40 +0300

    Remove unuseful loop

--- src/transformState.js
+++ src/transformState.js
@@ -6,26 +6,24 @@
  */
 function transformState(state, actions) {
   for (const item in actions) {
-    for (const keys in actions[item]) {
-      switch (actions[item][keys]) {
-        case 'addProperties':
-          for (const data in actions[item].extraData) {
-            state[data] = actions[item].extraData[data];
-          }
-          break;
+    switch (actions[item].type) {
+      case 'addProperties':
+        for (const data in actions[item].extraData) {
+          state[data] = actions[item].extraData[data];
+        }
+        break;
 
-        case 'removeProperties':
-          for (const remove of actions[item].keysToRemove) {
-            delete state[remove];
-          }
-          break;
+      case 'removeProperties':
+        for (const remove of actions[item].keysToRemove) {
+          delete state[remove];
+        }
+        break;
 
-        case 'clear':
-          for (const clear in state) {
-            delete state[clear];
-          }
-          break;
-      }
+      case 'clear':
+        for (const clear in state) {
+          delete state[clear];
+        }
+        break;
     }
   }
 }
