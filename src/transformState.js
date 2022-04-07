a8e51bed764e8fb5068dc51f4075139055486c0b
Andrii Rutkovskii <greateman94@gmail.com>
2022-04-07 17:41:53 +0300

    rewrite without extrainclouding in array

--- src/transformState.js
+++ src/transformState.js
@@ -4,25 +4,33 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, ...actions) {
+function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    for (const char of actions[i]) {
-      if (char.type === 'addProperties') {
-        for (const key in char.extraData) {
-          state[key] = char.extraData[key];
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          state[key] = actions[i].extraData[key];
         }
-      } else if (char.type === 'removeProperties') {
-        for (const key of char.keysToRemove) {
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
           if (!state[key]) {
             continue;
           }
+
           delete state[key];
         }
-      } else if (char.type === 'clear' && Object.keys(state).length !== 0) {
-        for (const keyOfState in state) {
-          delete state[`${keyOfState}`];
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
         }
-      }
+        break;
+
+      default:
+        break;
     }
   }
 }
