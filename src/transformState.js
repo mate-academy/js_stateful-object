fb7509e54de2fc9c9f36ced87e0fbcd0397d4eea
Andrei Sucman <sucmanandrei@gmail.com>
2022-04-20 21:35:44 +0300

    task completed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i]['type'] === 'addProperties') {
+      Object.assign(state, actions[i]['extraData']);
+    } else if (actions[i]['type'] === 'removeProperties') {
+      for (const property in actions[i]['keysToRemove']) {
+        delete state[actions[i]['keysToRemove'][property]];
+      }
+    } else if (actions[i]['type'] === 'clear') {
+      for (const property in state) {
+        delete state[property];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
