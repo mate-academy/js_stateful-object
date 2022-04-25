df98e95778f2db31293de7aba775e39e056e5c84
Vitalii-Heiko <papus@ukr.net.com>
2022-04-25 15:34:39 +0300

    chenged solution

--- src/transformState.js
+++ src/transformState.js
@@ -7,21 +7,39 @@
 function transformState(state, actions) {
   // write code here
   for (const i in actions) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
 
-    if (actions[i].type === 'removeProperties') {
-      for (const key of actions[i].keysToRemove) {
-        delete state[key];
-      }
-    }
+    // if (actions[i].type === 'addProperties') {
+    //   Object.assign(state, actions[i].extraData);
+    // }
 
-    if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
+    // if (actions[i].type === 'removeProperties') {
+    //   for (const key of actions[i].keysToRemove) {
+    //     delete state[key];
+    //   }
+    // }
+
+    // if (actions[i].type === 'clear') {
+    //   for (const key in state) {
+    //     delete state[key];
+    //   }
+    // }
   }
 }
 
