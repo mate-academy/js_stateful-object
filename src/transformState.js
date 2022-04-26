c28dfe7468979146811acabacaa9d02affc19c4d
Andriyk0 <andriy22221@gmail.com>
2022-04-26 19:28:23 +0300

    create transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(state, key.extraData);
+    } else if (key.type === 'removeProperties') {
+      for (const j of key.keysToRemove) {
+        delete state[j];
+      }
+    } else if (key.type === 'clear') {
+      for (const k in state) {
+        delete state[k];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
