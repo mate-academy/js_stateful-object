54c2ec8164d2b6d91558ec3035469824ac9293ec
Hovera-Liubomyr <ljusuk@gmail.com>
2022-04-26 12:53:37 +0300

    stateful_object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const x of actions) {
+    if (x.type === 'addProperties') {
+      Object.assign(state, x.extraData);
+    }
+
+    if (x.type === 'removeProperties') {
+      for (const y of x.keysToRemove) {
+        delete state[y];
+      }
+    }
+
+    if (x.type === 'clear') {
+      for (const k in state) {
+        delete state[k];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
