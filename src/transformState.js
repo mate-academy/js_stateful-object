915f76d8d0a7dfac556964e0f2d83a087835c0cf
Andrii Rutkovskii <greateman94@gmail.com>
2022-04-26 18:28:49 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -8,17 +8,11 @@ function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     switch (actions[i].type) {
       case 'addProperties':
-        for (const key in actions[i].extraData) {
-          state[key] = actions[i].extraData[key];
-        }
+        Object.assign(state, actions[i].extraData);
         break;
 
       case 'removeProperties':
         for (const key of actions[i].keysToRemove) {
-          if (!state[key]) {
-            continue;
-          }
-
           delete state[key];
         }
         break;
