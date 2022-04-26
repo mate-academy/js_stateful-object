26123d9e4588142bb8cca9daf74571f6982e6637
elkhayate <melkhayate61@gmail.com>
2022-04-26 02:38:22 +0000

    project init

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,28 @@
  */
 function transformState(state, actions) {
   // write code here
+  let output = {};
+
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        output = Object.assign({}, state, actions[i].extraData);
+        break;
+
+      case 'clear':
+        output = {};
+        break;
+
+      case 'removeProperties':
+        for (const key in state) {
+          if (actions[i].keysToRemove.indexOf(key) === -1) {
+            output[key] = state[key];
+          }
+        }
+    }
+  }
+
+  return output;
 }
 
 module.exports = transformState;
