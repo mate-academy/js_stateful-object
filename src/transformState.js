a87da9cf38ca49c1996c00b9ee3c54eb92fe7650
MabuzaM <mabuzamfano@gmail.com>
2022-04-27 00:53:15 +0200

    Added logic to add and remove properties from the object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const data in action.extradata) {
+        state[data] = data;
+      }
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
