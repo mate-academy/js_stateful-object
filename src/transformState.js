9a9ceece021c118136f3537e2a92b6c65057ca04
MabuzaM <mabuzamfano@gmail.com>
2022-04-27 01:09:04 +0200

    Added logic to delete all state object properties

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  const stateLink = state;
+
   for (const action of actions) {
     if (action.type === 'addProperties') {
       for (const data in action.extradata) {
-        state[data] = data;
+        stateLink[data] = data;
       }
     } else if (action.type === 'removeProperties') {
       for (const key of action.keysToRemove) {
-        delete state[key];
+        delete stateLink[key];
+      }
+    } else if (action.type === 'clear') {
+      for (const prop in stateLink) {
+        delete stateLink[prop];
       }
     }
   }
 
-  return state;
+  return stateLink;
 }
 
 module.exports = transformState;
