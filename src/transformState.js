63b2c1280a30472f519549d3c72456138061c3af
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-04-26 20:51:28 +0300

    replaced if conditions with switch

--- src/transformState.js
+++ src/transformState.js
@@ -1,23 +1,28 @@
 'use strict';
 
 /**
- * @param {Object} state
- * @param {Object[]} actions
+ * @param {action} state
+ * @param {action[]} actions
  */
 function transformState(state, actions) {
-  for (const object of actions) {
-    if (object.type === 'addProperties') {
-      for (const key in object.extraData) {
-        state[key] = object.extraData[key];
-      }
-    } else if (object.type === 'removeProperties') {
-      for (const item of object.keysToRemove) {
-        delete state[item];
-      }
-    } else if (object.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          state[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 }
