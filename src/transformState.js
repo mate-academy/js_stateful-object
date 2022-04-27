9f27b2ebec43f09d77ccd0091e9091171b6bd9c0
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-04-27 16:39:47 +0300

    add switch case

--- src/transformState.js
+++ src/transformState.js
@@ -8,16 +8,22 @@ function transformState(state, actions) {
   // write code here
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      for (const del in state) {
-        delete state[del];
-      }
-    } else if (actions[i].type === 'removeProperties') {
-      for (let y = 0; y < actions[i].keysToRemove.length; y++) {
-        delete state[actions[i].keysToRemove[y]];
-      }
-    } else if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
+    switch (actions[i].type) {
+      case 'removeProperties':
+        for (let y = 0; y < actions[i].keysToRemove.length; y++) {
+          delete state[actions[i].keysToRemove[y]];
+        }
+        break;
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+      case 'clear':
+        for (const del in state) {
+          delete state[del];
+        }
+        break;
+      default:
+        break;
     }
   }
 
