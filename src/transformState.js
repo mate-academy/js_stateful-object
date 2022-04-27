b394e4b93c91a3b1b5ceac8dcac683a251d57ab6
Ihor Yeremenko <igoree86@gmail.com>
2022-04-27 09:43:03 +0300

    First_commit_TransformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+
+  for (const value of actions) {
+    if (value.type === 'addProperties') {
+      Object.assign(state, value.extraData);
+    }
+
+    if (value.type === 'removeProperties') {
+      for (const value2 of value.keysToRemove) {
+        if (state.hasOwnProperty(value2)) {
+          delete state[value2];
+        }
+      }
+    }
+
+    if (value.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
