3df66b39e4686783ab31faaf0fbf7a4ee22559dc
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-04-27 19:32:55 +0200

    make stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const act of action.keysToRemove) {
+        delete state[act];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const del in state) {
+        delete state[del];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
