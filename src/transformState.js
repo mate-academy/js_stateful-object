84934496bf3e0def1bd461cd66d9a270750fbbb9
Andriyk0 <andriy22221@gmail.com>
2022-04-27 15:16:51 +0300

    use switch case

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,27 @@
  */
 function transformState(state, actions) {
   for (const key of actions) {
-    if (key.type === 'addProperties') {
-      Object.assign(state, key.extraData);
-    } else if (key.type === 'removeProperties') {
-      for (const j of key.keysToRemove) {
-        delete state[j];
-      }
-    } else if (key.type === 'clear') {
-      for (const k in state) {
-        delete state[k];
-      }
+    switch (key.type) {
+      case 'addProperties' :
+
+        Object.assign(state, key.extraData);
+        break;
+
+      case 'removeProperties' :
+
+        for (const j of key.keysToRemove) {
+          delete state[j];
+        }
+        break;
+
+      case 'clear' :
+
+        for (const k in state) {
+          delete state[k];
+        }
     }
   }
 
   return state;
 }
-
 module.exports = transformState;
