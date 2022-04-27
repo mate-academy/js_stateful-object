77cb661be884ea9c91dece752065bc7190a2933a
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-04-27 19:41:09 +0300

    solution to switch case

--- src/transformState.js
+++ src/transformState.js
@@ -7,22 +7,23 @@
 function transformState(state, actions) {
   // write code here
   for (const obj of actions) {
-    if (obj.type === 'removeProperties') {
-      for (const key1 of obj.keysToRemove) {
-        delete state[key1];
-      }
-    }
+    switch (obj.type) {
+      case 'removeProperties':
+        for (const key1 of obj.keysToRemove) {
+          delete state[key1];
+        }
+        break;
 
-    if (obj.type === 'addProperties') {
-      for (const key2 in obj.extraData) {
-        state[key2] = obj.extraData[key2];
-      }
-    }
+      case 'addProperties':
+        for (const key2 in obj.extraData) {
+          state[key2] = obj.extraData[key2];
+        }
+        break;
 
-    if (obj.type === 'clear') {
-      for (const key3 in state) {
-        delete state[key3];
-      }
+      case 'clear':
+        for (const key3 in state) {
+          delete state[key3];
+        }
     }
   }
 }
