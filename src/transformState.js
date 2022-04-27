8daa3f9076320f32815b818a9a6ea9949fdf7c53
Wojtek <wojciech.cz.bor@gmail.com>
2022-04-27 21:56:19 +0200

    object-illiterate

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,31 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const element in actions) {
+    const { type, extraData, keysToRemove } = element;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+    }
+
+    switch (type) {
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete state[key];
+        }
+        break;
+    }
+
+    switch (type) {
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+    }
+    break;
+  }
+
+  return state;
 }
 
 module.exports = transformState;
