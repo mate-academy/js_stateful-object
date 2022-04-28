eea1c4f65381315e50480cc33d6b8e46fc8132ba
Daryna Chekmariova <dashachekmariova@gmail.com>
2022-04-28 13:17:11 +0300

    stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      const copyExtra = { ...obj.extraData };
+
+      for (const prop in copyExtra) {
+        state[prop] = copyExtra[prop];
+      }
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const removeArr of obj.keysToRemove) {
+        delete state[removeArr];
+      }
+    }
+
+    if (obj.type === 'clear') {
+      for (const prop in state) {
+        delete state[prop];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
