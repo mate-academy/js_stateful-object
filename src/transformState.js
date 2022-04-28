1ed4cb0d4179b188ae0fec607d27fd6a12752fc7
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2022-04-28 16:46:22 +0300

    transformState first rewue

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,31 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  if (actions.length === 0) {
+    return state;
+  }
+
+  for (const property of actions) {
+    if (property.type === 'removeProperties') {
+      for (const action of property.keysToRemove) {
+        delete state[action];
+      }
+    }
+
+    if (property.type === 'addProperties') {
+      Object.assign(state, property.extraData);
+    }
+
+    if (property.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return (state);
 }
 
 module.exports = transformState;
