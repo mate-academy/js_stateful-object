1e81ec4416869586c7f2fa4b3dbecb3b191de407
Daria Liubymova <dellachaise@gmail.com>
2022-04-28 17:18:09 +0300

    Solved stateful object task

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const currentAction of actions) {
+    if (currentAction.type === 'addProperties') {
+      Object.assign(state, currentAction['extraData']);
+    }
+
+    if (currentAction.type === 'removeProperties') {
+      const listToDelete = currentAction['keysToRemove'];
+
+      for (let i = 0; i < listToDelete.length; i++) {
+        const currentDeleteKey = listToDelete[i];
+
+        if (state.hasOwnProperty(currentDeleteKey)) {
+          delete state[currentDeleteKey];
+        }
+      }
+    }
+
+    if (currentAction.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
