b66e9b577db5b5f782bfd3a3009621dd8a432f6a
Server <isr.lviv2021@gmail.com>
2022-04-29 17:51:03 +0300

    transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  if (actions.length === 0) {
+    return state;
+  }
+
+  for (const keys of actions) {
+    if (keys.type === 'removeProperties') {
+      for (const action of keys.keysToRemove) {
+        delete state[action];
+      }
+    } else if (keys.type === 'addProperties') {
+      Object.assign(state, keys.extraData);
+    } else if (keys.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
