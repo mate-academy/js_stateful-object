4a9e6cc3c2c3887b123d36909924424f6a31d74f
PRO-GRAM-MER <ankitbaage3@gmail.com>
2022-04-29 15:23:08 +0530

    changes done

--- src/transformState.js
+++ src/transformState.js
@@ -1,22 +1,27 @@
 'use strict';
 
 function transformState(state, actions) {
-  // write code here
-  for (const action in actions) {
-    if (actions[action]['type'] === 'addProperties') {
-      Object.assign(state, actions[action]['extraData']);
-    } else if (actions[action]['type'] === 'removeProperties') {
-      for (const key in actions[action]['keysToRemove']) {
-        delete state[actions[action]['keysToRemove'][key]];
-      }
-    } else if (actions[action]['type'] === 'clear') {
-      for (const property in state) {
-        delete state[property];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
     }
   }
-
-  return state;
 }
 
 module.exports = transformState;
