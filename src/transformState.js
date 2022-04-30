419666db4eac9a9a77fc5465c702c416524617e2
Andriy Simyachenko <simyachenko@gmail.com>
2022-04-30 17:08:50 +0300

    commit message

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const toDelete of action.keysToRemove) {
+          delete state[toDelete];
+        };
+        break;
+
+      case 'clear':
+        for (const entries in state) {
+          delete state[entries];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
