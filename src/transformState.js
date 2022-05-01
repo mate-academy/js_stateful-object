ccc35862315d7740a87d7bf6ba4956fa36250633
Kamil Witek <kamilprogramuje@gmail.com>
2022-05-01 14:59:11 +0200

    adding properties and remove properties works fine

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,26 @@
  */
 function transformState(state, actions) {
   // write code here
+  // console.log(actions);
+  // console.log(state);
+
+  for (const action of actions) {
+    // add properties
+    if (action.type === 'addProperties') {
+      const keysToAdd = Object.keys(action.extraData);
+
+      for (const keyToAdd of keysToAdd) {
+        state[keyToAdd] = action.extraData[keyToAdd];
+      }
+    }
+
+    // remove properties
+    if (action.type === 'removeProperties') {
+      for (const keyToRemove of action.keysToRemove) {
+        delete state[keyToRemove];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
