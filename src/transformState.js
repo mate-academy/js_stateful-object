74c459748366e848ecd85b25d0f9d37b95ff668b
Kamil Witek <kamilprogramuje@gmail.com>
2022-05-03 11:12:02 +0200

    if changed to switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,25 +6,24 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    // add
-    if (action.type === 'addProperties') {
-      for (const keyToAdd in action.extraData) {
-        state[keyToAdd] = action.extraData[keyToAdd];
-      }
-    }
-
-    // remove
-    if (action.type === 'removeProperties') {
-      for (const keyToRemove of action.keysToRemove) {
-        delete state[keyToRemove];
-      }
-    }
-
-    // clear
-    if (action.type === 'clear') {
-      for (const stateKey in state) {
-        delete state[stateKey];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        for (const keyToAdd in action.extraData) {
+          state[keyToAdd] = action.extraData[keyToAdd];
+        }
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete state[keyToRemove];
+        }
+        break;
+      case 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+        break;
+      default:
+        throw new Error('wrong input');
     }
   }
 }
