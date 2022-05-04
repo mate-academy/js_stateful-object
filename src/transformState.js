84e7a347b24ff13eede4b41495f9591800d871aa
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-05-04 22:35:11 +0300

    Solution /  First commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const order of actions) {
+    switch (true) {
+      case order.type === 'addProperties':
+        Object.assign(state, order.extraData);
+        break;
+
+      case order.type === 'removeProperties':
+        for (const toBeRemoved of order.keysToRemove) {
+          if (toBeRemoved in state) {
+            delete state[toBeRemoved];
+          }
+        }
+        break;
+
+      default:
+        for (const eraseAll in state) {
+          delete state[eraseAll];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
