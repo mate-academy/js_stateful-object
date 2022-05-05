b80d840a25b76a37f2a23455db7d35cde4a6461a
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-05-05 12:51:28 +0300

    transformFirstCommit

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,29 @@
  */
 function transformState(state, actions) {
   // write code here
+  let actionsObject = {};
+  let value = '';
+
+  for (const ch of actions) {
+    actionsObject = ch;
+    Object.assign(state, actionsObject.extraData);
+
+    for (const key in actionsObject.keysToRemove) {
+      value = actionsObject.keysToRemove[key];
+
+      for (const key2 in state) {
+        if (value === key2) {
+          delete state[key2];
+        }
+      }
+    }
+
+    for (const key3 in state) {
+      if (actionsObject.type === 'clear') {
+        delete state[key3];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
