5d2fe3982b879e4bb10a765ca3c8fbcfdebe2e8f
Roman Marunkevich <vinnibea@gmail.com>
2022-05-09 01:22:12 +0300

    solution. I hate braces

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      for (const prop in actions[i].extraData) {
+        state[prop] = actions[i].extraData[prop];
+      }
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (const word in actions[i].keysToRemove) {
+        delete state[actions[i].keysToRemove[word]];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
