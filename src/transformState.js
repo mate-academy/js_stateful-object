91389e4948b0d8e4a046c88d2ad0be1ca7f44704
KostyaKonev <kosgoh.214@gmail.com>
2022-05-12 15:46:31 +0300

    fff

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,16 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    switch (action['type']) {
+    switch (action.type) {
       case 'addProperties':
-        for (const prop in action['extraData']) {
-          state[prop] = action['extraData'][prop];
+        for (const prop in action.extraData) {
+          state[prop] = action.extraData[prop];
         }
         break;
 
       case 'removeProperties':
-        for (const key of action['keysToRemove']) {
-          for (const prop in state) {
-            if (key === prop) {
-              delete state[prop];
-            }
-          }
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
         break;
 
