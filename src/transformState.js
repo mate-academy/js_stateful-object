2138739da8bfd06fb72fd0f2566b1eb0b52e7418
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-16 14:57:30 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,51 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    for (const key in actions[i]) {
+      /*  CLEAR  */
+
+      if (actions[i][key] === 'clear') {
+        for (const keyState in state) {
+          delete state[keyState];
+        }
+      }
+
+      /*   REMOVE  */
+
+      if (actions[i][key] === 'removeProperties') {
+        const values = [];
+
+        for (const keyAct in actions[i]) {
+          values.push(actions[i][keyAct]);
+        }
+
+        for (let j = 0; j < values[1].length; j++) {
+          for (const keyState in state) {
+            if (keyState === values[1][j]) {
+              delete state[keyState];
+            }
+          }
+        }
+      }
+
+      /*  ADD  */
+
+      if (actions[i][key] === 'addProperties') {
+        const values = [];
+
+        for (const keyAct in actions[i]) {
+          values.push(actions[i][keyAct]);
+        }
+
+        for (const addKey in values[1]) {
+          state[addKey] = values[1][addKey];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
