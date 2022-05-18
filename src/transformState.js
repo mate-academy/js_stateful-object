cdd6ddb753d91080e0aa4514ac8aee62dceafdfd
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-18 17:46:18 +0300

    fix

--- src/transformState.js
+++ src/transformState.js
@@ -6,40 +6,21 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    for (const key in action) {
-      if (action[key] === 'clear') {
+    switch (action.type) {
+      case 'clear':
         for (const keyState in state) {
           delete state[keyState];
         }
-      }
+        break;
 
-      if (action[key] === 'removeProperties') {
-        const values = [];
-
-        for (const keyAct in action) {
-          values.push(action[keyAct]);
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
-
-        for (let j = 0; j < values[1].length; j++) {
-          for (const keyState in state) {
-            if (keyState === values[1][j]) {
-              delete state[keyState];
-            }
-          }
-        }
-      }
-
-      if (action[key] === 'addProperties') {
-        const values = [];
-
-        for (const keyAct in action) {
-          values.push(action[keyAct]);
-        }
-
-        for (const addKey in values[1]) {
-          state[addKey] = values[1][addKey];
-        }
-      }
+        break;
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
     }
   }
 
