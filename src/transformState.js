16e0c88da0454a2f75d69107fc1815d6e5116891
Artem Furdela <furdela.artem@gmail.com>
2022-05-21 17:54:13 +0300

    transformState first commit

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,30 @@
  */
 function transformState(state, actions) {
   // write code here
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+
+      continue;
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+
+      continue;
+    }
+
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+
+      continue;
+    }
+  }
 }
 
 module.exports = transformState;
