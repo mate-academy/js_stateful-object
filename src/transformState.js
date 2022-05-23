d6e476833e7a9b877deaa409ab5b6f1698ccfc83
Katia Zhurenko <87322698+Katia-zz@users.noreply.github.com>
2022-05-23 21:19:37 +0200

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,24 +6,49 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const prop in action.extraData) {
-        state[prop] = action.extraData[prop];
-      }
-    }
+    switch (true) {
+      case action.type === 'addProperties'
+        :
+        for (const prop in action.extraData) {
+          state[prop] = action.extraData[prop];
+        }
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const keys of action.keysToRemove) {
-        delete state[keys];
-      }
-    }
+      case action.type === 'removeProperties'
+        :
+        for (const keys of action.keysToRemove) {
+          delete state[keys];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const states in state) {
-        delete state[states];
-      }
+      case action.type === 'clear'
+        :
+        for (const states in state) {
+          delete state[states];
+        }
+        break;
     }
   }
 }
+//   for (const action of actions) {
+//     if (action.type === 'addProperties') {
+//       for (const prop in action.extraData) {
+//         state[prop] = action.extraData[prop];
+//       }
+//     }
+
+//     if (action.type === 'removeProperties') {
+//       for (const keys of action.keysToRemove) {
+//         delete state[keys];
+//       }
+//     }
+
+//     if (action.type === 'clear') {
+//       for (const states in state) {
+//         delete state[states];
+//       }
+//     }
+//   }
+// }
 
 module.exports = transformState;
