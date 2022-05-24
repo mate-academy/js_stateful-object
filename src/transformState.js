e03039a56d87b929f1fa28a54832cc2cedd6c204
Oleg Lukyanchuk <oleeluk@gmail.com>
2022-05-24 03:10:17 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let k = 0; k < actions.length; k++) {
+    if (actions[k].type === 'clear') {
+      for (const x in state) {
+        delete state[x];
+      }
+    }
+
+    if (actions[k].type === 'removeProperties') {
+      for (const ch of actions[k].keysToRemove) {
+        if (state[ch] !== undefined) {
+          delete state[ch];
+        }
+      }
+    }
+
+    for (const ch in actions[k].extraData) {
+      if (actions[k].type === 'addProperties') {
+        state[ch] = actions[k].extraData[ch];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
