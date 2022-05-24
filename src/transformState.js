88e2dcca2b64c7e736034a5622a1e041edde3c52
Artem Furdela <furdela.artem@gmail.com>
2022-05-24 22:17:22 +0300

    Transform state commit after changes

--- src/transformState.js
+++ src/transformState.js
@@ -8,26 +8,22 @@ function transformState(state, actions) {
   // write code here
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-      continue;
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-
-      continue;
-    }
-
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-
-      continue;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
