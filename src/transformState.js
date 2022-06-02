0569d129e1c7de812194d7935b16ab1a684b689c
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-06-02 12:46:43 +0300

    1-st commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      for (const prop in key.extraData) {
+        state[prop] = key.extraData[prop];
+      }
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const delKey of key.keysToRemove) {
+        delete state[delKey];
+      }
+    }
+
+    if (key.type === 'clear') {
+      for (const props in state) {
+        delete state[props];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
