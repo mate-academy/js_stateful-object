03f8662045b927252e6b3e74d9c499b75c4a1985
mira1zu <mira1zu733@gmail.com>
2022-06-04 16:07:38 +0200

    Fixed according to checklist

--- src/transformState.js
+++ src/transformState.js
@@ -5,29 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const obj of actions) {
-    switch (obj.type) {
+  for (const action of actions) {
+    const { type, extraData } = action;
+    let { keysToRemove } = action;
+
+    switch (type) {
       case 'addProperties': {
-        for (const [key, value] of Object.entries(obj.extraData)) {
+        for (const [key, value] of Object.entries(extraData)) {
           state[key] = value;
         }
 
         break;
       }
 
+      case 'clear':
+        keysToRemove = Object.keys(state);
+        // fallsthrough
+
       case 'removeProperties': {
-        for (const key of obj.keysToRemove) {
+        for (const key of keysToRemove) {
           delete state[key];
         }
 
         break;
       }
 
-      case 'clear': {
-        for (const key in state) {
-          delete state[key];
-        }
-
+      default: {
         break;
       }
     }
