aa706055691bc7aa51c24203a79a4f9d560527c1
Nikita Karbazin <karbazinnikita@gmail.com>
2022-06-07 15:02:56 +0300

    fix bugs

--- src/transformState.js
+++ src/transformState.js
@@ -5,21 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const key in actions[i].extraData) {
-        state[key] = actions[i].extraData[key];
-      }
-    } else if (actions[i].type === 'removeProperties') {
-      for (let x = 0; x < actions[i].keysToRemove.length; x++) {
-        if (actions[i].keysToRemove[x] in state) {
-          delete state[actions[i].keysToRemove[x]];
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          state[key] = action.extraData[key];
         }
-      }
-    } else if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+      case 'removeProperties':
+        for (const remove of action.keysToRemove) {
+          if (state.hasOwnProperty(remove)) {
+            delete state[remove];
+          }
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
