19d266f9a707b7b80ee499355a8d26a41091a2e7
Tanya Hryn <tanyagryn1984@gmail.com>
2022-06-07 20:28:20 +0300

    fixedWithSwitch

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,22 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const value of action.keysToRemove) {
-        if (state.hasOwnProperty(value)) {
+      case 'removeProperties':
+        for (const value of action.keysToRemove) {
           delete state[value];
         }
-      }
-    }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
