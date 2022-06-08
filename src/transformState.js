5072306cc6653066e75d7db2130267441c27f4f3
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-06-08 18:15:44 +0300

    fixes after mentor review

--- src/transformState.js
+++ src/transformState.js
@@ -5,33 +5,37 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  let result = ({}, state);
+  let newState = state;
 
-  for (const act of actions) {
-    if (act.type === 'addProperties') {
-      result = Object.assign(result, act.extraData);
-    }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        for (const key in newState) {
+          delete newState[key];
+        }
+        break;
 
-    if (act.type === 'clear') {
-      for (const st in result) {
-        delete result[st];
-      }
-    }
+      case 'addProperties':
+        newState = Object.assign(
+          newState,
+          action.extraData
+        );
+        break;
 
-    if (act.type === 'removeProperties') {
-      const removeElts = act.keysToRemove;
-
-      for (const st in result) {
-        for (const elem of removeElts) {
-          if (elem === st) {
-            delete result[st];
+      case 'removeProperties': {
+        for (const key of action.keysToRemove) {
+          if (Object.keys(newState).includes(key)) {
+            delete newState[key];
           }
         }
+
+        break;
       }
+
+      default:
+        break;
     }
   }
-
-  return result;
 }
 
 module.exports = transformState;
