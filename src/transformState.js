ddd42e8258bffd32366b61ad59e4b3c28af8de38
Maksym Stupin <64157342+k0nstrukt-or@users.noreply.github.com>
2022-06-08 08:47:51 +0300

    Update transformState.js

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const action of actions) {
-    switch (action.type) {
+  for (const action of actions) {                       //sort out an array with actions 
+    switch (action.type) {                              // pass to switch key with action
       case 'addProperties':
-        Object.assign(state, action.extraData);
+        Object.assign(state, action.extraData);         //rewrite the state array with additional properties 
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
+        for (const key of action.keysToRemove) {       //delete from the state array some properties
           delete state[key];
         }
         break;
 
-      case 'clear':
+      case 'clear':                                   //delete all properties from the state object
         for (const key in state) {
           delete state[key];
         }
