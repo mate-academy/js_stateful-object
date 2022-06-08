a9a2dc1376fcf2854ec6aea2d45c4a51c07ab8d8
Alina Melentieva <melenteva2906@gmail.com>
2022-06-08 11:47:29 +0300

    Solution after fixes

--- src/transformState.js
+++ src/transformState.js
@@ -6,18 +6,23 @@
  */
 function transformState(state, actions) {
   for (const args of actions) {
-    if (args.type === 'addProperties') {
-      Object.assign(state, args.extraData);
-    } else if (args.type === 'removeProperties') {
-      for (const keyToRemove of args.keysToRemove) {
-        if (state.hasOwnProperty(keyToRemove)) {
-          delete state[keyToRemove];
+    switch (args.type) {
+      case 'addProperties':
+        Object.assign(state, args.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of args.keysToRemove) {
+          if (state.hasOwnProperty(keyToRemove)) {
+            delete state[keyToRemove];
+          }
         }
-      }
-    } else if (args.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+
+      default:
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 
