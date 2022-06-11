294cc1202b955886853c85a6f5ff94485246a74c
Dmytro Legerda <terrain1984@gmail.com>
2022-06-11 16:09:59 +0300

    Fixed after code review

--- src/transformState.js
+++ src/transformState.js
@@ -8,21 +8,39 @@ function transformState(state, actions) {
   // write code here
   const stateCopy = state;
 
-  actions.forEach(function(action) {
-    if (action.type === 'addProperties') {
-      Object.assign(stateCopy, action.extraData);
-    }
+  actions.forEach((action) => {
+    // if (action.type === 'addProperties') {
+    //   Object.assign(stateCopy, action.extraData);
+    // }
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete stateCopy[key];
-      };
-    }
+    // if (action.type === 'removeProperties') {
+    //   for (const key of action.keysToRemove) {
+    //     delete stateCopy[key];
+    //   };
+    // }
 
-    if (action.type === 'clear') {
-      for (const key in stateCopy) {
-        delete stateCopy[key];
-      }
+    // if (action.type === 'clear') {
+    //   for (const key in stateCopy) {
+    //     delete stateCopy[key];
+    //   }
+    // }
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(stateCopy, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        };
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
     }
   });
 
