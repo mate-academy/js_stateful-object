c2136a54684317b7c895d1d62424798a3e4ee821
Zykhor Andrii <zuxoran@gmail.com>
2022-06-12 02:13:53 +0300

    task done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const char of actions) {
+    for (const key in char) {
+      if (key === 'extraData') {
+        for (const tey in char[key]) {
+          state[tey] = char[key][tey];
+        }
+      }
+
+      if (key === 'keysToRemove') {
+        for (const remov of char[key]) {
+          delete state[remov];
+        }
+      }
+
+      if (char[key] === 'clear') {
+        for (const allKey in state) {
+          delete state[allKey];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
