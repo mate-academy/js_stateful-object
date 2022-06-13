6a10ebc51ed5c3f78d676412a0452dcb84e68e7f
Leonid Levtseniuk <llevtseniuk@gmail.com>
2022-06-13 18:28:39 +0300

    Transform State

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,24 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const prop in state) {
+          delete state[prop];
+        }
+    }
+  }
 }
 
 module.exports = transformState;
