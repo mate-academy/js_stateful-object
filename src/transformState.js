c70b3f0f7ed02a7b833d4aa911fac4b8f5e41ab0
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-14 14:13:42 +0300

    first commit

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const object of actions) {
+    if (object.type === `addProperties`) {
+      Object.assign(state, object.extraData);
+    }
+
+    if (object.type === `clear`) {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    if (object.type === `removeProperties`) {
+      object.keysToRemove.forEach((el) => {
+        if (state[el]) {
+          delete state[el];
+        }
+      });
+    }
+  }
 }
 
 module.exports = transformState;
