62bed3251ee3acb7601b85ca02907b54dbf16ac0
IvanG6 <64322485+IvanG6@users.noreply.github.com>
2022-06-16 17:38:01 +0300

    changed

--- src/transformState.js
+++ src/transformState.js
@@ -5,40 +5,33 @@
  * @param {Object[]} actions
  */
 
-const stateVal = {};
-
-const actionVal = [
-  {
-    type: 'addProperties',
-    extraData: {
-      name: 'Jim', hello: 'world',
-    },
-  },
-];
-
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key of Object.keys(action.extraData)) {
-        state[key] = action.extraData[key];
-      }
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const keyToRemove of action.keysToRemove) {
-        if (state[keyToRemove]) {
-          delete state[keyToRemove];
+    switch (action.type) {
+      case 'addProperties': {
+        for (const key of Object.keys(action.extraData)) {
+          state[key] = action.extraData[key];
         }
+        break;
       }
-    }
 
-    if (action.type === 'clear') {
-      for (const keyToRm of Object.keys(state)) {
-        delete state[keyToRm];
+      case 'removeProperties': {
+        for (const keyToRemove of action.keysToRemove) {
+          if (state[keyToRemove]) {
+            delete state[keyToRemove];
+          }
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
       }
     }
   }
 }
 
-transformState(stateVal, actionVal);
 module.exports = transformState;
