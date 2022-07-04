43c91e9d636a24d6e2639c417d4283e8e343b658
Mykhailo Oblat <moblat890@gmail.com>
2022-07-04 16:03:12 +0300

    task js_stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      Object.assign(state, key.extraData);
+    } else if (key.type === 'removeProperties') {
+      for (const keyToDel of key.keysToRemove) {
+        delete state[keyToDel];
+      }
+    } else if (key.type === 'clear') {
+      for (const keyOfState in state) {
+        delete state[keyOfState];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
