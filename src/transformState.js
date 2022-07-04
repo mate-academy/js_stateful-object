dd10dac62c025d0ec1b77e620b529f9213452233
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-04 13:25:09 +0300

    ready

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const element of actions) {
+    if (element.type === 'addProperties') {
+      Object.assign(state, element['extraData']);
+    } else if (element.type === 'removeProperties') {
+      for (const removeProperti of element['keysToRemove']) {
+        delete state[removeProperti];
+      }
+    } else if (element.type === 'clear') {
+      for (const stateElement in state) {
+        delete state[stateElement];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
