864ace368410b03b6b9e89f02bd208919cf56aef
Petro <akktothebrawl@gmail.com>
2022-07-04 20:09:22 +0300

    code optimization

--- src/transformState.js
+++ src/transformState.js
@@ -8,20 +8,20 @@ function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        for (const key in action.extraData) {
-          state[key] = action.extraData[key];
-        }
+        Object.assign(state, action.extraData);
         break;
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          state[key] = undefined;
+          delete state[key];
         }
         break;
       case 'clear':
         for (const key in state) {
-          state[key] = undefined;
+          delete state[key];
         }
         break;
+      default:
+        throw new Error('Invalid value');
     }
   }
 }
