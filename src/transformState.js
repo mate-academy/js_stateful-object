3c2dbf16300e522b6f9e38bedc7ad75332891f15
Nikita Biriukov <flimsy00@gmail.com>
2022-07-04 14:30:47 +0300

    if was changed with switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,21 +6,51 @@
  */
 
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i]['extraData']) {
-      for (const keys in actions[i]['extraData']) {
-        state[keys] = actions[i]['extraData'][keys];
-      }
-    } else if (actions[i]['keysToRemove']) {
-      for (const values of actions[i]['keysToRemove']) {
-        delete state[values];
-      }
-    } else if (actions[i]['type'] === 'clear') {
-      for (const deleted in state) {
-        delete state[deleted];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (key in state) {
+            delete state[key];
+          }
+        }
+
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(state)) {
+          delete state[key];
+        }
+
+        break;
+
+      default:
+        break;
     }
   }
 }
+
+// function transformState(state, actions) {
+//   for (let i = 0; i < actions.length; i++) {
+//     if (actions[i]['extraData']) {
+//       for (const keys in actions[i]['extraData']) {
+//         state[keys] = actions[i]['extraData'][keys];
+//       }
+//     } else if (actions[i]['keysToRemove']) {
+//       for (const values of actions[i]['keysToRemove']) {
+//         delete state[values];
+//       }
+//     } else if (actions[i]['type'] === 'clear') {
+//       for (const deleted in state) {
+//         delete state[deleted];
+//       }
+//     }
+//   }
+// }
 
 module.exports = transformState;
