e186374482ebafb80828b966bdf6de10145d1e8f
Mariya Sukhanova <suhanovamasha21@gmail.com>
2022-07-05 19:38:13 +0300

    Added switch

--- src/transformState.js
+++ src/transformState.js
@@ -10,18 +10,24 @@ function transformState(state, actions) {
     const act = action.type;
     const data = action.extraData;
 
-    if (act === 'addProperties') {
-      for (const key in data) {
-        state[key] = data[key];
-      }
-    } else if (act === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-    } else if (act === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (act) {
+      case 'addProperties':
+        for (const key in data) {
+          state[key] = data[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        return 'No actions needed';
     }
   }
 }
