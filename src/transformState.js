9a63da3c56961e9eebce6829dc8a14cb0da9ad4d
Mykhailo Oblat <moblat890@gmail.com>
2022-07-06 13:16:32 +0300

    change from if to switch case

--- src/transformState.js
+++ src/transformState.js
@@ -6,16 +6,22 @@
  */
 function transformState(state, actions) {
   for (const key of actions) {
-    if (key.type === 'addProperties') {
-      Object.assign(state, key.extraData);
-    } else if (key.type === 'removeProperties') {
-      for (const keyToDel of key.keysToRemove) {
-        delete state[keyToDel];
-      }
-    } else if (key.type === 'clear') {
-      for (const keyOfState in state) {
-        delete state[keyOfState];
-      }
+    switch (key.type) {
+      case 'addProperties':
+        Object.assign(state, key.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToDel of key.keysToRemove) {
+          delete state[keyToDel];
+        }
+        break;
+
+      case 'clear':
+        for (const keyOfState in state) {
+          delete state[keyOfState];
+        }
+        break;
     }
   }
 
