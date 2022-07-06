aebdf7476f050f1718405ee308207423ecef5b9a
lentyaeev <daniilshaliuk@gmail.com>
2022-07-06 09:07:41 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(e => {
+    switch (e.type) {
+      case 'addProperties':
+        Object.assign(state, e.extraData);
+        break;
+
+      case 'removeProperties':
+        e.keysToRemove.forEach(key => {
+          delete state[key];
+        });
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw new Error('No action of such type yet');
+    }
+  });
+
+  return state;
 }
 
 module.exports = transformState;
