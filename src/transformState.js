48cb7a6cf1695bce8e4ca5e570f3d8a503755f94
Volodya <balyshev.volodya@gmail.com>
2022-07-06 16:52:23 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,28 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const { type: actionType, extraData, keysToRemove } of actions) {
+    switch (actionType) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of keysToRemove) {
+          delete state[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw new Error('Not supported action type');
+    }
+  }
 }
 
 module.exports = transformState;
