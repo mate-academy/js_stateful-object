3ae69c626de41b4a22625b5ec4ef244fc1617edb
fenderukr <priststoreua@gmail.com>
2022-07-07 16:31:10 +0300

    transformState0.1

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  let stateCopy = state;
+
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        stateCopy = Object.assign(state, action['extraData']);
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action['keysToRemove']) {
+          delete stateCopy[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          if (stateCopy.hasOwnProperty(key)) {
+            delete stateCopy[key];
+          }
+        }
+    }
+  }
+
+  return stateCopy;
 }
 
 module.exports = transformState;
