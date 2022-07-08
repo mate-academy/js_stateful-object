9349822450cb289be6a77da8ddb0ce9ca03db04e
darynakravchuk <darinakravchuk@gmail.com>
2022-07-08 21:17:55 +0200

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,27 @@
  */
 function transformState(state, actions) {
   // write code here
+  const newObj = state;
+
+  for (const a of actions) {
+    if (a.type === 'addProperties') {
+      Object.assign(newObj, a.extraData);
+    }
+
+    if (a.type === 'removeProperties') {
+      for (const i of a.keysToRemove) {
+        delete newObj[i];
+      }
+    }
+
+    if (a.type === 'clear') {
+      for (const k in newObj) {
+        delete newObj[k];
+      }
+    }
+  }
+
+  return newObj;
 }
 
 module.exports = transformState;
