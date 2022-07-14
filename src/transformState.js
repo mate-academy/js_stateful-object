8d89d3500c2ca99996a8c85470cdc9cbe385b766
Boris Kerekesha <kerekesha90@ukr.net>
2022-07-14 14:59:02 +0300

    transFormState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const property of actions) {
+    if (property.type === 'addProperties') {
+      for (const key in property.extraData) {
+        state[key] = property.extraData[key];
+      }
+    }
+
+    if (property.type === 'removeProperties') {
+      for (const keyremove of property.keysToRemove) {
+        if (keyremove in state) {
+          delete state[keyremove];
+        }
+      }
+    }
+
+    if (property.type === 'clear') {
+      for (const keyclear in state) {
+        delete state[keyclear];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
