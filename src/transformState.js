35e76354a79be55e0ba71999afa011d13d826a0a
Bohdan Tron <ikwiczzts@gmail.com>
2022-07-17 17:02:08 +0300

    stateful object is ready

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const aType = actions[i].type;
+    const aExtra = actions[i].extraData;
+    const aKeysToRemove = actions[i].keysToRemove;
+
+    if (aType === 'addProperties') {
+      Object.assign(state, aExtra);
+    } else if (aType === 'removeProperties') {
+      for (let x = 0; x < aKeysToRemove.length; x++) {
+        delete state[aKeysToRemove[x]];
+      }
+    } else if (aType === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
