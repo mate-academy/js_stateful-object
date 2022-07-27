d7cbec29a0a9e32408b710a4c37fb66aabd251b0
DominoSl <artemenko.mik@gmail.com>
2022-07-27 17:24:36 +0300

    solution to the task

--- src/transformState.js
+++ src/transformState.js
@@ -61,21 +61,34 @@
  * @param {Object} state
  * @param {Object[]} transforms
  */
+
 function transformState(state, transforms) {
   for (const transform of transforms) {
-    if (transform.operation === 'addProperties') {
-      Object.assign(state, transform.properties);
-    }
+    switch (transform.operation) {
+      case 'addProperties' : {
+        Object.assign(state, transform.properties);
 
-    if (transform.operation === 'removeProperties') {
-      for (const property of transform.properties) {
-        delete state[property];
+        break;
       }
-    }
 
-    if (transform.operation === 'clear') {
-      for (const property in state) {
-        delete state[property];
+      case 'removeProperties' : {
+        for (const property of transform.properties) {
+          delete state[property];
+        }
+
+        break;
+      }
+
+      case 'clear' : {
+        for (const property in state) {
+          delete state[property];
+        }
+
+        break;
+      }
+
+      default: {
+        break;
       }
     }
   }
