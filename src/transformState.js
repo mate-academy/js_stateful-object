f0bee0c9db8453a482ff9a4f9d94874a68905289
Mark Shapka <mark.shapka1@gmail.com>
2022-08-01 11:15:27 +0300

    Task_stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,31 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const arr = Object.values(actions[i]);
+
+    if (arr[0] === 'addProperties') {
+      const data = arr[1];
+
+      for (const key in data) {
+        state[key] = data[key];
+      }
+    }
+
+    if (arr[0] === 'removeProperties') {
+      const data = arr[1];
+
+      for (let j = 0; j < data.length; j++) {
+        delete state[data[j]];
+      }
+    }
+
+    if (arr[0] === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
