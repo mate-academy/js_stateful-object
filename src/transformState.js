07655843875935a9a602a933a13c96d74039cae7
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-03 23:39:00 +0300

    switch

--- src/transformState.js
+++ src/transformState.js
@@ -8,16 +8,22 @@ function transformState(state, actions) {
   const result = state;
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(result, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete result[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const key in result) {
-        delete result[key];
-      }
+    switch (action.type) {
+      case 'addProperties' :
+        Object.assign(result, action.extraData);
+        break;
+
+      case 'removeProperties' :
+        for (const key of action.keysToRemove) {
+          delete result[key];
+        }
+        break;
+
+      case 'clear' :
+        for (const key in result) {
+          delete result[key];
+        }
+        break;
     }
   }
 
