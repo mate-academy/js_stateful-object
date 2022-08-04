b17b50c8922e350a108ab9215171bbbcf1203ef9
Adrian Faryniuk <forever0754@gmail.com>
2022-08-04 16:30:54 +0300

    done all passed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(state, obj.extraData);
+    }
+
+    if (obj.type === 'removeProperties') {
+      const toRemove = obj.keysToRemove;
+
+      for (const rem of toRemove) {
+        delete state[rem];
+      }
+    }
+
+    if (obj.type === 'clear') {
+      for (const clear in state) {
+        delete state[clear];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
