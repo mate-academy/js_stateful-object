e84b4ffa6abe44517fa34eeb5720ab9ccb4ad19c
Andrii Stiebalov <stiebalov@gmail.com>
2022-08-08 08:38:33 +0300

    transformState was added

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,29 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties' :
+        for (const key in obj.extraData) {
+          state[key] = obj.extraData[key];
+        }
+
+        break;
+
+      case 'removeProperties':
+        for (const j of obj.keysToRemove) {
+          delete state[j];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
