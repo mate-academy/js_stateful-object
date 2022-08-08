d20ce617b67e9866698cb7819c6229d152259625
Dmytro Matsehora <ninjawithoutface@gmail.com>
2022-08-08 12:14:49 +0300

    remade with switch case

--- src/transformState.js
+++ src/transformState.js
@@ -6,19 +6,25 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties': {
+        Object.assign(state, action.extraData);
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
+        break;
       }
-    }
 
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
+      case 'removeProperties': {
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
+          delete state[key];
+        }
       }
     }
   }
