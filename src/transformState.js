495f206189c52e76f3626a4a2c330606a8382475
Artem Kodesnikov <artiguard@gmail.com>
2022-08-08 18:37:44 +0300

    rewrited code with switch case

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const action in actions) {
-    if (actions[action].type === 'addProperties') {
-      for (const data in actions[action].extraData) {
-        state[data] = actions[action].extraData[data];
-      }
-    } else if (actions[action].type === 'removeProperties') {
-      for (const key of actions[action].keysToRemove) {
-        delete state[key];
-      }
-    } else {
-      for (const key in state) {
-        delete state[key];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties' :
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties' :
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear' :
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
