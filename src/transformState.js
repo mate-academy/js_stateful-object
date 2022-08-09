f5e8ea6b6b5a4a231f3bf3790d96c36ff5f736ed
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-09 17:38:34 +0300

    rewrite in switch...case

--- src/transformState.js
+++ src/transformState.js
@@ -8,28 +8,29 @@ function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     const type = actions[i].type;
 
-    if (type === 'addProperties') {
-      const data = actions[i].extraData;
+    switch (type) {
+      case 'addProperties':
+        const data = actions[i].extraData;
 
-      for (const key in data) {
-        state[key] = data[key];
-      }
-    }
-
-    if (type === 'removeProperties') {
-      const remove = actions[i].keysToRemove;
-
-      for (let y = 0; y < remove.length; y++) {
-        if (state[remove[y]]) {
-          delete state[remove[y]];
+        for (const key in data) {
+          state[key] = data[key];
         }
-      }
-    }
+        break;
 
-    if (type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'removeProperties':
+        const remove = actions[i].keysToRemove;
+
+        for (let y = 0; y < remove.length; y++) {
+          if (state[remove[y]]) {
+            delete state[remove[y]];
+          }
+        }
+        break;
+
+      default:
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 
