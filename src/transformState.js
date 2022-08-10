8696c2983df9d984ceed0828baa4bef44d9130ac
Mark Shapka <mark.shapka1@gmail.com>
2022-08-10 20:29:14 +0300

    changed if condition to the switch-case

--- src/transformState.js
+++ src/transformState.js
@@ -8,27 +8,28 @@ function transformState(state, actions) {
   // write code here
   for (let i = 0; i < actions.length; i++) {
     const arr = Object.values(actions[i]);
+    const data = arr[1];
 
-    if (arr[0] === 'addProperties') {
-      const data = arr[1];
+    switch (arr[0]) {
+      case 'addProperties':
+        for (const key in data) {
+          state[key] = data[key];
+        }
+        break;
 
-      for (const key in data) {
-        state[key] = data[key];
-      }
-    }
+      case 'removeProperties':
+        for (let j = 0; j < data.length; j++) {
+          delete state[data[j]];
+        }
+        break;
 
-    if (arr[0] === 'removeProperties') {
-      const data = arr[1];
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
 
-      for (let j = 0; j < data.length; j++) {
-        delete state[data[j]];
-      }
-    }
-
-    if (arr[0] === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      default:
     }
   }
 }
