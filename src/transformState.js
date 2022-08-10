1bc52f6d93e6d4199730c17d9c75ebc0e8b3e10f
Anna Grechana <grechanaanna@gmial.com>
2022-08-10 22:47:54 +0300

    1st version

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(state, actions[i].extraData);
+    } else if (actions[i].type === 'removeProperties') {
+      for (const del of actions[i].keysToRemove) {
+        delete state[del];
+      }
+    } else if (actions[i].type === 'clear') {
+      for (const clear in state) {
+        delete state[clear];
+      }
+    }
+  };
 }
 
 module.exports = transformState;
