29bda9a5835a2cb0e1ec036a7d9dada5f0211c73
EhorSaprykin <astralik1993@gmail.com>
2022-08-19 14:48:42 +0300

    Transform state done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,22 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action in actions) {
+    const removeArr = actions[action].keysToRemove;
+    const actionType = actions[action].type;
+
+    if (actionType === 'addProperties') {
+      Object.assign(state, actions[action].extraData);
+    } else if (actionType === 'removeProperties') {
+      for (let i = 0; i < removeArr.length; i++) {
+        delete state[removeArr[i]];
+      }
+    } else if (actionType === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
