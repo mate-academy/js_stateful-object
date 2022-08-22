a74b7b0fccfa317f36adf18188a6eb0f05eb9d49
Taras Nakonechnyi <nakonechnyi.taras@gmail.com>
2022-08-22 11:21:44 +0300

    used switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,16 +6,23 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
-    } else if (actions[i].type === 'removeProperties') {
-      for (let x = 0; x <= actions[i].keysToRemove.length; x++) {
-        delete state[actions[i].keysToRemove[x]];
-      }
-    } else if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    const operation = actions[i];
+    const operationType = operation.type;
+
+    switch (operationType) {
+      case 'addProperties':
+        Object.assign(state, operation.extraData);
+        break;
+      case 'removeProperties':
+        for (let x = 0; x <= operation.keysToRemove.length; x++) {
+          delete state[operation.keysToRemove[x]];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
