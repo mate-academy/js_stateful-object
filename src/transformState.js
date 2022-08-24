4e52124fa7298d57b6b788b31652d97f049a6eda
Nazar Vorona <voron1765@gmail.com>
2022-08-24 17:28:01 +0300

    task is done

--- src/transformState.js
+++ src/transformState.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     switch (actions[i].type) {
