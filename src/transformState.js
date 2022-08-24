ce7cce95b2d28654ef0d2cc68a99cfbc103d332c
Nazar Vorona <voron1765@gmail.com>
2022-08-24 17:26:56 +0300

    task is done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,24 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+
+      case 'removeProperties':
+        actions[i].keysToRemove.forEach((x) => {
+          delete state[`${x}`];
+        });
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[`${key}`];
+        }
+    }
+  }
 }
 
 module.exports = transformState;
