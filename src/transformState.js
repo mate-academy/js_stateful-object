72412ee00e54f0c304ae934db366719eb43115d0
Kate Novak <novak5683@gmail.com>
2022-08-28 13:17:23 +0200

    switch_case_added

--- src/transformState.js
+++ src/transformState.js
@@ -7,20 +7,24 @@
 
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
+    const expr = action.type;
 
-    if (action.type === 'removeProperties') {
-      for (let i = 0; i < action.keysToRemove.length; i++) {
-        delete state[action.keysToRemove[i]];
-      }
-    }
+    switch (expr) {
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
 
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
+      case 'removeProperties':
+        for (let i = 0; i < action.keysToRemove.length; i++) {
+          delete state[action.keysToRemove[i]];
+        }
+        break;
+
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
     }
   }
 }
