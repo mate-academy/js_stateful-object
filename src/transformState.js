f72e7397b94f6cf9ac2ad390ac966ef808565287
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-09-01 02:09:37 +0300

    very intrestin cod

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    for (const key in actions[i]) {
+      if (actions[i][key] === 'clear') {
+        for (const x in state) {
+          delete state[x];
+        }
+      }
+
+      if (key === 'keysToRemove') {
+        for (const j of actions[i][key]) {
+          for (const x in state) {
+            if (j === x) {
+              delete state[x];
+            }
+          }
+        }
+        // eslint-disable-next-line no-console
+        console.log(actions[i][key]);
+      }
+
+      if (key === 'extraData') {
+        Object.assign(state, actions[i][key]);
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
