8f9768671db131d8e195f39552e131691836a493
Kolya Movchan <kmovchan261@gmail.com>
2022-09-01 13:41:27 +0300

    transformState js is done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const object of actions) {
+    Object.assign(state, object.extraData);
+
+    if (object.keysToRemove) {
+      for (const x of object.keysToRemove) {
+        if (x in state) {
+          delete state[x];
+        }
+      }
+    }
+
+    if (object.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
