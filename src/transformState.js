23abfab318f9aeb5bacc052f92a8340dd4240164
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-05 13:47:22 +0300

    created

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,37 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const object of actions) {
+    switch (object.type) {
+      case 'addProperties':
+        addPropertiesFunc(state, object.extraData);
+        break;
+
+      case 'removeProperties':
+        removePropertiesFunc(state, object.keysToRemove);
+        break;
+
+      case 'clear':
+        clearFunc(state);
+        break;
+    }
+  }
 }
+
+const addPropertiesFunc = (state, data) => {
+  return Object.assign(state, data);
+};
+
+const removePropertiesFunc = (state, toRemove) => {
+  for (const value of toRemove) {
+    delete state[value];
+  }
+};
+
+const clearFunc = state => {
+  for (const key in state) {
+    delete state[key];
+  }
+};
 
 module.exports = transformState;
