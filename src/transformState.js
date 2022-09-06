3ea2cbc5dece25cd42e6a5884db1e4a329571ecb
Hlazman <glazman.b@gmail.com>
2022-09-06 14:56:35 +0300

    made function transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const iterator of actions) {
+    if (iterator.type === 'addProperties') {
+      for (const key in iterator.extraData) {
+        state[key] = iterator.extraData[key];
+      }
+    }
+
+    if (iterator.type === 'removeProperties') {
+      for (let i = 0; i <= iterator.keysToRemove.length - 1; i++) {
+        delete state[iterator.keysToRemove[i]];
+      }
+    }
+
+    if (iterator.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
