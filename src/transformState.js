690127a3804567d27f2707951a92a0e9030ac91f
biniyam yemaneberhane <biniyy2021@gmail.com>
2022-09-05 18:07:46 -0700

    STATEFUL_OBJECT SOLUTION

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,21 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      for (const key in obj.extraData) {
+        state[key] = obj.extraData[key];
+      }
+    } else if (obj.type === 'removeProperties') {
+      for (const key of obj.keysToRemove) {
+        delete state[key];
+      }
+    } else if (obj.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
