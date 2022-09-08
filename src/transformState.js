9e44ee4fbf040ce6454631fa730766a1ca8daf83
Christian Wolf <chwolf@waterstone-usa.com>
2022-09-08 15:21:19 +0200

    commit

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,28 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
   // write code here
-}
+  for (const i in actions) {
+    for (const k in actions[i]) {
+      switch (actions[i][k]) {
+        case 'addProperties':
+          Object.assign(state, actions[i].extraData);
+          break;
 
+        case 'removeProperties':
+          for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+            delete state[actions[i].keysToRemove[j]];
+          }
+          break;
+
+        case 'clear':
+          for (const m in state) {
+            delete state[m];
+          }
+      }
+    }
+  }
+}
 module.exports = transformState;
