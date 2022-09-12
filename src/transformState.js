696372cb2d030d732c668d7ff84c5ab764031f1d
Dmytro Sydorenko <dsydorenko534@gmail.com>
2022-09-12 22:29:13 +0300

    Solution to stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,27 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        Object.assign(state, action['extraData']);
+        break;
+
+      case 'removeProperties':
+        for (const delClause of action['keysToRemove']) {
+          delete state[delClause];
+        }
+        break;
+
+      case 'clear':
+        for (const delAllTheClauses in state) {
+          delete state[delAllTheClauses];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
