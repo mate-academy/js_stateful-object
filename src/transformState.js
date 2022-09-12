58d89257c6aab3bf91b40f98d58c9353a2e61117
Ihor Andrianov <andrianoia@gmail.com>
2022-09-12 19:20:32 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,3 +1,4 @@
+/* eslint-disable spaced-comment */
 'use strict';
 
 /**
@@ -5,7 +6,58 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const reducer = (_state, action) => {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          _state[key] = action.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete _state[key];
+        }
+        break;
+      case 'clear':
+        const objectProps = Object.getOwnPropertyNames(_state);
+
+        objectProps.forEach((key) => delete _state[key]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  for (const act of actions) {
+    reducer(state, act);
+  }
+  //non-mutating function :(
+  /*const reducer = (_state, action) => {
+    switch (action.type) {
+      case 'addProperties':
+        return {
+          ..._state,
+          ...action.extraData,
+        };
+      case 'removeProperties':
+        const updatedState = action.keysToRemove.reduce((object, key) => {
+          const { [key]: _, ...p } = object;
+
+          return p;
+        }, _state);
+
+        return updatedState;
+      case 'clear':
+        return {};
+      default:
+        break;
+    }
+  };
+  const modifiedState = actions.reduce((__state, act) => {
+    return reducer(__state, act);
+  }, state);
+
+  return modifiedState; */
 }
 
 module.exports = transformState;
