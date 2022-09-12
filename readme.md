d0ff7adb1b8f81dfe4223e4a26c02018fb6e7af1
Polly Slipchenko <marmoria.1999science@gmail.com>
2022-09-12 18:23:31 +0300

    Solution

--- readme.md
+++ readme.md
@@ -70,3 +70,4 @@ Another example:
 
 // state === { foo: 'bar', name: 'Jim' }
 ```
+ 
--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action['type'] === 'addProperties') {
+      const additionalObj = action['extraData'];
+
+      for (const objKey in additionalObj) {
+        state[objKey] = additionalObj[objKey];
+      }
+    }
+
+    if (action['type'] === 'removeProperties') {
+      const arr = action['keysToRemove'];
+
+      for (const arrElem of arr) {
+        delete state[arrElem];
+      }
+    }
+
+    if (action['type'] === 'clear') {
+      for (const stateKey in state) {
+        delete state[stateKey];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
