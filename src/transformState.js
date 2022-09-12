f8122c6ff7061d013b1e9c1bf02f54e67264c763
Myroslav Diiak <diiakm23819@gmail.com>
2022-09-12 15:56:51 +0300

    some fixes

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,22 @@
  */
 function transformState(state, actions) {
   // write code here
-  let now;
+  let nowAction;
 
   for (let i = 0; i < actions.length; i++) {
-    now = Object.values(actions[i]);
+    nowAction = Object.values(actions[i]);
 
-    if (now[0] === 'addProperties') {
-      Object.assign(state, now[1]);
+    if (nowAction[0] === 'addProperties') {
+      Object.assign(state, nowAction[1]);
     }
 
-    if (now[0] === 'removeProperties') {
-      for (let k = 0; k < now[1].length; k++) {
-        delete state[now[1][k]];
+    if (nowAction[0] === 'removeProperties') {
+      for (let k = 0; k < nowAction[1].length; k++) {
+        delete state[nowAction[1][k]];
       }
     }
 
-    if (now[0] === 'clear') {
+    if (nowAction[0] === 'clear') {
       for (const key in state) {
         if (state.hasOwnProperty(key)) {
           delete state[key];
