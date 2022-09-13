08e500e8c80583709060011e794b8021a4b6fe94
Serhii Romaniuk <deepspark@gmail.com>
2022-09-13 19:08:15 +0300

    add js code who decides task

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const { type, extraData, keysToRemove } of actions) {
+    if (type === 'addProperties') {
+      Object.assign(state, extraData);
+    } else if (type === 'removeProperties') {
+      for (const item of keysToRemove) {
+        if (Object.keys(state).filter(elem => elem === item)) {
+          delete state[item];
+        }
+      }
+    } else {
+      for (const bit of Object.keys(state)) {
+        delete state[bit];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
