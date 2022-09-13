bb59d891cf5b7a4465ed3d64caa2f1fe7ddad346
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-09-13 17:50:50 +0300

    switch if on switch

--- src/transformState.js
+++ src/transformState.js
@@ -8,22 +8,24 @@ function transformState(state, actions) {
   // write code here
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
-
-    if (actions[i].type === 'removeProperties') {
-      for (const key in state) {
-        if (actions[i].keysToRemove.includes(key)) {
+    switch (actions[i].type) {
+      case 'clear':
+        for (const key in state) {
           delete state[key];
         }
-      }
-    }
+        break;
 
-    if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
+      case 'removeProperties':
+        for (const key in state) {
+          if (actions[i].keysToRemove.includes(key)) {
+            delete state[key];
+          }
+        }
+        break;
+
+      case 'addProperties' :
+        Object.assign(state, actions[i].extraData);
+        break;
     }
   }
 }
