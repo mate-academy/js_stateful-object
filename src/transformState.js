a12dd9dc0e1ffa20f5ff5adc2540ced94c83a8b9
Olena Shevchenko <shevochkas@gmail.com>
2022-09-14 20:33:50 +0300

    function transformState was added

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const step of actions) {
+    if (step.type === 'addProperties') {
+      Object.assign(state, step.extraData);
+    }
+
+    if (step.type === 'removeProperties') {
+      const keysToDelete = step.keysToRemove;
+
+      for (const key of keysToDelete) {
+        delete state[key];
+      }
+    }
+
+    if (step.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
