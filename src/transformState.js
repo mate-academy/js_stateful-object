d96a08bde8ad0c9c760a3b9502033c16017eb8d0
Arsen-Shpak <a.shpakovich2000@gmail.com>
2022-09-14 12:45:59 +0300

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const item of actions) {
+    switch (item.type) {
+      case 'addProperties':
+        Object.assign(state, item.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of item.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
