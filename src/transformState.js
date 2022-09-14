c42088364afc6aa5e2ac8796dcbb716b9f145163
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-09-14 14:13:52 +0300

    result

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const states = state;
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(states, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete states[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in states) {
+          delete states[key];
+        }
+        break;
+
+      default:
+        throw new Error('unknown action type');
+    }
+  }
+
+  return states;
 }
 
 module.exports = transformState;
