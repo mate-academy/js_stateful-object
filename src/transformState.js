81b0eab7771ff12254354034f344bdb41f6869eb
AniaHna <annahnativ2@gmail.com>
2022-09-14 14:09:18 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const states = state;
+  // const copyState = ;
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(states, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete states[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in states) {
+          delete states[key];
+        }
+        break;
+
+      default:
+        throw Error('unknown action type');
+    }
+  }
+
+  return states;
 }
 
 module.exports = transformState;
