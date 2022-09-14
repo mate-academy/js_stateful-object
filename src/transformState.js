709d184b93829c508da827fb8a475e9eb1d0d5ef
Dima <ddikey1@gmail.com>
2022-09-14 11:41:30 +0300

    fixed property access

--- src/transformState.js
+++ src/transformState.js
@@ -9,11 +9,11 @@ function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(state, action['extraData']);
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action['keysToRemove']) {
+        for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
