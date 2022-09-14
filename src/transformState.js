a5dbd58cb7e4fd49435a147a623ce192ca64b746
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-09-14 11:57:30 +0300

    fixes

--- src/transformState.js
+++ src/transformState.js
@@ -4,24 +4,28 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i]['type'] === 'addProperties') {
-      Object.assign(state, actions[i]['extraData']);
-    } else if (actions[i]['type'] === 'removeProperties') {
-      const remove = Object.keys(state);
 
-      for (let g = 0; g < remove.length; g++) {
-        for (let k = 0; k < remove.length; k++) {
-          if (remove[g] === actions[i].keysToRemove[k]) {
-            delete state[remove[g]];
-          }
+function transformState(state, actions) {
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
-      }
-    } else if (actions[i]['type'] === 'clear') {
-      for (const key of Object.keys(state)) {
-        delete state[key];
-      }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw Error('unknown action type');
     }
   }
 }
