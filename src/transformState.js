9bc38c22810d6970e81410e7ecf02407bd1c5328
Bohdan Borsuk <barsuchellos@gmail.com>
2022-09-15 09:54:05 +0300

    complete the task

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(element => {
+    switch (element.type) {
+      case 'addProperties':
+        Object.assign(state, element.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of element.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const k in state) {
+          delete state[k];
+        }
+        break;
+
+      default:
+        throw Error('unknown action type');
+    }
+  });
+
+  return state;
 }
 
 module.exports = transformState;
