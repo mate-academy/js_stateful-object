0d36433e83b8df56474f139d9ed304fcdafa8370
Ihor Andrianov <andrianoia@gmail.com>
2022-09-15 21:49:31 +0300

    fixed comments

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,22 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  const reducer = (_state, action) => {
+  const reducer = (reducerState, action) => {
     switch (action.type) {
       case 'addProperties':
         for (const key in action.extraData) {
-          _state[key] = action.extraData[key];
+          reducerState[key] = action.extraData[key];
         }
         break;
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          delete _state[key];
+          delete reducerState[key];
         }
         break;
       case 'clear':
-        const objectProps = Object.getOwnPropertyNames(_state);
-
-        objectProps.forEach((key) => delete _state[key]);
+        for (const param in reducerState) {
+          delete reducerState[param];
+        }
         break;
       default:
         break;
@@ -31,33 +31,6 @@ function transformState(state, actions) {
   for (const act of actions) {
     reducer(state, act);
   }
-  //non-mutating function :(
-  /*const reducer = (_state, action) => {
-    switch (action.type) {
-      case 'addProperties':
-        return {
-          ..._state,
-          ...action.extraData,
-        };
-      case 'removeProperties':
-        const updatedState = action.keysToRemove.reduce((object, key) => {
-          const { [key]: _, ...p } = object;
-
-          return p;
-        }, _state);
-
-        return updatedState;
-      case 'clear':
-        return {};
-      default:
-        break;
-    }
-  };
-  const modifiedState = actions.reduce((__state, act) => {
-    return reducer(__state, act);
-  }, state);
-
-  return modifiedState; */
 }
 
 module.exports = transformState;
