a805482df76a0052b7e16af8b4200771528f3b75
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-15 17:58:26 +0300

    variables created for keysToRemove and StateObjectKeys and also extraData destructured. Default case only used for througing error

--- src/transformState.js
+++ src/transformState.js
@@ -7,28 +7,35 @@
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     const { type } = actions[i];
+    const { extraData } = actions[i];
+    const keysToRemove = actions[i].keysToRemove;
+    const stateObjectKeys = Object.keys(state);
 
     switch (type) {
       case 'addProperties':
-        for (const [key, value] of Object.entries(actions[i].extraData)) {
+        for (const [key, value] of Object.entries(extraData)) {
           state[key] = value;
         }
         break;
 
       case 'removeProperties':
-        for (const key of actions[i].keysToRemove) {
+        for (const key of keysToRemove) {
           if (state[key]) {
             delete state[key];
           }
         }
         break;
 
-      default:
+      case 'clear':
         if (state) {
-          for (const key of Object.keys(state)) {
+          for (const key of stateObjectKeys) {
             delete state[key];
           }
         }
+        break;
+
+      default:
+        return 'Broken Input';
     }
   }
 }
