e01ce79b31ed0abd84dd013536bfdf65faa21d66
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2022-09-15 19:41:14 +0200

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -26,8 +26,7 @@ function transformState(state, actions) {
           delete state[states];
         }
         break;
-      default:
-        return 'unknown action input';
+      default:;
     }
   }
 }
