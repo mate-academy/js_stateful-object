50134275cf4adf75513263231c9278fe7cfdd767
Soboleva Tatiana <tanya_sobol@ukr.net>
2022-09-18 16:22:00 +0200

    Changed variables names

--- src/transformState.js
+++ src/transformState.js
@@ -6,31 +6,28 @@
  */
 function transformState(state, actions) {
   // write code here
-  const nextActions = [];
 
-  for (const key1 of actions) {
-    switch (key1.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        Object.assign(state, key1.extraData);
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key2 of key1.keysToRemove) {
-          delete state[key2];
+        for (const key1 of action.keysToRemove) {
+          delete state[key1];
         }
         break;
 
       case 'clear':
-        for (const key3 in state) {
-          delete state[key3];
+        for (const key2 in state) {
+          delete state[key2];
         }
         break;
 
       default:
         throw Error('Error');
     }
-
-    nextActions.push({ ...state });
   }
 
   return state;
