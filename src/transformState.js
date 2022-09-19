4cf0b49545705e346fd1232da422e9ebe930d2a2
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-09-19 13:18:08 +0300

    task was completed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (const key in actions) {
+    const obj = actions[key];
 
+    if (obj.type === 'addProperties') {
+      Object.assign(state, obj.extraData);
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const word of obj.keysToRemove) {
+        delete state[word];
+      }
+    }
+
+    if (obj.type === 'clear') {
+      const stateKeys = Object.keys(state);
+
+      for (const word of stateKeys) {
+        delete state[word];
+      }
+    }
+  }
+}
 module.exports = transformState;
