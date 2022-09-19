e9d9644e617bfc77806705aa5ae148cf72f8a428
Danil <dasha8854567@gmail.com>
2022-09-19 13:18:55 +0300

    commit1

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,27 @@
  */
 function transformState(state, actions) {
   // write code here
+
+  for (const key in actions) {
+    if (actions[key].type === 'addProperties') {
+      for (const keyProp in actions[key].extraData) {
+        state[keyProp] = actions[key].extraData[keyProp];
+      }
+    }
+
+    if (actions[key].type === 'removeProperties') {
+      for (const keyProp of actions[key].keysToRemove) {
+        delete state[`${keyProp}`];
+      }
+    }
+
+    if (actions[key].type === 'clear') {
+      for (const keyProp in state) {
+        delete state[keyProp]
+      }
+    }
+  }
+
 }
 
 module.exports = transformState;
