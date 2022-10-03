c08b00a8ee1c490d228d6dbafb925edffe9de543
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2022-10-03 21:48:14 +0300

    commit fixed

--- src/transformState.js
+++ src/transformState.js
@@ -5,8 +5,8 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const middleActions of actions) {
-    switch (middleActions.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'clear':
         for (const key in state) {
           delete state[key];
@@ -14,11 +14,11 @@ function transformState(state, actions) {
         break;
 
       case 'addProperties':
-        Object.assign(state, middleActions.extraData);
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const elem of middleActions.keysToRemove) {
+        for (const elem of action.keysToRemove) {
           delete state[elem];
         }
         break;
