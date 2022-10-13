5af5a01678aa6255acca95cb2ff2f5a323be6d2d
Maks <comanderius@gmail.com>
2022-10-13 15:11:55 +0200

    Used switch case

--- src/transformState.js
+++ src/transformState.js
@@ -10,26 +10,31 @@ function transformState(state, actions) {
   const remove = 'removeProperties';
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === clear) {
-      for (const ch in state) {
-        delete state[ch];
-      }
-    }
-
-    if (actions[i].type === add) {
-      for (const jey in actions[i].extraData) {
-        state[jey] = actions[i].extraData[jey];
-      }
-    }
-
-    if (actions[i].type === remove) {
-      for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+    switch (actions[i].type) {
+      case clear:
         for (const ch in state) {
-          if (ch === actions[i].keysToRemove[j]) {
-            delete state[ch];
+          delete state[ch];
+        }
+        break;
+
+      case add:
+        for (const jey in actions[i].extraData) {
+          state[jey] = actions[i].extraData[jey];
+        }
+        break;
+
+      case remove:
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          for (const ch in state) {
+            if (ch === actions[i].keysToRemove[j]) {
+              delete state[ch];
+            }
           }
         }
-      }
+        break;
+
+      default:
+        break;
     }
   }
 }
