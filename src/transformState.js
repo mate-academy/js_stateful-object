521200b290906bd7e3ace957db9656c6ecf119e1
haduigon <haduigon@gmail.com>
2022-10-14 14:36:37 +0300

    first commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  // let state2 = {};
+
+  Object.assign(state, state);
+
+  for (const action in actions) {
+    switch (actions[action].type) {
+      case 'addProperties':
+        Object.assign(state, actions[action].extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key in actions[action].keysToRemove) {
+          delete state[actions[action].keysToRemove[key]];
+        } break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
