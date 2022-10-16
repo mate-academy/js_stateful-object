824908556c11c65bf3dea449fb4333d8042f953b
s.aleksandrovych <s.aleksandrovych@valtech.com>
2022-10-16 17:31:48 +0300

    transformState function impl.

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(state, action.keysToRemove);
+        break;
+
+      case 'clear':
+        removeProperties(state, Object.keys(state));
+        break;
+    }
+  }
+}
+
+/**
+ * @param {Object} source
+ * @param {String[]} keys
+ */
+function removeProperties(source, keys) {
+  for (const key of keys) {
+    delete source[key];
+  }
 }
 
 module.exports = transformState;
