ed7bfa7496aa0279e5fb3b01505ffdd250303d87
Max Fedak <max.fedak@gmail.com>
2022-10-18 10:32:24 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,27 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      Object.keys(state).forEach(key => {
+        delete state[key];
+      });
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach(key => {
+        delete state[key];
+      });
+    }
+
+    if (action.type === 'addProperties') {
+      for (const [key, value] of Object.entries(action.extraData)) {
+        state[key] = value;
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
