b2f032fb6e3968d43099b70fc8dc5af03809f3db
Artem66 <melodpi@gmail.com>
2022-10-19 13:44:15 +0100

    impelemented transorm state object function

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key in actions) {
+    const type = actions[key].type;
+
+    if (type === 'addProperties') {
+      for (const extraDataKey in actions[key].extraData) {
+        state[extraDataKey] = actions[key].extraData[extraDataKey];
+      }
+    }
+
+    if (type === 'removeProperties') {
+      const arrRemove = actions[key].keysToRemove;
+
+      for (const i of arrRemove) {
+        delete state[i];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const removeKey in state) {
+        delete state[removeKey];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
