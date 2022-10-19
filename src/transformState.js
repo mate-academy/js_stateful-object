d7ea647652d581c85be9cfe3c10e7be8247dd0fa
Kukharuk <its.up.to.you.dk@gmail.com>
2022-10-19 12:16:02 +0300

    transfStateDK

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const type of actions) {
+    for (const prop in type) {
+      if (type[prop] === 'clear') {
+        for (const props in state) {
+          delete state[props];
+        }
+      } else if (type[prop] === 'addProperties') {
+        Object.assign(state, type.extraData);
+      } else if (type[prop] === 'removeProperties') {
+        for (const ch of type.keysToRemove) {
+          delete state[ch];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
