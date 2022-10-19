05e92be3f3229d7c41b2aa67fc04754d0fa5f306
Alex Tsimba <alexander.tsimba@gmail.com>
2022-10-19 21:40:27 +0300

    transformState task is done

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,28 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
-}
+const transformState = (state, actions) => {
+  for (const item of actions) {
+    switch (true) {
+      case (item.type === 'clear') :
+        for (const prop of Object.keys(state)) {
+          delete state[prop];
+        }
+        break;
+
+      case (item.type === 'removeProperties') :
+        for (let i = 0; i < item.keysToRemove.length; i++) {
+          delete state[item.keysToRemove[i]];
+        }
+        break;
+
+      case (item.type === 'addProperties') :
+        Object.assign(state, item.extraData);
+        break;
+    }
+  }
+
+  return state;
+};
 
 module.exports = transformState;
