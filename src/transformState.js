e654c980518a413eb19dfa35b92b4649a73789e5
Bohdan Stepanets <number1xy@gmail.com>
2022-10-20 18:01:02 +0300

    Solution with question

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,34 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  // Is it OK to create a function inside function or better move it above?
+  // Is it OK to create function in this task or better use cycle?
+  function deleteAllProperties() {
+    for (const key in state) {
+      delete state[key];
+    }
+  }
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        continue;
+      case `removeProperties`:
+        for (let i = 0; i < action.keysToRemove.length; i++) {
+          delete state[action.keysToRemove[i]];
+        }
+        continue;
+      case `clear`:
+        deleteAllProperties(state);
+        continue;
+      default:
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
