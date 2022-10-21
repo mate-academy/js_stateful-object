a953055d4a0c11bc8c46dcf91be1a74fb4ac1e12
Vadym Baranov <vadym.baranov@hotmail.com>
2022-10-21 19:40:28 +0300

    New solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,14 +5,17 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const action of actions) {
-    switch (action.type) {
+  for (const part of actions) {
+    const { type } = part;
+    const { extraData } = part;
+
+    switch (type) {
       case 'addProperties':
-        Object.assign(state, action.extraData);
+        Object.assign(state, extraData);
         break;
 
       case 'removeProperties':
-        for (const keysToRemove of action.keysToRemove) {
+        for (const keysToRemove of part.keysToRemove) {
           delete state[keysToRemove];
         }
         break;
@@ -27,34 +30,6 @@ function transformState(state, actions) {
         throw new Error('Invalid action');
     }
   }
-
-  return state;
 }
-
-// for (const part of actions) {
-//   const { type } = part;
-//   const { keysToRemove } = part;
-//   const { extraData } = part;
-
-//   switch (type) {
-//     case 'addProperties':
-//       Object.assign(state, extraData);
-//       break;
-
-//     case 'removeProperties':
-//       delete state[keysToRemove];
-//       break;
-
-//     case 'clear':
-//       for (const keys in state) {
-//         delete state[keys];
-//       }
-//       break;
-
-//     default:
-//       throw new Error('Invalid action');
-//   }
-// }
-// }
 
 module.exports = transformState;
