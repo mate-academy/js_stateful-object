08567fcac4935529a4ec6b6163377de0047c7570
taras <nazarko94@gmail.com>
2022-10-26 11:29:21 +0300

    fixes after code review

--- src/transformState.js
+++ src/transformState.js
@@ -8,14 +8,14 @@ const transformState = (state, actions) => {
   for (const action of actions) {
     switch (action.type) {
       case 'clear':
-        for (const item of Object.keys(state)) {
-          delete state[item];
+        for (const key of Object.keys(state)) {
+          delete state[key];
         }
         break;
 
       case 'removeProperties':
-        for (let i = 0; i < action.keysToRemove.length; i++) {
-          delete state[action.keysToRemove[i]];
+        for (const key of action.keysToRemove) {
+          delete state[[key]];
         }
         break;
 
