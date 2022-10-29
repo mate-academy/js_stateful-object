563ac8a34a3a9d2c8674605958cdf33282fd96a6
Saleh BUD <saleh.7734@gmail.com>
2022-10-29 22:05:20 +0300

    Complete

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const res = state;
+
+  for (const eachObj of actions) {
+    if (eachObj.type === 'addProperties') {
+      Object.assign(res, eachObj.extraData);
+    } else if (eachObj.type === 'removeProperties') {
+      for (const key of eachObj.keysToRemove) {
+        delete res[key];
+      }
+    } else if (eachObj.type === 'clear') {
+      for (const each of Object.getOwnPropertyNames(res)) {
+        delete res[each];
+      }
+    } else {}
+  }
+
+  return res;
 }
 
 module.exports = transformState;
