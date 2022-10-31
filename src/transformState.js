eef893666088306e5d1ad8800bf7bb4352373542
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2022-10-31 15:32:42 +0200

    initial

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action in actions) {
+    switch (true) {
+      case actions[action]['type'] === 'addProperties':
+        Object.assign(state, actions[action]['extraData']);
+        break;
+
+      case actions[action]['type'] === 'removeProperties':
+        for (const keyToRemove in actions[action]['keysToRemove']) {
+          const propToRemove = actions[action]['keysToRemove'][keyToRemove];
+
+          if (state.hasOwnProperty(propToRemove)) {
+            delete state[propToRemove];
+          }
+        }
+        break;
+
+      case actions[action]['type'] === 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
