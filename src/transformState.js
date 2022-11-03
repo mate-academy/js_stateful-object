de65e88862d4e57fc5d2f751a8494f5a5d4bfb41
Demut <uddarendeli1@gmail.com>
2022-11-03 14:13:52 +0300

    transformState implemented

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        const obj = Object.entries(action.extraData);
+
+        for (let i = 0; i < obj.length; i++) {
+          const element = obj[i];
+
+          state[element[0]] = element[1];
+        }
+        break;
+      case 'removeProperties':
+        for (let i = 0; i < action.keysToRemove.length; i++) {
+          const element = action.keysToRemove[i];
+
+          if (element in state) {
+            delete state[element];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+    }
+  });
 }
 
 module.exports = transformState;
