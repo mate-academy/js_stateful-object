efa3473841c104e868c23fed75d7cd4636ed080e
Nick Busarhin <busarginkolia@gmail.com>
2022-11-03 13:06:28 +0200

    changedState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,39 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const a of actions) {
+    if (a.type === 'addProperties') {
+      const steps = a.extraData;
+
+      for (const key in steps) {
+        if (!state[key]) {
+          state[key] = steps[key];
+        }
+
+        if (state[key]) {
+          state[key] = steps[key];
+        }
+      }
+    }
+
+    if (a.type === 'removeProperties') {
+      const takeAway = a.keysToRemove;
+
+      for (const name of takeAway) {
+        if (state[name]) {
+          delete state[name];
+        }
+      }
+    }
+
+    if (a.type === 'clear') {
+      for (const n in state) {
+        delete state[n];
+      }
+    }
+  };
+
+  return state;
 }
 
 module.exports = transformState;
