35e738181c563b7818d4d153350f5061695ee74a
Anastasiia Tsvihunova <nastya.tsvigunova@gmail.com>
2022-11-10 12:28:45 +0200

    solution with switch

--- src/transformState.js
+++ src/transformState.js
@@ -8,18 +8,22 @@ function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     const obj = actions[i];
 
-    if (obj.type === 'addProperties') {
-      for (const key in obj.extraData) {
-        state[key] = obj.extraData[key];
-      }
-    } else if (obj.type === 'removeProperties') {
-      for (const key of obj.keysToRemove) {
-        delete state[key];
-      }
-    } else if (obj.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (obj.type) {
+      case 'addProperties' :
+        for (const key in obj.extraData) {
+          state[key] = obj.extraData[key];
+        }
+        break;
+      case 'removeProperties' :
+        for (const key of obj.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear' :
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
