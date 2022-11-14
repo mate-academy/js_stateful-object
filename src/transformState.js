7d4a1d328bf79ba79299e34f66a4381d7c211d1d
Artem Hirzhev <a.hirzhev@gmail.com>
2022-11-14 14:41:50 +0200

    Initial commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const { type, extraData, keysToRemove } = actions[i];
+
+    if (type === 'addProperties') {
+      Object.assign(state, extraData);
+    }
+
+    if (type === 'removeProperties') {
+      for (const key in keysToRemove) {
+        delete state[keysToRemove[key]];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const clear in state) {
+        delete state[clear];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
