bca933441a13b128158ccfc8f3748d5e4ce12ed8
Cate <katerina.ryzhenko@gmail.com>
2022-11-16 14:18:14 +0100

    solution switch fix

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,32 @@
  */
 function transformState(state, actions) {
   for (const key in actions) {
-    if (actions[key].type === 'addProperties') {
-      const extraData = Object.assign({}, actions[key].extraData);
+    switch (actions[key].type) {
+      case 'addProperties':
 
-      Object.assign(state, extraData);
-    } else if (actions[key].type === 'removeProperties') {
-      const keysToRemove = actions[key].keysToRemove;
+        const extraData = Object.assign({}, actions[key].extraData);
 
-      for (const value in keysToRemove) {
-        delete state[keysToRemove[value]];
-      }
-    } else if (actions[key].type === 'clear') {
-      for (const key1 in state) {
-        delete state[key1];
-      }
+        Object.assign(state, extraData);
+
+        break;
+
+      case 'removeProperties':
+
+        const keysToRemove = actions[key].keysToRemove;
+
+        for (const value in keysToRemove) {
+          delete state[keysToRemove[value]];
+        }
+
+        break;
+
+      case 'clear':
+
+        for (const key1 in state) {
+          delete state[key1];
+        }
+
+        break;
     }
   }
 }
