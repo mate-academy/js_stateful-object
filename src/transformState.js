c3e3f5df035dc69643332285f640c5d97d8d5a61
Saleh BUD <saleh.7734@gmail.com>
2022-11-18 14:24:08 +0200

    Fix: Separating the if

--- src/transformState.js
+++ src/transformState.js
@@ -10,15 +10,19 @@ function transformState(state, actions) {
   for (const eachObj of actions) {
     if (eachObj.type === 'addProperties') {
       Object.assign(res, eachObj.extraData);
-    } else if (eachObj.type === 'removeProperties') {
+    }
+
+    if (eachObj.type === 'removeProperties') {
       for (const key of eachObj.keysToRemove) {
         delete res[key];
       }
-    } else if (eachObj.type === 'clear') {
+    }
+
+    if (eachObj.type === 'clear') {
       for (const each of Object.getOwnPropertyNames(res)) {
         delete res[each];
       }
-    } else {}
+    }
   }
 
   return res;
