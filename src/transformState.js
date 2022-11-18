97d9932e45edd7935b5f9333ed746bf237262a85
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2022-11-18 22:23:56 +0200

    delete if/esle and changed it for switch, changed varibles by p2

--- src/transformState.js
+++ src/transformState.js
@@ -4,30 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, [...array]) {
-  const result = state;
+function transformState(state, [...actions]) {
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-  for (const key of array) {
-    for (const value in key) {
-      if (value === 'type' && key[value] !== 'clear') {
-        continue;
-      } else if (value === 'extraData') {
-        Object.assign(result, key[value]);
-      } else if (value === 'keysToRemove') {
-        for (const inner in key[value]) {
-          delete result[key[value][inner]];
+      case 'removeProperties':
+        for (const states of action.keysToRemove) {
+          delete state[states];
         }
-      } else if (key[value] === 'clear') {
-        for (const keys in result) {
-          delete result[keys];
+        break;
+
+      case 'clear':
+        for (const states in state) {
+          delete state[states];
         }
-      } else {
-        return result;
-      }
     }
   }
 
-  return result;
+  return state;
 }
 
 module.exports = transformState;
