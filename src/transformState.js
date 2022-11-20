737d420c41b9d27b08feb3d01cb71511d58d8763
mr-valentos <nerpavlad@gmail.com>
2022-11-20 18:19:33 +0200

    transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach((el) => {
+    switch (el.type) {
+      case 'addProperties':
+        Object.assign(state, el.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of el.keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 module.exports = transformState;
