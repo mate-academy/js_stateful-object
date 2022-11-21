1fbaf75fd2834305b8d9e0cc9537defae1415677
Deniskakaka <krollik1000000@gmail.com>
2022-11-21 19:21:31 +0200

    optimize code

--- src/transformState.js
+++ src/transformState.js
@@ -6,18 +6,24 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    } else if (actions[i].type === 'addProperties') {
-      for (const key in actions[i].extraData) {
-        state[key] = actions[i].extraData[key];
-      }
-    } else if (actions[i].type === 'removeProperties') {
-      for (let q = 0; q < actions[i].keysToRemove.length; q++) {
-        delete state[actions[i].keysToRemove[q]];
-      }
+    switch (actions[i].type) {
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          state[key] = actions[i].extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (let q = 0; q < actions[i].keysToRemove.length; q++) {
+          delete state[actions[i].keysToRemove[q]];
+        }
+        break;
+      default:
+        return state;
     }
   }
 }
