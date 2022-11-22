79cac1f4ed57b3f439e3a2f4085ba9062705b567
Halyna Rydasova <halyna.rydasova@gmail.com>
2022-11-22 03:48:30 +0200

    Fixed

--- src/transformState.js
+++ src/transformState.js
@@ -8,30 +8,25 @@ function transformState(state, actions) {
   actions.forEach((action) => {
     switch (action.type) {
       case 'addProperties':
-        for (const [key, value] of Object.entries(action.extraData)) {
-          state[key] = value;
-        }; break;
-      case 'removeProperties':
-        removeProperties(state, 'removeProperties', action.keysToRemove); break;
-      case 'clear': removeProperties(state, 'clear'); break;
+        Object.assign(state, action.extraData);
+				
+        break;
+
+      case 'removeProperties': {
+        action.keysToRemove.forEach(key => delete state[key]);
+
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
+          delete state[key];
+        };
+
+        break;
+      }
     }
   });
 }
-
-const removeProperties = (fromObj, option, keysArray = []) => {
-  if (Object.keys(fromObj).length > 0) {
-    for (const objectKey of Object.keys(fromObj)) {
-      if (keysArray.length !== 0 && option === 'removeProperties') {
-        keysArray.forEach(keyToRemove => {
-          if (objectKey === keyToRemove) {
-            delete fromObj[objectKey];
-          }
-        });
-      } else if (option === 'clear') {
-        delete fromObj[objectKey];
-      }
-    }
-  }
-};
 
 module.exports = transformState;
