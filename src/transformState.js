61fdbfd23bd1947c7755c63fae9d5d6f3240acd3
Vlad Boikov <vladboikov95@hotmail.com>
2022-11-23 10:01:15 +0200

    Changes applied

--- src/transformState.js
+++ src/transformState.js
@@ -6,18 +6,22 @@
  */
 function transformState(state, actions) {
   for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      for (const key in obj.extraData) {
-        state[key] = obj.extraData[key];
-      }
-    } else if (obj.type === 'removeProperties') {
-      obj.keysToRemove.forEach(el => delete state[el]);
-    } else if (obj.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
-  }
+    switch (obj.type) {
+      case 'addProperties':
+        for (const key in obj.extraData) {
+          state[key] = obj.extraData[key];
+        };
+        break;
+      case 'removeProperties':
+        obj.keysToRemove.forEach(el => delete state[el]);
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    };
+  };
 
   return state;
 }
