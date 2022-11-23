eeb16806665937a014014130917b5ace97279e87
Denis Bokov <denisbokov1703@gmail.com>
2022-11-23 20:45:04 +0100

    statful--object

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,28 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const keys in actions) {
+    switch (actions[keys].type) {
+      case 'addProperties':
+        Object.assign(state, actions[keys].extraData);
+        break;
+
+      case 'removeProperties':
+
+        for (const remove in actions[keys].keysToRemove) {
+          delete state[actions[keys].keysToRemove[remove]];
+        }
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
