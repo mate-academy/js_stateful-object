4bc9bd237a62c654caaeba4ab0be83c385bec8d1
JzSol <111354281+JzSol@users.noreply.github.com>
2022-11-24 16:59:16 +0200

    Finished task

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const object of actions) {
+    if (object.type === 'addProperties') {
+      Object.assign(state, object.extraData);
+    }
+
+    if (object.type === 'removeProperties') {
+      for (const key of object.keysToRemove) {
+        delete state[key];
+      }
+    }
+
+    if (object.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
