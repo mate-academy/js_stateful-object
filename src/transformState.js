4c92cd04f4eb0440a24dcb81a0a285b616131cd9
Kavster92 <kav.athi1992@googlemail.com>
2022-11-25 13:11:57 +0100

    Retrieved State of Object, Transformed State Based on Rules Given

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const order of actions) {
+    switch (order.type) {
+      case 'addProperties':
+        Object.assign(state, order.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const key of order.keysToRemove) {
+          delete state[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+
+        break;
+
+      default:
+        break;
+    }
+  }
 }
+
+/* blank lines added for my clarity */
 
 module.exports = transformState;
