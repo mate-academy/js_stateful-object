1e2bee6854ee04f02b6df87f1f18b1e37d05e076
Illia Shcherbak <illiashcherbak6@gmail.com>
2022-11-27 00:24:41 +0200

    Created transformState function

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action['extraData']);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const removeProp of action['keysToRemove']) {
+        delete state[removeProp];
+      }
+    }
+
+    if (action['type'] === 'clear') {
+      for (const prop in state) {
+        delete state[prop];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
