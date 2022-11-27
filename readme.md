1ec1670882790246cb44ba78773c86d74c94ae86
Mykhailyk Vadym <ihappyspam@gmail.com>
2022-11-27 13:01:27 +0200

    Solution

--- readme.md
+++ readme.md
@@ -10,7 +10,7 @@ should change the `state` basing on the given `actions` array.
 - `state` is an object. You are supposed to add, change, or delete its
   properties instead of creating a new object
 
-- `actions` is an array of objects. Each object in this array has the next properties:
+- `actions` is an array of objects. Each object in this arxray has the next properties:
   - `type` contains a string: either `'addProperties'`, `'removeProperties'` or `'clear'`;
   - The second property of each object depends on `type` and may be one of the following:
     - if `type` is `addProperties`, second property is `extraData`. It contains an object
--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,34 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const changedState = state;
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(changedState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const property of action.keysToRemove) {
+          delete changedState[property];
+        }
+        break;
+
+      case 'clear':
+        for (const key in changedState) {
+          if (changedState.hasOwnProperty(key)) {
+            delete changedState[key];
+          }
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return changedState;
 }
 
 module.exports = transformState;
