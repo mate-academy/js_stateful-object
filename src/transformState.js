bea1c2127c46401d248f5f7865bf6752ef4c0797
Veronika Yasinska <yasinskaa.veronika@gmail.com>
2022-11-28 15:49:34 +0200

    renamed variables

--- src/transformState.js
+++ src/transformState.js
@@ -12,14 +12,14 @@ function transformState(state, actions) {
         break;
 
       case 'removeProperties':
-        for (const propertyToRemove of action.keysToRemove) {
-          delete state[propertyToRemove];
+        for (const keyToRemove of action.keysToRemove) {
+          delete state[keyToRemove];
         }
         break;
 
       case 'clear':
-        for (const clearAll in state) {
-          delete state[clearAll];
+        for (const key in state) {
+          delete state[key];
         }
         break;
 
