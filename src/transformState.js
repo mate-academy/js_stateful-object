56470711592920257aa58ed2cbeba7eac3d29de5
Vadym Prydatok <vadymprydatok@gmail.com>
2022-11-29 14:04:44 +0200

    transform

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,45 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const newObj = {};
+
+  for (const action of actions) {
+    for (const key in action) {
+      // console.log(key);
+      if (action[key] === 'removeProperties') {
+        delete
+        state[action[key]];
+      }
+    }
+  }
+
+  console.log(state);
 }
 
-module.exports = transformState;
+const state = {
+  foo: 'bar', name: 'Jim', another: 'one',
+};
+
+transformState(state, [
+  {
+    type: 'removeProperties', keysToRemove: ['another'],
+  },
+  { type: 'clear' },
+  { type: 'clear' },
+  { type: 'clear' },
+  {
+    type: 'addProperties', extraData: { yet: 'another property' },
+  },
+  { type: 'clear' },
+  {
+    type: 'addProperties',
+    extraData: {
+      foo: 'bar', name: 'Jim',
+    },
+  },
+  {
+    type: 'removeProperties', keysToRemove: ['name', 'hello'],
+  },
+]);
+
+// module.exports = transformState;
