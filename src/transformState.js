633d20e0824c8fdaf694725ae84278a97648aa67
Nikitin Evgeny <ngenya81@gmail.com>
2022-11-30 16:44:42 +0200

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const total = Object.assign({}, actions);
+
+  for (const type in total) {
+    const bot = total[type];
+
+    for (const pool in bot) {
+      if (bot[pool] === 'addProperties') {
+        Object.assign(state, { ...bot.extraData });
+      }
+
+      if (bot[pool] === 'removeProperties') {
+        const bro = bot.keysToRemove;
+
+        for (const n of bro) {
+          delete state[n];
+        }
+      }
+
+      if (bot[pool] === 'clear') {
+        for (const ch in state) {
+          delete state[ch];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
