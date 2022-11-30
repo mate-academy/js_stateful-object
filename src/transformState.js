a78e0a0927274b2d5fc8349736ede535d5278a82
Vadym Prydatok <vadymprydatok@gmail.com>
2022-11-30 14:10:31 +0200

    transform

--- src/transformState.js
+++ src/transformState.js
@@ -5,45 +5,35 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  const newObj = {};
-
   for (const action of actions) {
     for (const key in action) {
-      // console.log(key);
+      if (action[key] === 'addProperties') {
+        Object.assign(state, action.extraData);
+      }
+
       if (action[key] === 'removeProperties') {
         delete
-        state[action[key]];
+        state[action.keysToRemove];
+
+        if (action.keysToRemove.length > 1) {
+          for (const i of action.keysToRemove) {
+            delete
+            state[i];
+          }
+        }
+      }
+
+      if (action[key] === 'clear') {
+        for (const i in state) {
+          if (i in state) {
+            delete state[i];
+          }
+        }
       }
     }
   }
 
-  console.log(state);
+  return state;
 }
 
-const state = {
-  foo: 'bar', name: 'Jim', another: 'one',
-};
-
-transformState(state, [
-  {
-    type: 'removeProperties', keysToRemove: ['another'],
-  },
-  { type: 'clear' },
-  { type: 'clear' },
-  { type: 'clear' },
-  {
-    type: 'addProperties', extraData: { yet: 'another property' },
-  },
-  { type: 'clear' },
-  {
-    type: 'addProperties',
-    extraData: {
-      foo: 'bar', name: 'Jim',
-    },
-  },
-  {
-    type: 'removeProperties', keysToRemove: ['name', 'hello'],
-  },
-]);
-
-// module.exports = transformState;
+module.exports = transformState;
