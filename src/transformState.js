1d87b84f7f067acaf9f0c3fd8ebeb1cb3a34cfba
Sergei Yurchenko <sergyurch@gmail.com>
2022-12-01 13:09:14 +0200

    Add transformState function

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties': {
+        Object.assign(state, action.extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        action.keysToRemove.forEach((key) => {
+          if (key in state) {
+            delete state[key];
+          }
+        });
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
+          delete state[key];
+        }
+      }
+    }
+  });
 }
 
 module.exports = transformState;
