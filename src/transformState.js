2d69e985435cbebf9972c7cfbfd80adca32f795e
spencerspe <spencerspe@gmail.com>
2022-12-07 15:01:50 +0000

    first success

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const changes = Object.values(actions);
+
+  const toRemove = [];
+
+  for (let i = 0; i < changes.length; i++) {
+  // console.log(changes[i])
+    if (changes[i]['type'] === 'addProperties') {
+      Object.assign(state, changes[i]['extraData']);
+    }
+
+    if (changes[i]['type'] === 'removeProperties') {
+      Object.assign(toRemove, changes[i]['keysToRemove']);
+    }
+
+    if (changes[i]['type'] === 'clear') {
+      for (const entries in state) {
+        delete state[entries];
+      }
+    }
+  }
+
+  for (let i = 0; i < toRemove.length; i++) {
+    delete state[toRemove[i]];
+  }
+
+  return state;
 }
 
 module.exports = transformState;
