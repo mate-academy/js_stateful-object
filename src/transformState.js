570f92c502c2669d7fa971bb67b48a44eb91e910
Andrii Derevianko <andrejderevanko@gmail.com>
2022-12-09 21:38:20 +0200

    use switch

--- src/transformState.js
+++ src/transformState.js
@@ -8,16 +8,21 @@ function transformState(state, actions) {
   // write code here
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const keyRemove of action.keysToRemove) {
-        delete state[keyRemove];
-      }
-    } else {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyRemove of action.keysToRemove) {
+          delete state[keyRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 }
