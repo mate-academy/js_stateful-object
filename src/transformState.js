dfbbb54933b9826bcef323145056f89e87b75309
Ilona Romaniuk <ilona09041983@gmail.com>
2022-12-15 22:46:59 +0200

    Function transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const object of actions) {
+    for (const key in object) {
+      if (object[key] === 'addProperties') {
+        Object.assign(state, object.extraData);
+      } else if (object[key] === 'removeProperties') {
+        for (const value of object.keysToRemove) {
+          delete state[value];
+        }
+      } else if (object[key] === 'clear') {
+        for (const i in state) {
+          delete state[i];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
