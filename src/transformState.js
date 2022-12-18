d85e3f3c9784abe3ebe9c20bb92aa3a273c22f77
Ilona Romaniuk <ilona09041983@gmail.com>
2022-12-18 16:51:52 +0200

    Function transformState corrected: 1

--- src/transformState.js
+++ src/transformState.js
@@ -7,16 +7,21 @@
 function transformState(state, actions) {
   for (const object of actions) {
     for (const key in object) {
-      if (object[key] === 'addProperties') {
-        Object.assign(state, object.extraData);
-      } else if (object[key] === 'removeProperties') {
-        for (const value of object.keysToRemove) {
-          delete state[value];
-        }
-      } else if (object[key] === 'clear') {
-        for (const i in state) {
-          delete state[i];
-        }
+      switch (object[key]) {
+        case 'addProperties':
+          Object.assign(state, object.extraData);
+          break;
+
+        case 'removeProperties':
+          for (const value of object.keysToRemove) {
+            delete state[value];
+          }
+          break;
+
+        case 'clear':
+          for (const i in state) {
+            delete state[i];
+          }
       }
     }
   }
