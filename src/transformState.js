32bd56b66b17ac8d985d520acc71f59fb315480f
Taras Baran <baranprivat22@gmail.com>
2022-12-19 23:44:28 +0200

    finished

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(state, obj.extraData);
+    }
+
+    if (obj.type === 'clear') {
+      Object.keys(state).forEach(v => delete state[v]);
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const prop of obj.keysToRemove) {
+        if (state[prop]) {
+          delete state[prop];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
