991a470c7e12fa6cfc82424a9c526e04d13bb5a2
Denys <kolbasin1990@gmail.com>
2022-12-23 21:01:51 +0200

    task_stateful_object_v_01

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  let obj = state;
+
+  for (const key of actions) {
+    if (key.type === 'addProperties') {
+      obj = Object.assign(obj, key.extraData);
+    }
+
+    if (key.type === 'removeProperties') {
+      for (const arr of key.keysToRemove) {
+        delete obj[arr];
+      }
+    }
+
+    if (key.type === 'clear') {
+      for (const objKey in state) {
+        delete obj[objKey];
+      }
+    }
+  }
+
+  return obj;
 }
 
 module.exports = transformState;
