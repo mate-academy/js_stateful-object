7a64e9b6dbd37ad150287cf056553be1d57db525
Tamara-Paskalova <forptv@gmail.com>
2022-12-25 16:20:33 +0200

    add logic to transformState function

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,47 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const type = action.type;
+    const actingProperties = action.extraData || action.keysToRemove;
+
+    performAction(state, type, actingProperties);
+  }
+}
+
+function performAction(state, action, actingProperties) {
+  switch (action) {
+    case 'addProperties':
+      addProperties(state, actingProperties);
+      break;
+    case 'removeProperties':
+      removeProperties(state, actingProperties);
+      break;
+    case 'clear':
+      removeProperties(state);
+      break;
+    default:
+  }
+
+  function addProperties(object, properties) {
+    for (const property in properties) {
+      object[property] = properties[property];
+    }
+  }
+
+  function removeProperties(object, properties) {
+    const shouldClearObject = !properties;
+
+    if (shouldClearObject) {
+      for (const property in object) {
+        delete object[property];
+      }
+    } else {
+      for (const property of properties) {
+        delete object[property];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
