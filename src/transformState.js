fb6af2eeb1ea26360700f6cf9c0c71caf7822384
Roma Tsvindin <tsvindin1995@gmail.com>
2022-12-25 15:00:45 +0200

    Using switch/case instead of if/else

--- src/transformState.js
+++ src/transformState.js
@@ -8,16 +8,20 @@ function transformState(state, actions) {
   const transformedState = state;
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(transformedState, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete transformedState[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const key in transformedState) {
-        delete transformedState[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(transformedState, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete transformedState[key];
+        };
+        break;
+      case 'clear':
+        for (const key in transformedState) {
+          delete transformedState[key];
+        }
+        break;
     }
   }
 
