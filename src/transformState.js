6f73b04c17435c16bddd6adb48ff524a2e4105a9
Bohdan Kutasevich <obvinmusic@gmail.com>
2022-12-28 14:57:57 +0200

    function done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+
+    if (action.type === 'addProperties') {
+  
+      for (const propertie in action.extraData) {
+        state[propertie] = action.extraData[propertie];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+
+      for (const propertie of action.keysToRemove) {
+        delete state[propertie];
+      }
+    }
+
+    if (action.type === 'clear') {
+
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
