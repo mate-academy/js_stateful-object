ffe82117fe682b5f55779bc782a8307bbe711836
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2022-12-28 16:27:06 +0200

    v-01

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const el of actions) {
+    if (el.type === 'addProperties') {
+      Object.assign(state, el.extraData);
+    }
+
+    if (el.type === 'removeProperties') {
+      for (const keysRemove of el.keysToRemove) {
+        delete state[keysRemove];
+      }
+    }
+
+    if (el.type === 'clear') {
+      for (const key in state) {
+        if (state.hasOwnProperty([key])) {
+          delete state[key];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
