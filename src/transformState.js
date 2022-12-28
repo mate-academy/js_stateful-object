21c8d05898e074a8b3a219245eb477b2b8c24d0b
Brandon Sunrise <stepansky93@gmail.com>
2022-12-28 16:33:41 +0200

    Remake with a switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,19 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const keyToRemove of action.keysToRemove) {
-        delete state[keyToRemove];
-      }
-    }
-
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+      default:
+        break;
     }
   }
 }
-
 module.exports = transformState;
