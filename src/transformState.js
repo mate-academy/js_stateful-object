8fdf0876cf16241f0644aa403f91d5b111b7ddca
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2022-12-28 18:25:49 +0200

    fixes after checklist

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action['type']) {
+      case 'addProperties':
+        for (const data in action.extraData) {
+          state[data] = action.extraData[data];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const toRemove of action.keysToRemove) {
+          delete state[toRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const toClear in state) {
+          delete state[toClear];
+        }
+        break;
+
+      default:
+        throw new Error('Unexpected state');
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
