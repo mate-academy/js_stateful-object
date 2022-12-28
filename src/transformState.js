8e1b7520d36effc2284d7f4f9afb0a485ba23b38
svitjojo <kornotavr@gmail.com>
2022-12-28 13:14:59 +0200

    Result

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const prop of actions) {
+    if (prop.type === 'addProperties') {
+      for (const proper in prop.extraData) {
+        state[proper] = prop.extraData[proper];
+      }
+    }
+
+    if (prop.type === 'removeProperties') {
+      for (const proper of prop.keysToRemove) {
+        delete state[proper];
+      }
+    }
+
+    if (prop.type === 'clear') {
+      for (const proper in state) {
+        delete state[proper];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
