e19f5af34389e84773d5261cf5855e7eca286a75
viktor.ionov <viktorionovcvlrs@gmail.com>
2022-12-28 19:50:12 +0100

    Solution with switch/case

--- src/transformState.js
+++ src/transformState.js
@@ -6,24 +6,19 @@
  */
 function transformState(state, actions) {
   for (const transform of actions) {
-    if (transform.type === 'addProperties') {
-      Object.assign(state, transform.extraData);
-    }
-
-    if (transform.type === 'removeProperties') {
-      for (const keyToRemove of transform.keysToRemove) {
-        delete state[keyToRemove];
-      }
-    }
-
-    if (transform.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (transform.type) {
+      case 'addProperties':
+        Object.assign(state, transform.extraData);
+        break;
+      case 'removeProperties':
+        transform.keysToRemove.forEach(keyToRemove =>
+          delete state[keyToRemove]);
+        break;
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
     }
   }
-
-  return state;
 }
 
 module.exports = transformState;
