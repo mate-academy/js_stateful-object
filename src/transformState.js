5db2d12f65eafc04b77dd3cebf261348764c854b
Andrii <kurash.andrii@gmail.com>
2022-12-28 18:23:37 +0200

    finished task Stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  let res = {};
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      res = Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      res = action.keysToRemove.forEach(key => delete state[key]);
+    }
+
+    if (action.type === 'clear') {
+      res = Object.keys(state).forEach(key => delete state[key]);
+    }
+  }
+
+  return res;
 }
 
 module.exports = transformState;
