46b5315fc66f3972563526d1518fa9c7dca945ab
viktor.ionov <viktorionovcvlrs@gmail.com>
2022-12-29 12:18:01 +0100

    Solution with default

--- src/transformState.js
+++ src/transformState.js
@@ -17,6 +17,9 @@ function transformState(state, actions) {
       case 'clear':
         Object.keys(state).forEach(key => delete state[key]);
         break;
+
+      default:
+        throw new Error('unknown action type');
     }
   }
 }
