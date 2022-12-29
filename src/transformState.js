e83e21ba99d7ab19ca96f63638469cb071616fb5
svitjojo <kornotavr@gmail.com>
2022-12-29 12:41:41 +0200

    Variavble for "for of" loop was changed

--- src/transformState.js
+++ src/transformState.js
@@ -8,20 +8,20 @@ function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        for (const proper in action.extraData) {
-          state[proper] = action.extraData[proper];
+        for (const property in action.extraData) {
+          state[property] = action.extraData[property];
         };
         break;
 
       case 'removeProperties':
-        for (const proper of action.keysToRemove) {
-          delete state[proper];
+        for (const property of action.keysToRemove) {
+          delete state[property];
         };
         break;
 
       case 'clear':
-        for (const proper in state) {
-          delete state[proper];
+        for (const property in state) {
+          delete state[property];
         };
         break;
 
