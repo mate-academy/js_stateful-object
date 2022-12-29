2de23ab5fe185de6cc37aff1504f4fcc4b985177
Andrii Andriushchenko <andrewandrew1604@gmail.com>
2022-12-29 13:09:46 +0200

    Useless brackets were removed

--- src/transformState.js
+++ src/transformState.js
@@ -7,17 +7,17 @@
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
-      case ('addProperties'):
+      case 'addProperties':
         Object.assign(state, action.extraData);
         break;
 
-      case ('removeProperties'):
+      case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
 
-      case ('clear'):
+      case 'clear':
         for (const key in state) {
           delete state[key];
         }
