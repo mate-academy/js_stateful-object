a4645f5dfc340bea46608e7ba8858f466218ec04
Alessandra Nitti <alessandrannitti@gmail.com>
2023-01-03 14:45:42 +0100

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key in actions) {
+    if (actions[key].type === 'clear') {
+      for (const property in state) {
+        delete state[property];
+      }
+    }
+
+    if (actions[key].type === 'addProperties') {
+      Object.assign(state, actions[key].extraData);
+    }
+
+    if (actions[key].type === 'removeProperties') {
+      for (const azione in actions[key].keysToRemove) {
+        delete state[actions[key].keysToRemove[azione]];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
