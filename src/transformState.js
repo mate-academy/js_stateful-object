5d1d9b4192b314b9b7c582326c8868f0f1b06791
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-05 15:31:01 +0200

    Finish task

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const prop in action.extraData) {
+        state[prop] = action.extraData[prop];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const prop of action.keysToRemove) {
+        delete state[prop];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const prop in state) {
+        delete state[prop];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
