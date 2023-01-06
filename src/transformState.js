002d3b8c64c1c529fd86e77b2866a1626178e761
Igor Galkin <igor_galkin@ukr.net>
2023-01-06 16:45:01 +0200

    Solution4

--- src/transformState.js
+++ src/transformState.js
@@ -5,8 +5,8 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const arr of actions) {
-    switch (arr['type']) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'clear':
         for (const prop in state) {
           delete state[prop];
@@ -14,13 +14,18 @@ function transformState(state, actions) {
         break;
 
       case 'addProperties':
-        Object.assign(state, arr['extraData']);
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const prop of arr['keysToRemove']) {
+        for (const prop of action.keysToRemove) {
           delete state[prop];
         };
+        break;
+
+      default:
+        // eslint-disable-next-line no-console
+        console.log('Action not found');
     }
   }
 }
