e654c9537f58d6ff3f5d3fd4d7f5ee8fc6cd8b34
Lerimunia <ia.lerimund@gmail.com>
2023-01-09 20:51:55 +0100

    Renamed const of actions

--- src/transformState.js
+++ src/transformState.js
@@ -6,13 +6,13 @@
  */
 function transformState(state, actions) {
   // write code here
-  for (const obj of actions) {
-    switch (obj.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        Object.assign(state, obj.extraData);
+        Object.assign(state, action.extraData);
         break;
       case 'removeProperties':
-        for (const keyword of obj.keysToRemove) {
+        for (const keyword of action.keysToRemove) {
           delete state[keyword];
         };
         break;
