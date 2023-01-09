dd31061982b03e3185d07655ef2a67d87833e99d
Constantin <Constantin.Andreevich.Zhevzhik@gmail.com>
2023-01-09 10:56:07 +0200

    added edits

--- src/transformState.js
+++ src/transformState.js
@@ -5,16 +5,14 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const prop of actions) {
-    switch (prop.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        for (const key in prop.extraData) {
-          state[key] = prop.extraData[key];
-        }
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const key of prop.keysToRemove) {
+        for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
