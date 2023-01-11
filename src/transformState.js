77845c3017420b57cf7bca116f363f73dde3fe1c
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-11 11:55:15 +0100

    Stateful_Object were implemented

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const { type, extraData, keysToRemove } = actions[i];
+
+    if (type === 'addProperties') {
+      Object.assign(state, extraData);
+    } else if (type === 'removeProperties') {
+      for (let j = 0; j < keysToRemove.length; j++) {
+        if (state.hasOwnProperty(keysToRemove[j])) {
+          delete state[keysToRemove[j]];
+        }
+      }
+    } else if (type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
