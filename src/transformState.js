930372f16b001d26c3c62180d8a32a56d2496aa1
Oleksandr Prykhodko <olexandr.prykhodko@gmail.com>
2023-01-13 13:47:11 +0100

    If ... else were changed to Switch ... case

--- src/transformState.js
+++ src/transformState.js
@@ -8,18 +8,27 @@ function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     const { type, extraData, keysToRemove } = actions[i];
 
-    if (type === 'addProperties') {
-      Object.assign(state, extraData);
-    } else if (type === 'removeProperties') {
-      for (let j = 0; j < keysToRemove.length; j++) {
-        if (state.hasOwnProperty(keysToRemove[j])) {
-          delete state[keysToRemove[j]];
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties':
+        for (let j = 0; j < keysToRemove.length; j++) {
+          if (state.hasOwnProperty(keysToRemove[j])) {
+            delete state[keysToRemove[j]];
+          }
         }
-      }
-    } else if (type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        break;
     }
   }
 }
