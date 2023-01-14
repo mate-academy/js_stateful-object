2214511f962f3023282f1d903fca32269647aef7
Oleksandr Maslo <98667890az@gmail.com>
2023-01-14 17:06:45 +0200

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    const type = obj.type;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, obj.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keysRemove of obj.keysToRemove) {
+          delete state[keysRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
