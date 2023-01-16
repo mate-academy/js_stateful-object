ab4938b3f1be7b36b628d78fe1972a8c36d4d44a
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-01-16 21:21:37 +0200

    js_stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const property of actions) {
+    if (property.type === 'addProperties') {
+      Object.assign(state, property.extraData);
+    }
+
+    if (property.type === 'removeProperties') {
+      for (const item of property.keysToRemove) {
+        delete state[item];
+      }
+    }
+
+    if (property.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
