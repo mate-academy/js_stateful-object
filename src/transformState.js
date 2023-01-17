a8c476db684e181c59e58104de4cf66d15d60bc2
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-01-17 17:29:34 +0000

    the task was splved

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const objects of actions) {
+    if (objects.type === 'addProperties') {
+      for (const keys in objects.extraData) {
+        state[keys] = objects.extraData[keys];
+      }
+    } else if (objects.type === 'removeProperties') {
+      for (const remove of objects.keysToRemove) {
+        delete state[remove];
+      }
+    } else {
+      for (const deleteAll in state) {
+        delete state[deleteAll];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
