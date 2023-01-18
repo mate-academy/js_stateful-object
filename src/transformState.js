835be9084aa295a1f1b2f173f8e8d78687b8c280
Mykola Serbin <serbin.nick@gmail.com>
2023-01-18 23:43:37 +0100

    fixed switch

--- src/transformState.js
+++ src/transformState.js
@@ -5,27 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
-    };
-
-    if (actions[i].type === 'removeProperties') {
-      for (let j = 0; j < actions[i].keysToRemove.length; j++) {
-        delete state[actions[i].keysToRemove[j]];
-      }
-    };
-
-    if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    };
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+      default:
+        break;
+    }
   }
-
-  return state;
 }
 
 module.exports = transformState;
