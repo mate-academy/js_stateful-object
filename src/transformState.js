bbdd82ae6bee57f70ed1b66bb33312be3cd23011
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-01-19 15:04:37 +0200

    Changes was added

--- src/transformState.js
+++ src/transformState.js
@@ -6,32 +6,31 @@
  */
 function transformState(state, actions) {
   // write code here
-  const result = state;
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i]['type'] === 'addProperties') {
-      const extraData = actions[i]['extraData'];
+    if (actions[i].type === 'addProperties') {
+      const extraData = actions[i].extraData;
 
       for (const key in extraData) {
-        result[key] = extraData[key];
+        state[key] = extraData[key];
       };
     };
 
-    if (actions[i]['type'] === 'removeProperties') {
-      const keysToRemove = actions[i]['keysToRemove'];
+    if (actions[i].type === 'removeProperties') {
+      const keysToRemove = actions[i].keysToRemove;
 
       for (let j = 0; j < keysToRemove.length; j++) {
-        for (const key in result) {
+        for (const key in state) {
           if (keysToRemove[j] === key) {
-            delete result[key];
+            delete state[key];
           };
         };
       };
     };
 
-    if (actions[i]['type'] === 'clear') {
-      for (const key in result) {
-        delete result[key];
+    if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
       };
     };
   };
