f3dbec49bdba01e2f90787a67bdc7ec184409644
Nadiya Ovsiyenko <nadia.ovsienko1009@gmail.com>
2023-01-19 10:17:59 +0100

    Improved code

--- src/transformState.js
+++ src/transformState.js
@@ -5,23 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      Object.assign(state, obj.extraData);
-    }
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
 
-    if (obj.type === 'removeProperties') {
-      for (const key of obj.keysToRemove) {
-        if (key in state) {
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          if (key in state) {
+            delete state[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
           delete state[key];
         }
-      }
-    }
-
-    if (obj.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
     }
   }
 
