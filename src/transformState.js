0f67b538d9e535f6a982462a62cfad3bfa589cc1
Andrii Khomiak <andr.hamster@gmail.com>
2023-01-19 09:37:18 +0200

    action has been destructured

--- src/transformState.js
+++ src/transformState.js
@@ -6,13 +6,15 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    switch (action.type) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
       case 'addProperties':
-        Object.assign(state, action.extraData);
+        Object.assign(state, extraData);
         break;
 
       case 'removeProperties':
-        for (const item of action.keysToRemove) {
+        for (const item of keysToRemove) {
           delete state[item];
         }
         break;
