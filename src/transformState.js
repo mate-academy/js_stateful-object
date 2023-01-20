833df1ea0e19278d29de6f3cf2865db82140e5a1
Dmitriy Rybak <ryba@MacBook-Air-Dmitriy.local>
2023-01-20 10:05:58 -0600

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,31 +4,21 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, action) {
-  let key;
-  let key2;
-  let key3;
+function transformState(state, actions) {
+  for (let action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
 
-  for (key of action) {
-    if (key.type === 'addProperties') {
-      for (key2 in key.extraData) {
-        state[key2] = key.extraData[key2];
+    if (action.type === 'removeProperties') {
+      for (const keyToRemove of action.keysToRemove) {
+        delete state[keyToRemove];
       }
     }
 
-    if (key.type === 'removeProperties') {
-      for (key2 of key.keysToRemove) {
-        for (key3 in state) {
-          if (key3 === key2) {
-            delete state[key3];
-          }
-        }
-      }
-    }
-
-    if (key.type === 'clear') {
-      for (key in state) {
-        delete state[key];
+    if (action.type === 'clear') {
+      for (action in state) {
+        delete state[action];
       }
     }
   }
