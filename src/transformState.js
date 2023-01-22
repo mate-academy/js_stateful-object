5844d482d2e85fb19797e3d5b969cec5fb2246d8
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-01-22 18:52:34 +0200

    The tests were passed successfully

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const item of actions) {
+    for (const key in item) {
+      if (key === 'extraData') {
+        Object.assign(state, item[key]);
+      }
+
+      if (key === 'keysToRemove') {
+        for (const k in item[key]) {
+          delete state[item[key][k]];
+        }
+      }
+
+      if (item[key] === 'clear') {
+        for (const objKey in state) {
+          delete state[objKey];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
