9cc97f04698f1bfccd887f1e4920eedd9921dc69
Diana Snihur <dianasnigur2@gmail.com>
2023-01-22 16:52:31 +0300

    Fixes

--- src/transformState.js
+++ src/transformState.js
@@ -4,20 +4,26 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const extra in action.extraData) {
-        state[extra] = action.extraData[extra];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const rem in action.keysToRemove) {
-        delete state[action.keysToRemove[rem]];
-      }
-    } else {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        for (const extra in action.extraData) {
+          state[extra] = action.extraData[extra];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const rem in action.keysToRemove) {
+          delete state[action.keysToRemove[rem]];
+        }
+        break;
+
+      default:
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 }
