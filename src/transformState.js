bd33f961b916a79ba2e751df5e74224d58944625
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-23 22:32:26 +0200

    the issues were solved

--- src/transformState.js
+++ src/transformState.js
@@ -7,21 +7,27 @@
 function transformState(state, actions) {
   const copiedState = state;
 
-  for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(copiedState, actions[i].extraData);
-    } else if (actions[i].type === 'removeProperties') {
-      const arr = actions[i].keysToRemove;
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(copiedState, action.extraData);
+        break;
 
-      for (let k = 0; k < arr.length; k++) {
-        delete copiedState[arr[k]];
-      }
-    } else if (actions[i].type === 'clear') {
-      for (const key in copiedState) {
-        if (copiedState.hasOwnProperty(key)) {
-          delete copiedState[key];
+      case 'removeProperties':
+        const arr = action.keysToRemove;
+
+        for (let k = 0; k < arr.length; k++) {
+          delete copiedState[arr[k]];
+        };
+        break;
+
+      case 'clear':
+        for (const key in copiedState) {
+          if (copiedState.hasOwnProperty(key)) {
+            delete copiedState[key];
+          }
         }
-      }
+        break;
     }
   }
 }
