4e9923513e1daea74a6af5c92482e70cc9680bf6
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-23 15:12:47 +0200

    function was created

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const copiedState = state;
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      Object.assign(copiedState, actions[i].extraData);
+    } else if (actions[i].type === 'removeProperties') {
+      const arr = actions[i].keysToRemove;
+
+      for (let k = 0; k < arr.length; k++) {
+        delete copiedState[arr[k]];
+      }
+    } else if (actions[i].type === 'clear') {
+      for (const key in copiedState) {
+        if (copiedState.hasOwnProperty(key)) {
+          delete copiedState[key];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
