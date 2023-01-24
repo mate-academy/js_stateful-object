ce7e4f75777aea5967fc0d52cfce88ebfa918b8a
Diana Snihur <dianasnigur2@gmail.com>
2023-01-24 18:46:20 +0300

    Second fixes

--- src/transformState.js
+++ src/transformState.js
@@ -20,10 +20,14 @@ function transformState(state, actions) {
         }
         break;
 
-      default:
+      case 'clear':
         for (const key in state) {
           delete state[key];
         }
+        break;
+
+      default:
+        return 'ERROR';
     }
   }
 }
