3b06029004aaa958b344b9aa92ead20d08af216d
Oksana Tytanych <oksanatytanych@gmail.com>
2023-01-25 12:53:07 +0200

    two tests are failed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  // console.log(state, actions);
+
+  for (const part in actions) {
+    const action = actions[part];
+
+    if (action.type === 'addProperties') {
+      Object.assign(state, action['extraData']);
+    };
+
+    if (action.type === 'removeProperties') {
+      for (const removeElement in action.keysToRemove) {
+        for (const key in state) {
+          if (key === action.keysToRemove[removeElement]) {
+            delete state[key];
+          }
+        }
+      }
+    }
+
+    if (action.type === 'clear') {
+      // eslint-disable-next-line no-param-reassign
+      state = {};
+    }
+  }
+  // console.log(state);
 }
 
 module.exports = transformState;
