76d74a1d6e25d12a286da74a5aafd3f0331d62c6
danulyk05 <ivan.danulyk2016@gmail.com>
2023-01-28 12:49:12 +0200

    changed

--- src/transformState.js
+++ src/transformState.js
@@ -9,27 +9,28 @@ function transformState(state, actions) {
   const remove = 'removeProperties';
   const clearing = 'clear';
 
-  for (const ch of actions) {
-    if (ch.type === add) {
-      Object.assign(state, ch.extraData);
-    };
+  for (const action of actions) {
+    switch (action.type) {
+      case add:
+        Object.assign(state, action.extraData);
+        break;
 
-    if (ch.type === remove) {
-      for (const ck of ch.keysToRemove) {
-        if (ck in state) {
-          delete state[ck];
+      case remove:
+        for (const ck of action.keysToRemove) {
+          if (ck in state) {
+            delete state[ck];
+          };
         };
-      };
-    };
+        break;
 
-    if (ch.type === clearing) {
-      for (const del in state) {
-        delete state[del];
-      };
+      case clearing:
+        for (const del in state) {
+          delete state[del];
+        };
+        break;
     };
   };
 
   return state;
 };
-
 module.exports = transformState;
