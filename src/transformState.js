019682f6438b883deca824a3678633d0dde4ebde
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-01-29 11:38:26 +0200

    fixed

--- src/transformState.js
+++ src/transformState.js
@@ -8,18 +8,22 @@ function transformState(state, actions) {
   const resultArray = [];
 
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const item in actions[i].extraData) {
-        state[item] = actions[i].extraData[item];
-      }
-    } else if (actions[i].type === 'removeProperties') {
-      for (let j = 0; j < actions[i].keysToRemove.length; j++) {
-        delete state[actions[i].keysToRemove[j]];
-      }
-    } else {
-      for (const item in state) {
-        delete state[item];
-      }
+    switch (true) {
+      case actions[i].type === 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+
+      case actions[i].type === 'removeProperties':
+        for (let j = 0; j < actions[i].keysToRemove.length; j++) {
+          delete state[actions[i].keysToRemove[j]];
+        };
+
+        break;
+
+      default:
+        for (const item in state) {
+          delete state[item];
+        }
     }
 
     resultArray.push({
