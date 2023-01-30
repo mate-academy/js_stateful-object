77aa8083ccc8a05448b4f854755bb8b3073cd423
Bohdan33 <neon3301@gmail.com>
2023-01-30 16:00:05 +0200

    Destructure action

--- src/transformState.js
+++ src/transformState.js
@@ -7,13 +7,15 @@
 function transformState(state, actions) {
   // write code here
   for (const action of actions) {
-    switch (action.type) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
       case 'addProperties':
-        Object.assign(state, action.extraData);
+        Object.assign(state, extraData);
         break;
 
       case 'removeProperties':
-        for (const key of action.keysToRemove) {
+        for (const key of keysToRemove) {
           delete state[key];
         };
         break;
