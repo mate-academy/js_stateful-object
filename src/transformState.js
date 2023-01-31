c23501bcf028050c67bdec4bb7d17e47332e30a4
Serhii Melnychenko <8melya8@gmail.com>
2023-01-31 18:33:41 +0200

    fixed

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  if (actions['type'] === 'addProperties') {
+    for (const key in actions) {
+      if (actions[key] === 'extraData') {
+        for (const add in actions[key]) {
+          state[add] = key[add];
+        }
+      }
+    }
+  }
+
+  if (actions['type'] === 'removeProperties') {
+    for (const remove of actions['keysToRemove']) {
+      delete state[remove];
+    }
+  }
+
+  if (actions['type'] === 'clear') {
+    for (const clear in state) {
+      delete state[clear];
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
