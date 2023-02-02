17db6d26ec0aca35cb87e37f8261d92a5dd0ca2b
Yevhen Prymushko <yevhenpetrov6@gmail.com>
2023-02-02 13:20:39 +0200

    stateful object created

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const toDo in action.extraData) {
+        state[toDo] = action.extraData[toDo];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const remove of action.keysToRemove) {
+        delete state[remove];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const prop in state) {
+        delete state[prop];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
