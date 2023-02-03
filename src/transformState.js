b674816ab6c9e5b46e457233a5c269b6c9a11624
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-02-03 12:33:13 +0200

    Stateful Object v.0.1 - solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        for (const clear in state) {
+          delete state[clear];
+        }
+        break;
+
+      case 'addProperties':
+        for (const add in action) {
+          if (add === 'extraData') {
+            Object.assign(state, action[add]);
+          }
+        }
+        break;
+
+      case 'removeProperties':
+        for (const input of action.keysToRemove) {
+          delete state[input];
+        }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
