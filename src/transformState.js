70ed919dd95d04d8908c36f70c9d482816a25870
Ruslan Liapin <joy.esenin@gmail.com>
2023-02-04 17:43:48 +0200

    Solution with switch. What makes first block redundant?

--- src/transformState.js
+++ src/transformState.js
@@ -9,20 +9,29 @@ function transformState(state, actions) {
   let addedState = {};
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      addedState = action.extraData;
-      Object.assign(state, addedState);
-    } else if (action.type === 'removeProperties') {
-      for (const key in state) {
-        for (const keyRemove of action.keysToRemove) {
-          if (key === keyRemove) {
-            delete state[key];
+    switch (true) {
+      // eslint-disable-next-line no-lone-blocks
+      case action.type === 'addProperties': {
+        addedState = action.extraData;
+        Object.assign(state, addedState);
+        break;
+      };
+
+      case action.type === 'removeProperties': {
+        for (const key in state) {
+          for (const keyRemove of action.keysToRemove) {
+            if (key === keyRemove) {
+              delete state[key];
+            }
           }
-        }
+        };
+        break;
       }
-    } else if (action.type === 'clear') {
-      for (const clearKey in state) {
-        delete state[clearKey];
+
+      case (action.type === 'clear'): {
+        for (const clearKey in state) {
+          delete state[clearKey];
+        }
       }
     }
   }
