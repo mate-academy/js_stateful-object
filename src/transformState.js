9f4cfe7c37ad5594e5926608331ee4f9aa0ca1d8
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-02-05 02:20:01 +0200

    stateful object first commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const refToState = state;
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      const dataToAdd = actions[i].extraData;
+
+      Object.assign(refToState, dataToAdd);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      const keysToRemove = actions[i].keysToRemove;
+
+      for (const key of keysToRemove) {
+        delete refToState[key];
+      }
+    }
+
+    if (actions[i].type === 'clear') {
+      for (const key in refToState) {
+        delete refToState[key];
+      }
+    }
+  }
+
+  return refToState;
 }
 
 module.exports = transformState;
