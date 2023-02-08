5a2efc48123bde7499f5bd08ee0a785c2eeed0c3
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-08 18:22:14 +0200

    Task done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const remProp of action.keysToRemove) {
+          if (state[remProp]) {
+            delete state[remProp];
+          }
+        };
+        break;
+
+      case 'clear':
+        for (const prop in state) {
+          delete state[prop];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
