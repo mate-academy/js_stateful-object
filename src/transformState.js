a22ffa99563fa7eba03189d06a9b5aed31b92e3e
luka4ok <vitalik.shostak@gmail.com>
2023-02-08 20:39:02 +0200

    multiple ifs replaced with switch and used proper const naming

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,25 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+    switch (true) {
+      case (action.type === 'addProperties'):
+        Object.assign(state, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const deleteItem of action.keysToRemove) {
-        delete state[deleteItem];
-      }
-    }
+      case (action.type === 'removeProperties'):
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        };
+        break;
 
-    if (action.type === 'clear') {
-      for (const property in state) {
-        delete state[property];
-      }
+      case (action.type === 'clear'):
+        for (const property in state) {
+          delete state[property];
+        };
+        break;
+
+      default:
+        return state;
     }
   }
 
