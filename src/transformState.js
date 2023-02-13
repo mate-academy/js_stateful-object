4189645deff0ee3f20307948b76241e521a2b4e5
Bogdan <bogdan_mykhailov@icloud.com>
2023-02-13 23:21:50 +0200

    :white_check_mark: implement transform reducer

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const ADD_PROPS = 'addProperties';
+  const REMOVE_PROPS = 'removeProperties';
+  const CLEAR = 'clear';
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ADD_PROPS: {
+        Object.assign(state, action.extraData);
+        break;
+      }
+
+      case REMOVE_PROPS: {
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+      }
+
+      case CLEAR: {
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+      }
+
+      default:
+        throw new Error(`Error in ${action.type} action type`);
+    }
+  }
 }
 
 module.exports = transformState;
