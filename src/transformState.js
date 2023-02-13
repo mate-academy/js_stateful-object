915a3a7b6cb0ac3284027fb121b2ddbc0c8491ad
gretoq <dovhun3d@gmail.com>
2023-02-13 17:12:38 +0200

    Created a solution that helps to edit the object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,47 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const whatWeMustDo = action.type;
+    const isAdding = whatWeMustDo === 'addProperties';
+    const isDeleting = whatWeMustDo === 'removeProperties';
+    const isClearing = whatWeMustDo === 'clear';
+
+    switch (true) {
+      case (isAdding):
+        Object.assign(state, action.extraData);
+        break;
+
+      case (isDeleting):
+        deletProp(state, action.keysToRemove);
+        break;
+
+      case (isClearing):
+        clearingTheObject(state);
+        break;
+
+      default:
+        return 'Unknow error';
+    }
+  }
+
+  return state;
+}
+
+function clearingTheObject(state) {
+  for (const key in state) {
+    delete state[key];
+  }
+
+  return state;
+}
+
+function deletProp(state, props) {
+  for (const prop of props) {
+    delete state[prop];
+  }
+
+  return state;
 }
 
 module.exports = transformState;
