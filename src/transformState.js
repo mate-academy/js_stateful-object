16cdd3cf10554bd2fe3a0542afbd635d036620e3
Tolik Petras <toliketras4@gmail.com>
2023-02-13 20:27:01 +0200

    added default case, delete redundant loop

--- src/transformState.js
+++ src/transformState.js
@@ -6,35 +6,28 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    for (const key in action) {
-      switch (action[key]) {
-        case 'addProperties':
-          const keyValue = Object.entries(action.extraData);
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
 
-          for (const [extraKey, value] of keyValue) {
-            if (action.extraData.hasOwnProperty(extraKey)) {
-              state[extraKey] = value;
-            }
-          }
+        break;
 
-          break;
+      case 'removeProperties':
+        for (const deleteKey of action.keysToRemove) {
+          delete state[deleteKey];
+        }
 
-        case 'removeProperties':
-          for (const deleteKey of action.keysToRemove) {
-            if (state.hasOwnProperty(deleteKey)) {
-              delete state[deleteKey];
-            }
-          }
+        break;
 
-          break;
+      case 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
 
-        case 'clear':
-          for (const stateKey in state) {
-            delete state[stateKey];
-          }
+        break;
 
-          break;
-      }
+      default:
+        continue;
     }
   }
 }
