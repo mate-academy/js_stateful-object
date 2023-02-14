ca288616818b6b0457ef3e1c44bcd1d113146951
Степан <stepan12@MacBook-Air-Stepan.local>
2023-02-14 22:19:26 +0200

    transformState done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const extraDataValues = Object.entries(action.extraData);
+
+      for (const value of extraDataValues) {
+        state[value[0]] = value[1];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      const keysToRemoveValues = action.keysToRemove;
+
+      keysToRemoveValues.every(value => delete state[value]);
+    }
+
+    if (action.type === 'clear') {
+      Object.keys(state).forEach(value => delete state[value]);
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
