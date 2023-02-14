d27675f99ac24beb77167c3d82aeb5c7f035151b
Mark Babich <morak.bobama@gmail.com>
2023-02-14 19:27:47 +0200

    Fixed default, return removed

--- src/transformState.js
+++ src/transformState.js
@@ -8,25 +8,26 @@
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
-      case '': //             does it fit to checklist p.4 ?
-        return '';
       case 'addProperties':
         Object.assign(state, action.extraData);
         break;
+
       case 'removeProperties':
         for (const keyToRemove of action.keysToRemove) {
           delete state[keyToRemove];
         }
         break;
+
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
         break;
+
+      default:
+        break;
     }
   }
-
-  return state;
 }
 
 module.exports = transformState;
