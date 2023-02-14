8ca2551b644ca682750fadb55f51f6405bd6020e
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-02-14 18:37:31 +0200

    Stateful object redone

--- src/transformState.js
+++ src/transformState.js
@@ -4,24 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  for (const item of actions) {
-    for (const key in item) {
-      if (key === 'extraData') {
-        Object.assign(state, item[key]);
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-      if (key === 'keysToRemove') {
-        for (const k in item[key]) {
-          delete state[item[key][k]];
+      case 'removeProperties':
+        for (const item of action.keysToRemove) {
+          delete state[item];
         }
-      }
+        break;
 
-      if (item[key] === 'clear') {
-        for (const objKey in state) {
-          delete state[objKey];
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
         }
-      }
+        break;
+
+      default: break;
     }
   }
 }
