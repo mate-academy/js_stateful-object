910b5c9632f98bcfb73ab12adbeae8f5e2b1e84c
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-02-14 15:56:24 +0100

    create transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      Object.assign(state, act.extraData);
+    }
+
+    if (act.type === 'removeProperties') {
+      const arr = act.keysToRemove;
+
+      for (const key of arr) {
+        delete state[key];
+      }
+    }
+
+    if (act.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
