716188d6312869c56a32985bddfb29b388900f1e
Vladyslav Penkin <vy891078@gmail.com>
2023-02-15 20:11:27 +0200

    develop

--- src/transformState.js
+++ src/transformState.js
@@ -7,24 +7,24 @@
 
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 
   return state;
 }
-
 module.exports = transformState;
