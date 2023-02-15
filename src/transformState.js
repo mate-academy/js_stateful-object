79fa5fa897fc2b48b101b53bf9a2efc2d4189ffe
Andrii Kuznetsov <andreykuzmich91@gmail.com>
2023-02-15 22:11:59 +0200

    statefull solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const ADDPROP = 'addProperties';
+  const REMOVEPROP = 'removeProperties';
+  const CLEAR = 'clear';
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ADDPROP:
+        Object.assign(state, action.extraData);
+        break;
+      case REMOVEPROP:
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case CLEAR:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        throw new Error('Unknown action type');
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
