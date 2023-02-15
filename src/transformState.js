c8bc0ed86613e062cccfe3831a7a7fde2e1da8ad
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-02-15 14:36:36 +0200

    Soliton done v1

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,33 @@
  */
 function transformState(state, actions) {
   // write code here
+  let newState = state;
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        newState = Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in newState) {
+          delete [key];
+          delete newState[key];
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type ${action.type}`);
+    }
+  }
+
+  return newState;
 }
 
 module.exports = transformState;
