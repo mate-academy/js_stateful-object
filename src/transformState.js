bf570008775548cc1baaabe8914c988ba93e482f
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-02-15 09:51:21 +0200

    use switch instead of if

--- src/transformState.js
+++ src/transformState.js
@@ -5,22 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-  for (const key in actions) {
-    if (actions[key].type === 'addProperties') {
-      for (const keys in actions[key].extraData) {
-        state[keys] = actions[key].extraData[keys];
-      }
-    }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-    if (actions[key].type === 'removeProperties') {
-      for (const keys of actions[key].keysToRemove) {
-        delete state[keys];
-      }
-    }
+      case 'removeProperties':
+        for (const keys of action.keysToRemove) {
+          delete state[keys];
+        }
+        break;
 
-    if (actions[key].type === 'clear') {
-      Object.keys(state).forEach(k => delete state[k]);
+      case 'clear':
+        Object.keys(state).forEach(k => delete state[k]);
+        break;
     }
   }
 }
