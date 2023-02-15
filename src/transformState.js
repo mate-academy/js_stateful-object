91c254c872561ee559e391df97e5a7d6de726a37
Yuliia Vereshko <pyliatko@gmail.com>
2023-02-15 19:46:29 +0100

    fixed variable

--- src/transformState.js
+++ src/transformState.js
@@ -5,21 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  const stateModified = state;
-
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        Object.assign(stateModified, action.extraData);
+        Object.assign(state, action.extraData);
         break;
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          delete stateModified[key];
+          delete state[key];
         }
         break;
       case 'clear':
-        for (const key in stateModified) {
-          delete stateModified[key];
+        for (const key in state) {
+          delete state[key];
         }
         break;
       default:
