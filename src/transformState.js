ce22a3b2efec14edfdb3a7e25ef0d8aa323be1e0
Степан <stepan12@MacBook-Air-Stepan.local>
2023-02-16 14:24:17 +0200

    fixed spaces

--- src/transformState.js
+++ src/transformState.js
@@ -15,7 +15,6 @@ function transformState(state, actions) {
     switch (action.type) {
       case ActionType.AddProperties:
         Object.assign(state, action.extraData);
-
         break;
       case ActionType.RemoveProperties:
         for (const key of action.keysToRemove) {
@@ -23,14 +22,12 @@ function transformState(state, actions) {
             delete state[key];
           }
         }
-
         break;
       case ActionType.Clear:
         for (const key in state) {
           delete state[key];
         }
         break;
-
       default:
         throw new Error(`Unknown action type: "${action.type}"`);
     }
