e88ae4fee782c5de2ecc98d08ace34ec7c1184ec
Maksym <maksymmaliuk98@gmail.com>
2023-02-16 11:58:22 +0200

    Fixed solution

--- src/transformState.js
+++ src/transformState.js
@@ -7,23 +7,24 @@
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
-      case 'addProperties': {
+      case 'addProperties':
         Object.assign(state, action.extraData);
         break;
-      }
 
-      case 'removeProperties': {
-        action.keysToRemove.forEach(key => delete state[key]);
+      case 'removeProperties':
+        for (const item of action.keysToRemove) {
+          delete state[item];
+        }
         break;
-      }
 
-      case 'clear': {
-        Object.keys(state).forEach(key => delete state[key]);
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
         break;
-      }
 
       default:
-        Error('Error');
+        throw new Error(`Error in ${action.type} action type`);
     }
   }
 }
