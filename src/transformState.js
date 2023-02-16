36115cd04920dff1cf51eb87ad08ee9a11d256bd
Maksym <maksymmaliuk98@gmail.com>
2023-02-16 12:15:38 +0200

    lint fixes

--- src/transformState.js
+++ src/transformState.js
@@ -9,18 +9,21 @@ function transformState(state, actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(state, action.extraData);
+
         break;
 
       case 'removeProperties':
-        for (const item of action.keysToRemove) {
-          delete state[item];
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
+
         break;
 
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
+
         break;
 
       default:
