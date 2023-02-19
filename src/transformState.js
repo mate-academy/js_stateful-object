8cf688156ea12aa573365bf68d1480d3a14a0909
Ultramarin <000100010111ultramrin@gmail.com>
2023-02-20 01:31:17 +0200

    Finish

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    if (action['type'] === 'addProperties') {
+      Object.keys(action['extraData']).forEach(key => {
+        state[key] = action['extraData'][key];
+      });
+    }
+
+    if (action['type'] === 'removeProperties') {
+      action['keysToRemove'].forEach(key => {
+        delete state[key];
+      });
+    }
+
+    if (action['type'] === 'clear') {
+      Object.keys(state).forEach(key => {
+        delete state[key];
+      });
+    }
+  }
 }
 
 module.exports = transformState;
