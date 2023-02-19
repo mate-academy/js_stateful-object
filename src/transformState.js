7d3d8e7b7b0ab1cd635f162e0f787284d4c233ba
anton-chornobai <antonchornobajj@gmail.com>
2023-02-19 09:43:24 -0800

    Task was redone with switch case and destrocturisation

--- src/transformState.js
+++ src/transformState.js
@@ -5,23 +5,28 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const elem of actions) {
-    if (elem.type === 'addProperties') {
-      Object.assign(state, elem.extraData);
-    }
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
 
-    if (elem.type === 'removeProperties') {
-      for (const keyToRemove of elem.keysToRemove) {
-        delete state[keyToRemove];
-      }
-    }
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
 
-    if (elem.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'removeProperties':
+        for (const keyToRemove of keysToRemove) {
+          delete state[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
 
 module.exports = transformState;
+
