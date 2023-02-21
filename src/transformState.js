dfbc642584e6240273de20d9b4dcd80595700c5d
Maindloll <valeriy.mikheyev@gmail.com>
2023-02-21 18:25:54 +0200

    transform State js

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      const keysToRemove = action.keysToRemove;
+
+      for (let j = 0; j < keysToRemove.length; j++) {
+        const key = keysToRemove[j];
+
+        if (state.hasOwnProperty(key)) {
+          delete state[key];
+        }
+      }
+    } else if (action.type === 'clear') {
+      for (const key in state) {
+        if (state.hasOwnProperty(key)) {
+          delete state[key];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
