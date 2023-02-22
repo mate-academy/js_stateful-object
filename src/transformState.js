deb0a6e5a65c1a89548d90bc7de4f9c66fdc9f01
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-02-22 13:05:10 +0200

    change object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    switch (obj.type) {
+      case 'addProperties':
+        for (const key1 in obj.extraData) {
+          state[key1] = obj.extraData[key1];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key2 of obj.keysToRemove) {
+          delete state[key2];
+        }
+        break;
+
+      case 'clear':
+        for (const key3 in state) {
+          delete state[key3];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
