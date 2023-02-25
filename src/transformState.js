2eba6a51a5d65b0feb96294bbb7f0cf11acda71e
Julia <julia@ocuhealthcare.com>
2023-02-25 16:17:55 -0600

    Solution for Stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      const stateKeys = Object.keys(state);
+
+      for (const key of stateKeys) {
+        delete state[key];
+      }
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+    } else if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+  }
 }
 
 module.exports = transformState;
