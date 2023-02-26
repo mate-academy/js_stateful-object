445088e9c67ff87fd2c0180e56d502768787195b
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-02-26 14:34:46 +0200

    transformState was created

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,35 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const firstObj = actions[i];
+
+    if (firstObj.type === 'addProperties') {
+      const secondObj = firstObj.extraData;
+
+      for (const key in secondObj) {
+        state[key] = secondObj[key];
+      }
+    }
+
+    if (firstObj.type === 'removeProperties') {
+      const secondObj = firstObj.keysToRemove;
+
+      for (const key of secondObj) {
+        delete state[key];
+      }
+    }
+
+    if (firstObj.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
