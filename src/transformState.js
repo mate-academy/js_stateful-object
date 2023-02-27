f10743e8181493c7e1b4a43d5482831b17af9a9b
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-02-27 19:05:30 +0200

    first change

--- src/transformState.js
+++ src/transformState.js
@@ -7,30 +7,32 @@
 
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    const firstObj = actions[i];
+    const current = actions[i];
 
-    if (firstObj.type === 'addProperties') {
-      const secondObj = firstObj.extraData;
+    switch (true) {
+      case current.type === 'addProperties':
+        const first = current.extraData;
 
-      for (const key in secondObj) {
-        state[key] = secondObj[key];
-      }
+        for (const key in first) {
+          state[key] = first[key];
+        }
+        break;
+
+      case current.type === 'removeProperties':
+        const second = current.keysToRemove;
+
+        for (const key of second) {
+          delete state[key];
+        }
+        break;
+
+      case current.type === 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
-
-    if (firstObj.type === 'removeProperties') {
-      const secondObj = firstObj.keysToRemove;
-
-      for (const key of secondObj) {
-        delete state[key];
-      }
-    }
-
-    if (firstObj.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
-  }
+  };
 
   return state;
 }
