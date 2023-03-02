c3340cb3acffa3fc57a06b6d7a27c1f7887d1a2f
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-03-02 18:44:22 +0100

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,51 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const addProperties = 'addProperties';
+  const clear = 'clear';
+  const removeProperties = 'removeProperties';
+
+  for (const arrayItem of actions) {
+    if (isPresent(arrayItem.type, addProperties)) {
+      assignNewDataToState(state, arrayItem);
+    }
+
+    if (isPresent(arrayItem.type, clear)) {
+      const keysState = getSateKeys(state);
+
+      for (const key of keysState) {
+        deleteFromState(state, key);
+      }
+    }
+
+    if (isPresent(arrayItem.type, removeProperties)) {
+      for (const keyToRemove of getRemoveKeys(arrayItem)) {
+        deleteFromState(state, keyToRemove);
+      }
+    }
+  }
+
+  return state;
+}
+
+function assignNewDataToState(state, arrayItem) {
+  Object.assign(state, arrayItem.extraData);
+}
+
+function deleteFromState(state, key) {
+  delete state[key];
+}
+
+function getRemoveKeys(arrayItem) {
+  return arrayItem.keysToRemove;
+}
+
+function getSateKeys(myObject) {
+  return Object.keys(myObject);
+}
+
+function isPresent(param1, param2) {
+  return param1 === param2;
 }
 
 module.exports = transformState;
