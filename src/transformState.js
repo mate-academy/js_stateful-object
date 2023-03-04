3c3d2f385b9355ff66c23ec6ce485f2013503333
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-03-04 15:19:27 +0200

    task complete

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,31 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        });
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+    }
+  });
 }
 
 module.exports = transformState;
