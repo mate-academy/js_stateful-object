c5bdaeaff6dfaafea5944038dffa5c8e168a6574
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-03-06 01:01:54 +0200

    Fedenko-Stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const newState = state;
+
+  for (const actionProperties of actions) {
+    const { type, extraData, keysToRemove } = actionProperties;
+
+    if (type === 'addProperties') {
+      Object.assign(newState, extraData);
+    }
+
+    if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const key in newState) {
+        delete newState[key];
+      }
+    }
+  }
+
+  return newState;
 }
 
 module.exports = transformState;
