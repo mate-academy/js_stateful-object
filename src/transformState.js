a3c014367553b451599668b75f39362d6bbab9cb
Antonina Klishch <tonya91t@gmail.com>
2023-03-07 12:30:30 +0200

    Fixed

--- src/transformState.js
+++ src/transformState.js
@@ -5,23 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const { type, extraData, keysToRemove, clear } of actions) {
+  for (const { type, extraData, keysToRemove } of actions) {
     switch (type) {
       case 'addProperties':
         Object.assign(state, extraData);
         break;
-        
+
       case 'removeProperties':
         for (const key of keysToRemove) {
           delete state[key];
         }
         break;
-        
-      default:
+
+      case 'clear':
         for (const key1 in state) {
           delete state[key1];
         }
         break;
+
+      default:
+        return state;
     }
   }
 
