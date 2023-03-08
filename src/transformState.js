bcaaab3ec7b8d8dd0a7960882e3830f375de7c77
Yuri Shkrobut <georgebest2409@gmail.com>
2023-03-08 12:44:25 +0200

    Simplified to one loop

--- src/transformState.js
+++ src/transformState.js
@@ -7,24 +7,22 @@
 
 function transformState(state, actions) {
   for (const action of actions) {
-    for (const key in action) {
-      switch (action[key]) {
-        case 'clear':
-          for (const key2 in state) {
-            delete state[key2];
-          }
-          break;
+    switch (action.type) {
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
 
-        case 'addProperties':
-          Object.assign(state, action['extraData']);
-          break;
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-        case 'removeProperties':
-          for (const item of action['keysToRemove']) {
-            delete state[item];
-          }
-          break;
-      }
+      case 'removeProperties':
+        for (const item of action.keysToRemove) {
+          delete state[item];
+        }
+        break;
     }
   }
 
