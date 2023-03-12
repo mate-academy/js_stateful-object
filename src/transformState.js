c34c979e3db6378177a4bd76938601b47ab7c5fe
domkab <dominykas.bobinas@gmail.com>
2023-03-12 13:34:07 +0200

    solution 1.0.0

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,25 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
