6be0a4e37aa8f253e7e92dec95364371f9885dc7
domkab <dominykas.bobinas@gmail.com>
2023-03-12 13:40:42 +0200

    solution 1.0.1 switch case

--- src/transformState.js
+++ src/transformState.js
@@ -1,21 +1,45 @@
 'use strict';
 
+// function transformState(state, actions) {
+//   for (const action of actions) {
+//     if (action.type === 'clear') {
+//       for (const key in state) {
+//         delete state[key];
+//       }
+//     }
+
+//     if (action.type === 'addProperties') {
+//       Object.assign(state, action.extraData);
+//     }
+
+//     if (action.type === 'removeProperties') {
+//       for (const key of action.keysToRemove) {
+//         delete state[key];
+//       }
+//     }
+//   }
+
+//   return state;
+// }
+
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
-    }
+    switch (action.type) {
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
 
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
     }
   }
 
