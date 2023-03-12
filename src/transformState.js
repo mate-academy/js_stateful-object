40b8233faa627ffe073c5c2a8c56bcff0d64e414
Venher Vitalii <vengervn@gmail.com>
2023-03-12 13:02:28 +0200

    added function transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,22 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const { type, keysToRemove, extraData } of actions) {
+    switch (type) {
+      case 'removeProperties':
+        for (const key of keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+    }
+  }
 }
 
 module.exports = transformState;
