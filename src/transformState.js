c3f1b5348c6e019697111a0d6a73600ac22457d3
Vadym Dubniak <dubnyakv@gmail.com>
2023-03-13 17:57:31 +0200

    added transformState function realisation

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,47 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  let transformedState = state;
+
+  for (const action in actions) {
+    switch (actions[action].type) {
+      case 'addProperties' :
+        addProperties(transformedState, actions[action]);
+
+        break;
+      case 'removeProperties' :
+        removeProperties(transformedState, actions[action]);
+
+        break;
+
+      default :
+        clearState(transformedState);
+
+        break;
+    }
+  }
+
+  return transformedState;
+}
+
+function addProperties(state, action) {
+  Object.assign(state, action.extraData);
+}
+
+function removeProperties(state, action) {
+  for (const key in state) {
+    for (const keyToRemove in action.keysToRemove) {
+      if (action.keysToRemove[keyToRemove] === key) {
+        delete state[key];
+      }
+    }
+  }
+}
+
+function clearState(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformState;
