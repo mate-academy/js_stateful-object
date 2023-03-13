8dcadb28b970524b187eacabe4228c9c4899c92a
victor buldenko <gookidoo@gmail.com>
2023-03-13 19:09:18 +0300

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const obj = state;
+
+  actions.forEach(element => {
+    if (element['type'] === 'clear') {
+      for (const key in obj) {
+        delete obj[key];
+      }
+    } else if (element['type'] === 'removeProperties') {
+      element['keysToRemove'].forEach(prop => {
+        delete obj[prop];
+      });
+    } else {
+      Object.assign(obj, element['extraData']);
+    }
+  });
+
+  return obj;
 }
 
 module.exports = transformState;
