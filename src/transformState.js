a0df44006d862eaa919b940fa238fe052dfa19e5
Mark Boiko <boikomark555@gmail.com>
2023-03-13 18:47:40 +0200

    create transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  const isAddProperties = 'addProperties';
+  const isRemoveProperties = 'removeProperties';
+  const isClear = 'clear';
 
+  for (const obj of actions) {
+    if (obj.type === isAddProperties) {
+      Object.assign(state, obj.extraData);
+    }
+
+    if (obj.type === isRemoveProperties) {
+      obj.keysToRemove.forEach(elem => delete state[elem]);
+    }
+
+    if (obj.type === isClear) {
+      for (const elem in state) {
+        delete state[elem];
+      }
+    }
+  }
+
+  return state;
+}
 module.exports = transformState;
