1d0c13d4f36526a82a7f7ed7b18813d65da840f6
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-13 19:08:49 +0200

    added transform function

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    if (actions[i].type === 'addProperties') {
+      Object.assign(state, actions[i].extraData);
+    }
+
+    if (actions[i].type === 'removeProperties') {
+      for (let x = 0; x < actions[i].keysToRemove.length; x++) {
+        if (state[actions[i].keysToRemove[x]] !== undefined) {
+          delete state[actions[i].keysToRemove[x]];
+        }
+      }
+    }
+  }
 }
 
 module.exports = transformState;
