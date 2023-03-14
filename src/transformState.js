e84dcd8afe9550c3a316b8d32c0b82a3d827e896
Rostyslav Hirin <hirin.rostyslav@gmail.com>
2023-03-15 00:02:49 +0200

    State transformation function implemented

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const transformedState = state;
+
+  actions.forEach(action => {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          transformedState[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete transformedState[key]);
+        break;
+
+      case 'clear':
+        for (const key in transformedState) {
+          delete transformedState[key];
+        }
+        break;
+
+      default:
+        throw new Error('Something went wrong!');
+    }
+  });
 }
 
 module.exports = transformState;
