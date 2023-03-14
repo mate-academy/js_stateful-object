4ecfccf19535c00cb84e3c14d3a31d201162e86c
Mark Boiko <boikomark555@gmail.com>
2023-03-14 15:12:37 +0200

    improve function transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,22 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const obj of actions) {
-    switch (obj.type) {
+  for (const currentObject of actions) {
+    switch (currentObject.type) {
       case 'addProperties':
-        Object.assign(state, obj.extraData);
+        Object.assign(state, currentObject.extraData);
         break;
       case 'removeProperties':
-        obj.keysToRemove.forEach(elem => delete state[elem]);
+        currentObject.keysToRemove.forEach(elem => delete state[elem]);
         break;
 
       case 'clear':
-        for (const elem in state) {
-          delete state[elem];
-        }
+        toClearState(state);
+        break;
+      default:
+        return null;
     }
   }
 
   return state;
+}
+
+function toClearState(state) {
+  for (const elem in state) {
+    delete state[elem];
+  }
 }
 module.exports = transformState;
