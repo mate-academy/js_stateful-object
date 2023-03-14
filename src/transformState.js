f24d35d22db16a15baa3965cc9234bd2e6665969
Oleksandr <stambulyak1012@gmail.com>
2023-03-14 15:39:48 +0200

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,33 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const err1 = Error('Something went wrong');
+
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        Object.assign(state, act.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const removeKeys of act.keysToRemove) {
+          delete state[removeKeys];
+        }
+        break;
+
+      // eslint-disable-next-line no-fallthrough
+      case 'clear':
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+        break;
+
+      default:
+        throw err1;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
