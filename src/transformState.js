8a2920be8a668071a5a2291468cecea479012380
Daria <hamdsad8@gmail.com>
2023-03-14 13:28:41 +0200

    Added Object.assign

--- src/transformState.js
+++ src/transformState.js
@@ -8,9 +8,7 @@ function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        for (const key in action.extraData) {
-          state[key] = action.extraData[key];
-        }
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
