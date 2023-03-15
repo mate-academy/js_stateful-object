12171f191e189c7c6bd06030eaaa61aa16c23b75
Alex Rog <sasharg303@gmail.com>
2023-03-15 23:56:29 +0100

    version1

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const iterator of actions) {
+    if (iterator.type === 'addProperties') {
+      for (const key in iterator.extraData) {
+        state[key] = iterator.extraData[key];
+      }
+    } else if (iterator.type === 'removeProperties') {
+      for (const iterator1 of iterator.keysToRemove) {
+        delete state[iterator1];
+      }
+    } else if (iterator.type === 'clear') {
+      for (const key1 in state) {
+        delete state[key1];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
