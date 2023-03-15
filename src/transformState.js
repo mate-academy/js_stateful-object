74424abe28e9080ea5a16ea2a821eba138a08bde
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-15 22:33:54 +0200

    one loop was removed, for loop and switch loops were optimized

--- src/transformState.js
+++ src/transformState.js
@@ -7,24 +7,22 @@
 function transformState(state, actions) {
   try {
     for (const action of actions) {
-      for (const type in action) {
-        switch (true) {
-          case type === 'extraData':
-            for (const fieldToAdd in action[type]) {
-              state[fieldToAdd] = action[type][fieldToAdd];
-            };
-            break;
-          case type === 'keysToRemove':
-            for (const keyToRemove of action[type]) {
-              delete state[keyToRemove];
-            };
-            break;
-          case action[type] === 'clear':
-            for (const key in state) {
-              delete state[key];
-            };
-            break;
-        }
+      switch (action['type']) {
+        case 'addProperties':
+          for (const fieldToAdd in action['extraData']) {
+            state[fieldToAdd] = action['extraData'][fieldToAdd];
+          };
+          break;
+        case 'removeProperties':
+          for (const keyToRemove of action['keysToRemove']) {
+            delete state[keyToRemove];
+          };
+          break;
+        case 'clear':
+          for (const key in state) {
+            delete state[key];
+          };
+          break;
       }
     }
   } catch (error) {
