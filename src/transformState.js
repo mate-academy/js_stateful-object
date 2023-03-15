cd5aedebed9d7863ef65cc8c7bbe17dc31a1a81f
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-03-15 15:47:51 +0200

    function works

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const keyForState of action.keysToRemove) {
+        if (state[keyForState]) {
+          delete state[keyForState];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
