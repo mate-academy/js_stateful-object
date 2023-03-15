2c34e5be7aa33a0b89798da932679199d14034f2
kukla1989 <kukla.1989@ukr.net>
2023-03-15 09:24:18 +0200

    create function for stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      addProp(state, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach(key => delete state[key]);
+    } else if (action.type === 'clear') {
+      Object.keys(state).forEach(key => delete state[key]);
+    }
+  }
+
+  return state;
+}
+
+function addProp(state, properties) {
+  for (const key in properties) {
+    state[key] = properties[key];
+  }
 }
 
 module.exports = transformState;
