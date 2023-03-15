28fa96ed5b2f887489d648e94c6ae23472a96591
kukla1989 <kukla.1989@ukr.net>
2023-03-15 21:02:29 +0200

    raplace if on switch

--- src/transformState.js
+++ src/transformState.js
@@ -6,12 +6,17 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      action.keysToRemove.forEach(key => delete state[key]);
-    } else if (action.type === 'clear') {
-      Object.keys(state).forEach(key => delete state[key]);
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
     }
   }
 
