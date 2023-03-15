67487a951c399cb9ea66cb90325cfafd4cc6fb4c
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-15 19:30:01 +0200

    if replaced with switch

--- src/transformState.js
+++ src/transformState.js
@@ -8,22 +8,22 @@ function transformState(state, actions) {
   try {
     for (const action of actions) {
       for (const type in action) {
-        if (type === 'extraData') {
-          for (const fieldToAdd in action[type]) {
-            state[fieldToAdd] = action[type][fieldToAdd];
-          }
-        }
-
-        if (type === 'keysToRemove') {
-          for (const keyToRemove of action[type]) {
-            delete state[keyToRemove];
-          }
-        }
-
-        if (action[type] === 'clear') {
-          for (const key in state) {
-            delete state[key];
-          }
+        switch (true) {
+          case type === 'extraData':
+            for (const fieldToAdd in action[type]) {
+              state[fieldToAdd] = action[type][fieldToAdd];
+            };
+            break;
+          case type === 'keysToRemove':
+            for (const keyToRemove of action[type]) {
+              delete state[keyToRemove];
+            };
+            break;
+          case action[type] === 'clear':
+            for (const key in state) {
+              delete state[key];
+            };
+            break;
         }
       }
     }
