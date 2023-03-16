d7b3f0032f8b8cc7e65bdcc6a9e36aa7228cebc4
Alex Rog <sasharg303@gmail.com>
2023-03-16 14:22:55 +0100

    addSwitch

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const iterator of actions) {
-    if (iterator.type === 'addProperties') {
-      for (const key in iterator.extraData) {
-        state[key] = iterator.extraData[key];
-      }
-    } else if (iterator.type === 'removeProperties') {
-      for (const iterator1 of iterator.keysToRemove) {
-        delete state[iterator1];
-      }
-    } else if (iterator.type === 'clear') {
-      for (const key1 in state) {
-        delete state[key1];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw Error('Invalid data');
     }
   }
 }
