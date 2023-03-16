65620fe78cbc141af5b203d636d78beec5483f1d
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-16 11:40:21 +0200

    unnecessary quotes calles removed, loop replaced with object.assign, try-catch replaced with default error handler

--- src/transformState.js
+++ src/transformState.js
@@ -5,28 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  try {
-    for (const action of actions) {
-      switch (action['type']) {
-        case 'addProperties':
-          for (const fieldToAdd in action['extraData']) {
-            state[fieldToAdd] = action['extraData'][fieldToAdd];
-          };
-          break;
-        case 'removeProperties':
-          for (const keyToRemove of action['keysToRemove']) {
-            delete state[keyToRemove];
-          };
-          break;
-        case 'clear':
-          for (const key in state) {
-            delete state[key];
-          };
-          break;
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete state[keyToRemove];
+        };
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+
+      default:
+        throw new Error('Invalid operations');
     }
-  } catch (error) {
-    throw new Error('Invalid operation');
   }
 
   return state;
