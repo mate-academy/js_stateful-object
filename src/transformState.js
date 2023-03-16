06c9878f244531392f9df964a95f9d7bf661837f
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-03-16 11:15:55 +0200

    stateful object initial

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,38 @@
  */
 function transformState(state, actions) {
   // write code here
+  const transformed = [];
+
+  for (const object of actions) {
+    switch (object.type) {
+      case 'addProperties': {
+        Object.assign(state, object.extraData);
+        transformed.push(state);
+        break;
+      }
+
+      case 'clear': {
+        for (const item in state) {
+          delete state[item];
+        }
+        transformed.push({});
+        break;
+      }
+
+      case 'removeProperties': {
+        for (const remove of object.keysToRemove) {
+          delete state[remove];
+        }
+        transformed.push(state);
+        break;
+      }
+
+      default:
+        return object.type;
+    }
+  }
+
+  return transformed;
 }
 
 module.exports = transformState;
