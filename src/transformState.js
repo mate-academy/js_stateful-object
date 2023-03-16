0dc3004aaadf9b1f6258d7ffdcf83c3df8d8563e
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-03-16 09:35:59 +0200

    switch case solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const relatedStateKey of action.keysToRemove) {
+          if (state[relatedStateKey]) {
+            delete state[relatedStateKey];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw new Error('Something went wrong');
+    }
+  }
 }
 
 module.exports = transformState;
