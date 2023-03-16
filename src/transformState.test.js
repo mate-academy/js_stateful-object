ae22a56fd3987ab08649518389dc802288fd5d2c
swiftbuddy03 <bigguru03@gmail.com>
2023-03-16 21:11:07 +0300

    solution

--- src/transformState.test.js
+++ src/transformState.test.js
@@ -7,12 +7,12 @@ test('Should add a single property to an empty state', () => {
 
   transformState(state, [
     {
-      type: 'addProperties', extraData: { name: 'Jim' },
+      type: 'addProperties',
+      extraData: { name: 'Jim' },
     },
   ]);
 
-  expect(state)
-    .toEqual({ name: 'Jim' });
+  expect(state).toEqual({ name: 'Jim' });
 });
 
 test('Should add multiple properties to an empty state', () => {
@@ -22,55 +22,63 @@ test('Should add multiple properties to an empty state', () => {
     {
       type: 'addProperties',
       extraData: {
-        name: 'Jim', hello: 'world',
+        name: 'Jim',
+        hello: 'world',
       },
     },
   ]);
 
-  expect(state)
-    .toEqual({
-      name: 'Jim', hello: 'world',
-    });
+  expect(state).toEqual({
+    name: 'Jim',
+    hello: 'world',
+  });
 });
 
 test('Should add multiple properties to non empty state', () => {
   const state = {
-    foo: 'bar', bar: 'foo',
+    foo: 'bar',
+    bar: 'foo',
   };
 
   transformState(state, [
     {
       type: 'addProperties',
       extraData: {
-        name: 'Jim', hello: 'world',
+        name: 'Jim',
+        hello: 'world',
       },
     },
   ]);
 
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-    });
+  expect(state).toEqual({
+    foo: 'bar',
+    bar: 'foo',
+    name: 'Jim',
+    hello: 'world',
+  });
 });
 
 test('Should replace existing properties', () => {
   const state = {
-    foo: 'bar', bar: 'foo',
+    foo: 'bar',
+    bar: 'foo',
   };
 
   transformState(state, [
     {
       type: 'addProperties',
       extraData: {
-        foo: 'new', hello: 'world',
+        foo: 'new',
+        hello: 'world',
       },
     },
   ]);
 
-  expect(state)
-    .toEqual({
-      foo: 'new', bar: 'foo', hello: 'world',
-    });
+  expect(state).toEqual({
+    foo: 'new',
+    bar: 'foo',
+    hello: 'world',
+  });
 });
 
 test('Should remove the only property from the state', () => {
@@ -78,155 +86,176 @@ test('Should remove the only property from the state', () => {
 
   transformState(state, [
     {
-      type: 'removeProperties', keysToRemove: ['foo'],
+      type: 'removeProperties',
+      keysToRemove: ['foo'],
     },
   ]);
 
-  expect(state)
-    .toEqual({});
+  expect(state).toEqual({});
 });
 
 test('Should remove an existing property keeping the other ones', () => {
   const state = {
-    foo: 'bar', bar: 'foo',
+    foo: 'bar',
+    bar: 'foo',
   };
 
   transformState(state, [
     {
-      type: 'removeProperties', keysToRemove: ['bar'],
+      type: 'removeProperties',
+      keysToRemove: ['bar'],
     },
   ]);
 
-  expect(state)
-    .toEqual({ foo: 'bar' });
+  expect(state).toEqual({ foo: 'bar' });
 });
 
 test('Should remove several properties', () => {
   const state = {
-    foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
+    foo: 'bar',
+    bar: 'foo',
+    name: 'Jim',
+    hello: 'world',
   };
 
   transformState(state, [
     {
-      type: 'removeProperties', keysToRemove: ['hello', 'foo', 'name'],
+      type: 'removeProperties',
+      keysToRemove: ['hello', 'foo', 'name'],
     },
   ]);
 
-  expect(state)
-    .toEqual({ bar: 'foo' });
+  expect(state).toEqual({ bar: 'foo' });
 });
 
 test('Should do nothing when removing no properties', () => {
   const state = {
-    foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
+    foo: 'bar',
+    bar: 'foo',
+    name: 'Jim',
+    hello: 'world',
   };
 
   transformState(state, [
     {
-      type: 'removeProperties', keysToRemove: [],
+      type: 'removeProperties',
+      keysToRemove: [],
     },
   ]);
 
-  expect(state)
-    .toEqual({
-      foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
-    });
+  expect(state).toEqual({
+    foo: 'bar',
+    bar: 'foo',
+    name: 'Jim',
+    hello: 'world',
+  });
 });
 
 test('Should not fail when removing not existing property', () => {
   const state = {
-    foo: 'bar', bar: 'foo', name: 'Jim', hello: 'world',
+    foo: 'bar',
+    bar: 'foo',
+    name: 'Jim',
+    hello: 'world',
   };
 
   transformState(state, [
     {
-      type: 'removeProperties', keysToRemove: ['test', 'bar'],
+      type: 'removeProperties',
+      keysToRemove: ['test', 'bar'],
     },
   ]);
 
-  expect(state)
-    .toEqual({
-      foo: 'bar', name: 'Jim', hello: 'world',
-    });
+  expect(state).toEqual({
+    foo: 'bar',
+    name: 'Jim',
+    hello: 'world',
+  });
 });
 
 test('Should clear the state', () => {
   const state = {
-    foo: 'bar', name: 'Jim', another: 'one',
+    foo: 'bar',
+    name: 'Jim',
+    another: 'one',
   };
 
-  transformState(state, [
-    { type: 'clear' },
-  ]);
+  transformState(state, [{ type: 'clear' }]);
 
-  expect(state)
-    .toEqual({});
+  expect(state).toEqual({});
 });
 
 test('Should not fails when clearing an empty state', () => {
   const state = {};
 
-  transformState(state, [
-    { type: 'clear' },
-  ]);
+  transformState(state, [{ type: 'clear' }]);
 
-  expect(state)
-    .toEqual({});
+  expect(state).toEqual({});
 });
 
 test('Should apply several types', () => {
   const state = {
-    foo: 'bar', bar: 'foo',
+    foo: 'bar',
+    bar: 'foo',
   };
 
   transformState(state, [
     {
       type: 'addProperties',
       extraData: {
-        name: 'Jim', hello: 'world',
+        name: 'Jim',
+        hello: 'world',
       },
     },
     {
-      type: 'removeProperties', keysToRemove: ['bar', 'hello'],
+      type: 'removeProperties',
+      keysToRemove: ['bar', 'hello'],
     },
     {
-      type: 'addProperties', extraData: { another: 'one' },
+      type: 'addProperties',
+      extraData: { another: 'one' },
     },
   ]);
 
-  expect(state)
-    .toEqual({
-      foo: 'bar', name: 'Jim', another: 'one',
-    });
+  expect(state).toEqual({
+    foo: 'bar',
+    name: 'Jim',
+    another: 'one',
+  });
 });
 
 test('Should work with a long list of types', () => {
   const state = {
-    foo: 'bar', name: 'Jim', another: 'one',
+    foo: 'bar',
+    name: 'Jim',
+    another: 'one',
   };
 
   transformState(state, [
     {
-      type: 'removeProperties', keysToRemove: ['another'],
+      type: 'removeProperties',
+      keysToRemove: ['another'],
     },
     { type: 'clear' },
     { type: 'clear' },
     { type: 'clear' },
     {
-      type: 'addProperties', extraData: { yet: 'another property' },
+      type: 'addProperties',
+      extraData: { yet: 'another property' },
     },
     { type: 'clear' },
     {
       type: 'addProperties',
       extraData: {
-        foo: 'bar', name: 'Jim',
+        foo: 'bar',
+        name: 'Jim',
       },
     },
     {
-      type: 'removeProperties', keysToRemove: ['name', 'hello'],
+      type: 'removeProperties',
+      keysToRemove: ['name', 'hello'],
     },
   ]);
 
-  expect(state)
-    .toEqual({ foo: 'bar' });
+  expect(state).toEqual({ foo: 'bar' });
 });
