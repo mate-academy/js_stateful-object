b7fa7899fa76f56dc1c7fd9cc5dff2635fef3cab
Mys Yaroslav <gotluck13@gmail.com>
2023-03-16 12:33:57 +0200

    corrected solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,27 +6,28 @@
  */
 
 function transformState(state, actions) {
-
   for (const action of actions) {
-    
-    switch (action['type']) {
+    switch (action.type) {
       case 'addProperties':
         for (const key in action['extraData']) {
           state[key] = action['extraData'][key];
-          };
+        };
         break;
 
       case 'removeProperties':
         for (const removeKey of action['keysToRemove']) {
           delete state[removeKey];
-          };
+        };
         break;
 
       case 'clear':
         for (const key in state) {
           delete state[key];
-          };
+        };
         break;
+
+      default:
+        throw new Error(`Input is invalid - ${action.type}`);
     }
   }
 
