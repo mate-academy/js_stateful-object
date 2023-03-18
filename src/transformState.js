0fe301b065d41ad828ea22abb5bb45cd7309cab6
Dima <dmitriy.movchan.02@gmail.com>
2023-03-18 21:38:29 +0200

    fix spaces, usless validation and add default to switch

--- src/transformState.js
+++ src/transformState.js
@@ -10,18 +10,21 @@ function transformState(state, actions) {
       case 'addProperties':
         Object.assign(state, action.extraData);
         break;
+
       case 'removeProperties':
         for (const key of action.keysToRemove) {
-          if (state.hasOwnProperty(key)) {
-            delete state[key];
-          }
+          delete state[key];
         }
         break;
+
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
         break;
+
+      default:
+        throw new Error(`Unsupported action type: ${action.type}`);
     }
   }
 }
