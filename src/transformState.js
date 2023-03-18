5f9866811581487c04482c91a070de5b16d35c3e
Natali <swetswet1234@gmail.com>
2023-03-18 22:31:11 +0200

    develop

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,27 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const key in actions) {
+    if (actions[key].type === 'addProperties') {
+      Object.assign(state, actions[key].extraData);
+    }
+
+    if (actions[key].type === 'removeProperties') {
+      for (const t in actions[key].keysToRemove) {
+        if (actions[key].keysToRemove[t] in state) {
+          delete state[actions[key].keysToRemove[t]];
+        }
+      }
+    }
+
+    if (actions[key].type === 'clear') {
+      for (const n in state) {
+        delete state[n];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
