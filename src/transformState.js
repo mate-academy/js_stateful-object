c59677aec79fd814f41f8346d7ee7882d5f4f6f7
Maksym Kostetskyi <mkost996@gmail.com>
2023-03-20 11:23:26 +0200

    fixed

--- src/transformState.js
+++ src/transformState.js
@@ -5,24 +5,24 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    for (const [key, value] of Object.entries(actions[i])) {
-      if (key === 'type' && value === 'addProperties') {
-        for (const [key1, value1] of Object.entries(actions[i].extraData)) {
-          state[key1] = value1;
-        }
-      };
-
-      if (key === 'type' && value === 'removeProperties') {
-        for (const key2 of actions[i].keysToRemove) {
-          delete state[key2];
-        }
-      };
-
-      if (key === 'type' && value === 'clear') {
-        for (const key3 of Object.keys(state)) {
-          delete state[key3];
-        }
+  for (const object of actions) {
+    for (const value of Object.values(object)) {
+      switch (value) {
+        case 'addProperties':
+          for (const [key1, value1] of Object.entries(object.extraData)) {
+            state[key1] = value1;
+          };
+          break;
+        case 'removeProperties':
+          for (const key2 of object.keysToRemove) {
+            delete state[key2];
+          };
+          break;
+        case 'clear':
+          for (const key3 of Object.keys(state)) {
+            delete state[key3];
+          };
+          break;
       }
     }
   }
