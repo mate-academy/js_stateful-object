816425fce5d7909485f02df48ac80970d287a9dc
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-03-21 18:09:47 +0200

    commit

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,22 @@
  */
 function transformState(state, actions) {
   // write code here
+
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i]['type'] === 'addProperties') {
+      Object.assign(state, actions[i]['extraData']);
+    } else if (actions[i]['type'] === 'removeProperties') {
+      for (const key of actions[i]['keysToRemove']) {
+        delete state[key];
+      }
+    } else if (actions[i]['type'] === 'clear') {
+      for (const key of Object.keys(state)) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
