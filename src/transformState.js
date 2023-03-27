bb34c8faf766ed0a2413068580e76260dd502f04
Antonio S <soi4.toxa@ukr.net>
2023-03-27 11:25:06 +0300

    Transform-State

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const keys = action.extraData;
+
+      for (const key in keys) {
+        state[key] = keys[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        if (state[key]) {
+          delete state[key];
+        }
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
