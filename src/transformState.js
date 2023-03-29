c468566701fb63c5e43c1912cf5d1cd75d8d8da6
Natalia Klonowska <natalia742@gmail.com>
2023-03-29 20:50:00 +0200

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,25 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const property of action.keysToRemove) {
+          delete state[property];
+        }
+        break;
+      case 'clear':
+        for (const property in state) {
+          delete state[property];
+        }
+        break;
+      default:
+        throw new Error('Wrong type value');
+    }
+  }
 }
 
 module.exports = transformState;
