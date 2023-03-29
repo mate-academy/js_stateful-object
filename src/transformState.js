b59132ea2177dad6188018aaa4f4f7c3096dde04
Mikołaj <ostermi@wp.pl>
2023-03-29 12:08:20 +0100

    fix

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        for (const [key, value] of Object.entries(action.extraData)) {
+          state[key] = value;
+        }
+        break;
+      }
+
+      case `removeProperties`: {
+        for (const property of action.keysToRemove) {
+          delete state[property];
+        }
+        break;
+      }
+
+      case `clear`: {
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      }
+    }
+  }
 }
 
 module.exports = transformState;
