cf74b36c13ab7103d679af7917a8de0adc48bc27
Amadeusz Michalowski <amadeusz.michalowski@gmail.com>
2023-04-04 01:03:56 +0200

    switch added

--- src/transformState.js
+++ src/transformState.js
@@ -6,22 +6,27 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const key in actions[i].extraData) {
-        state[key] = actions[i].extraData[key];
-      }
-    }
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          state[key] = actions[i].extraData[key];
+        }
+        break;
 
-    if (actions[i].type === 'removeProperties') {
-      for (let a = 0; a < actions[i].keysToRemove.length; a++) {
-        delete state[actions[i].keysToRemove[a]];
-      }
-    }
+      case 'removeProperties':
+        for (let a = 0; a < actions[i].keysToRemove.length; a++) {
+          delete state[actions[i].keysToRemove[a]];
+        }
+        break;
 
-    if (actions[i].type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw new Error('Unexpected command');
     }
   }
 }
