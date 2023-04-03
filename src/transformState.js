f45a07fa14c6b14263df3cb2b73d70659fbe4071
Nazar Yants <nazyants@gmail.com>
2023-04-03 17:16:24 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,7 +4,6 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-// const state = {bar: '123'};
 
 function transformState(state, actions) {
   for (const i of actions) {
@@ -25,23 +24,5 @@ function transformState(state, actions) {
     }
   }
 }
-
-// transformState(state,
-//   [
-//     {
-//       type: 'addProperties',
-//       extraData: {
-//         name: 'Jim',
-//         hello: 'world',
-//       },
-//     },
-//     {
-//       type: 'removeProperties',
-//       keysToRemove: ['bar', 'hello'],
-//     },
-//   ]
-// );
-
-// console.log(state);
 
 module.exports = transformState;
