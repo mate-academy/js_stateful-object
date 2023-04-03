c344debddbd5611fd012866c61034c222909d645
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-04-03 15:05:57 +0200

    change template sting to string and remove comments

--- src/transformState.js
+++ src/transformState.js
@@ -5,36 +5,6 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-  // for (let i = 0; i < actions.length; i++) {
-  //   console.log(actions[1].type);
-  //   if (actions[i].type === 'addProperties') {
-  //     for (const key in actions[i].extraData) {
-  //       state[key] = actions[i].extraData[key];
-  //     }
-  //   } else {
-  //     throw new Error('error msg');
-  //   }
-
-  //   if (actions[i].type === 'removeProperties') {
-  //     for (const key of actions[i].keysToRemove) {
-  //       delete state[key];
-  //     }
-  //   } else {
-  //     throw new Error('error msg');
-  //   }
-
-  //   if (actions[i].type === 'clear') {
-  //     for (const key of actions[i].keysToRemove) {
-  //       delete state[key];
-  //     }
-  //   } else {
-  //     throw new Error('error msg');
-  //   }
-  // }
-
-  // return state;
-
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties': {
@@ -44,14 +14,14 @@ function transformState(state, actions) {
         break;
       }
 
-      case `removeProperties`: {
+      case 'removeProperties': {
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
       }
 
-      case `clear`: {
+      case 'clear': {
         for (const key in state) {
           delete state[key];
         }
