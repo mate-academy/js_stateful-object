8962ab7e48161800c9bec9e6fb73d099cc69b439
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-04-07 16:57:24 +0300

    stateful-object

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,32 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
-}
 
+function transformState(state, actions) {
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.keys(action.extraData).map(function(key) {
+          state[key] = action.extraData[key];
+        });
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.map(function(key) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        });
+        break;
+
+      case 'clear':
+        Object.keys(state).map(function(key) {
+          delete state[key];
+        });
+        break;
+    }
+  }
+
+  return state;
+}
 module.exports = transformState;
