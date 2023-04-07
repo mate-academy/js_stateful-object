ce49d98a0dc04ba76a4531b72bcf36b999f8c544
Roman <rdyakovich@gmail.com>
2023-04-07 16:59:58 +0300

    switchState

--- src/transformState.js
+++ src/transformState.js
@@ -6,24 +6,31 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      const extraData = action.extraData;
+    switch (action.type) {
+      case 'addProperties':
+        const extraData = action.extraData;
 
-      for (const key in extraData) {
-        state[key] = extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      const keysToRemove = action.keysToRemove;
+        for (const key in extraData) {
+          state[key] = extraData[key];
+        }
+    }
 
-      for (const key of keysToRemove) {
-        if (state.hasOwnProperty(key)) {
+    switch (action.type) {
+      case 'removeProperties':
+        const keysToRemove = action.keysToRemove;
+
+        for (const key of keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+    }
+
+    switch (action.type) {
+      case 'clear':
+        for (const key in state) {
           delete state[key];
         }
-      }
-    } else if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
     }
   }
 }
