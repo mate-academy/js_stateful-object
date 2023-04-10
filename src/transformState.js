4955a9a95726307b4c8fa8135f74bb23e9030ec2
Maksym Shevelin <shevmax01@gmail.com>
2023-04-10 21:03:15 +0300

    switch case

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,47 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const value of action.keysToRemove) {
-        if (state[value]) {
-          delete state[value];
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const value of action.keysToRemove) {
+          if (state[value]) {
+            delete state[value];
+          }
         }
-      }
-    } else if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        break;
     }
   }
 }
+
+// function transformState(state, actions) {
+//   for (const action of actions) {
+//     if (action.type === 'addProperties') {
+//       Object.assign(state, action.extraData);
+//     } else if (action.type === 'removeProperties') {
+//       for (const value of action.keysToRemove) {
+//         if (state[value]) {
+//           delete state[value];
+//         }
+//       }
+//     } else if (action.type === 'clear') {
+//       for (const key in state) {
+//         delete state[key];
+//       }
+//     }
+//   }
+// }
 
 module.exports = transformState;
