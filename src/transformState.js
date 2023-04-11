55925a961c3693230d76b78f1d76637d690e6067
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-11 13:09:24 +0200

    after code review corrections

--- src/transformState.js
+++ src/transformState.js
@@ -6,15 +6,18 @@
  */
 function transformState(state, actions) {
   let tempState = state;
+  // when I directly refer to state -
+  // then linter throws error: "assignement to function parametr."
+  // That's why im using tempState here :)
 
-  for (const singleAction of actions) {
-    switch (singleAction.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        tempState = Object.assign(tempState, singleAction.extraData);
+        tempState = Object.assign(tempState, action.extraData);
         break;
 
       case 'removeProperties':
-        singleAction.keysToRemove.forEach(el => {
+        action.keysToRemove.forEach(el => {
           delete tempState[el];
         });
         break;
@@ -23,6 +26,10 @@ function transformState(state, actions) {
         for (const el in tempState) {
           delete tempState[el];
         };
+        break;
+
+      default:
+        throw new Error('Unknow name of actions type.');
     };
   };
 
