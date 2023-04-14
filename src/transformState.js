868e522ee466b0d3d5d428014f9daf8d1f0ca75e
Aidini Meyer <aidinimeyer@outlook.com>
2023-04-14 15:28:20 -0500

    done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      for (act.type of actions) {
+        Object.assign(state, act.extraData);
+      }
+    }
+
+    if (act.type === 'removeProperties') {
+      for (const type of act.keysToRemove) {
+        delete state[type];
+      }
+    }
+
+    if (act.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
