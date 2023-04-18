b25060fd242bfa81f82d7cf215cadcbe6ae434a1
vladimirshub <vladimirscloud@icloud.com>
2023-04-18 20:59:05 +0300

    fixes

--- src/transformState.js
+++ src/transformState.js
@@ -5,21 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const action of actions) {
-    switch (action.type) {
+  for (const { type, ...action } of actions) {
+    switch (type) {
       case 'addProperties':
         Object.assign(state, action.extraData);
         break;
+
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
+
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
         break;
+
+      default:
+        throw new Error(`Invalid action type: ${action.type}`);
     }
   }
 }
