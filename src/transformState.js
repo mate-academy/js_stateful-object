523872445b944ea1de8c493bdf5da65499c89a63
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-04-19 14:34:16 +0200

    Stateful object resolved

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const remove of action.keysToRemove) {
+        delete state[remove];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const clear in state) {
+        delete state[clear];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
