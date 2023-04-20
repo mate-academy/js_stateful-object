18c59ba3064eff514e55405bd89aa49577bcf129
Iryna Khalniepiesova <karolinkagrey@gmail.com>
2023-04-20 21:22:12 +0300

    First commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key of actions) {
+    if (key.type === 'clear') {
+      for (const value in state) {
+        delete state[value];
+      }
+    }
+
+    if (key.type === 'addProperties') {
+      Object.assign(state, key.extraData);
+    }
+
+    if (key.type === `removeProperties`) {
+      for (const type of key.keysToRemove) {
+        delete state[type];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
