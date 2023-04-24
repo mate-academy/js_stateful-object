5a8aa5afd6ba0c7e29600e8f678b29bf5c574b80
Dmytro Vovk <bellboy.dv@gmail.com>
2023-04-24 19:12:38 +0300

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,25 +6,24 @@
  */
 function transformState(state, actions) {
   // write code here
-  const copy = { ...state };
 
   if (actions.type === 'addProperties') {
-    Object.assign(copy, actions.extraData);
+    Object.assign(state, actions.extraData);
   }
 
   if (actions.type === 'removeProperties') {
-    actions.keysToRemove.forEach(key => {
-      delete copy[key];
-    });
+    for (const key in actions.extraData) {
+      if (state.hasOwnProperty(key)) {
+        delete state[key];
+      }
+    }
   }
 
   if (actions.type === 'clear') {
-    Object.keys(copy).forEach(key => {
-      delete copy[key];
+    Object.keys(state).forEach(key => {
+      delete state[key];
     });
   }
-
-  return copy;
 }
 
 module.exports = transformState;
