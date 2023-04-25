cef05e5b99884129c30b0cc2e010dd41f1787a81
Hishchak Tetiana <tanaiosered@ukr.net>
2023-04-25 21:07:30 +0300

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+
+        break;
+
+      case 'clear':
+        for (const stat in state) {
+          delete state[stat];
+        }
+
+        break;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
