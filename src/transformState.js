67528214ca3058b85a5254bf74836a2b56ebf7b2
Diana Tuz <dianatuz12@gmail.com>
2023-05-03 00:31:11 +0300

    obj

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const key in actions) {
+    if (actions[key].type === 'addProperties') {
+      Object.assign(state, actions[key].extraData);
+    }
+
+    if (actions[key].type === 'removeProperties') {
+      for (const del of actions[key].keysToRemove) {
+        delete state[del];
+      }
+    }
+
+    if (actions[key].type === 'clear') {
+      for (const k in state) {
+        delete state[k];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
