8822a850db8495f203fccb0c74a0fe6c76e2d530
Maksym Nemera <nickmitchell@ukr.net>
2023-05-05 11:47:59 +0300

    changed var for iterations

--- src/transformState.js
+++ src/transformState.js
@@ -12,14 +12,14 @@ function transformState(state, actions) {
         break;
 
       case 'removeProperties':
-        for (const data of action.keysToRemove) {
-          delete state[data];
+        for (const key of action.keysToRemove) {
+          delete state[key];
         }
         break;
 
       case 'clear':
-        for (const data in state) {
-          delete state[data];
+        for (const key in state) {
+          delete state[key];
         }
         break;
 
