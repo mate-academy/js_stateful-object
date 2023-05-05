a3e667b5b7adc68cd72e84a6986b63a662e35c90
Dmytro Levchenko <levchenkod91@gmail.com>
2023-05-05 12:54:37 +0300

    fixes code as per reviewer request

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,22 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
-    if (action.type === 'removeProperties') {
-      for (const key in action.keysToRemove) {
-        delete state[action.keysToRemove[key]];
-      }
-    }
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
 
-    if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+      default:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 }
