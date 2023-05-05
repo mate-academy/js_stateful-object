56bd9abe0f6d4893807198332cb4ca334af3f94a
Andrii <zisuan.od@gmail.com>
2023-05-05 12:14:13 +0300

    task done

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,29 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const obj of actions) {
+    if (obj.type === 'addProperties') {
+      Object.assign(state, obj.extraData);
+    }
+
+    if (obj.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+
+    if (obj.type === 'removeProperties') {
+      for (const key in state) {
+        for (const arrElem of obj.keysToRemove) {
+          if (arrElem === key) {
+            delete state[key];
+          }
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
