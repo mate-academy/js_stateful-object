23f6a884143a0982269a09f37b99a583311c3f9c
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-05-06 21:16:25 +0300

    transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    switch (actions[i].type) {
+      case 'addProperties':
+        for (const key in actions[i].extraData) {
+          state[key] = actions[i].extraData[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of actions[i].keysToRemove) {
+          delete state[key];
+        }
+        break;
+      default:
+        return 'Something went wrong';
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
