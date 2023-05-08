fbeaf853bbf9773e1de2de3ae457fdd008ed8b56
Roman Didusenko <albertushoffman@proton.me>
2023-05-08 18:47:00 +0300

    fixed

--- src/transformState.js
+++ src/transformState.js
@@ -6,20 +6,34 @@
  */
 function transformState(state, actions) {
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const removed of action.keysToRemove) {
-        delete state[removed];
-      }
-    } else if (action.type === 'clear') {
-      for (const prop in state) {
-        delete state[prop];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        remove(state, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clear(state);
+        break;
     }
   }
 
   return state;
+}
+
+function remove(object, val) {
+  for (const key of val) {
+    delete object[key];
+  }
+}
+
+function clear(object) {
+  for (const key in object) {
+    delete object[key];
+  }
 }
 
 module.exports = transformState;
