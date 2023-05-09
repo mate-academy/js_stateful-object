b524aff9e0f0d8bab41a4aa5a5a39e5ac0cbbf54
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-09 17:03:21 +0300

    Add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,36 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperty(state, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clearObject(state);
+        break;
+
+      default:
+        return state;
+    }
+  }
+}
+
+function removeProperty(object, keys) {
+  for (const key of keys) {
+    delete object[key];
+  }
+}
+
+function clearObject(object) {
+  for (const key in object) {
+    delete object[key];
+  }
 }
 
 module.exports = transformState;
