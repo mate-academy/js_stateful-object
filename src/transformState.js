0a8722d816d21dd6fcd1d73e37477cea4b67700d
Vania Yarovyi <yarovyi@gmail.com>
2023-05-09 13:20:45 +0300

    add solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        for (const data in act.extraData) {
+          state[data] = act.extraData[data];
+        };
+        break;
+      case 'removeProperties':
+        for (const key of act.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+      default:
+        throw new Error('wrong action type');
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
