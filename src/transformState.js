eccdf822cd09c7278aefc92b3f3cbc5ef1403166
Mykola Maik <mykoluamaik@gmail.com>
2023-05-10 15:57:06 +0300

    remove if, add switch

--- src/transformState.js
+++ src/transformState.js
@@ -4,22 +4,49 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+// function transformState(state, actions) {
+//   for (const index of actions) {
+//     if (index.type === 'addProperties') {
+//       for (const key in index.extraData) {
+//         state[key] = index.extraData[key];
+//       }
+//     } else if (index.type === 'removeProperties') {
+//       for (const property of index.keysToRemove) {
+//         if (state[property]) {
+//           delete state[property];
+//         }
+//       }
+//     } else if (index.type === 'clear') {
+//       for (const key1 in state) {
+//         delete state[key1];
+//       }
+//     }
+//   }
+
+//   return state;
+// }
 function transformState(state, actions) {
   for (const index of actions) {
-    if (index.type === 'addProperties') {
-      for (const key in index.extraData) {
-        state[key] = index.extraData[key];
-      }
-    } else if (index.type === 'removeProperties') {
-      for (const property of index.keysToRemove) {
-        if (state[property]) {
-          delete state[property];
+    switch (index.type) {
+      case 'addProperties':
+        for (const key in index.extraData) {
+          state[key] = index.extraData[key];
         }
-      }
-    } else if (index.type === 'clear') {
-      for (const key1 in state) {
-        delete state[key1];
-      }
+        break;
+      case 'removeProperties':
+        for (const property of index.keysToRemove) {
+          if (state[property]) {
+            delete state[property];
+          }
+        }
+        break;
+      case 'clear':
+        for (const key1 in state) {
+          delete state[key1];
+        }
+        break;
+      default:
+        break;
     }
   }
 
