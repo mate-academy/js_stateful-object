340d4a2fc8a4ce4d6c7e57e51aba88d94f3d0c7b
Yehor Myronov <2726048@gmail.com>
2023-05-10 15:22:24 +0300

    fixed by comments

--- src/transformState.js
+++ src/transformState.js
@@ -5,21 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const kindOfAction of actions) {
-    switch (kindOfAction.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        Object.assign(state, kindOfAction.extraData);
+        Object.assign(state, action.extraData);
         break;
+
       case 'removeProperties':
-        kindOfAction.keysToRemove.forEach(el => {
-          delete state[el];
-        });
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        };
         break;
+
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
         break;
+
+      default:
+        throw new Error('ooops');
     }
   }
 
