84fb6f79b1ad01aa660065cdf7156a8c9dad7606
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-10 21:18:36 +0300

    add solution of stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        Object.keys(state).forEach(key => {
+          if (action.keysToRemove.includes(key)) {
+            delete state[key];
+          }
+        });
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach(key => {
+          delete state[key];
+        });
+        break;
+
+      default:
+        throw new Error(`Unsupported action type: ${action.type}`);
+    }
+  }
 }
 
 module.exports = transformState;
