53a322b562ed94812ec3837828a5e88778172816
Liza Strykharchuk <ol220681vol@gmail.com>
2023-05-10 20:31:56 +0300

    code

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,44 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
 
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(state)) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+}
 module.exports = transformState;
+
+// "version": "0.2.0",
+//   "configurations": [
+//     {
+//       "name": "Debug Jest Tests",
+//       "type": "node",
+//       "request": "launch",
+//       "runtimeArgs": [
+//         "--inspect-brk",
+//         "${workspaceRoot}/node_modules/jest/bin/jest.js",
+//         "--runInBand"
+//       ],
+//       "console": "integratedTerminal",
+//       "internalConsoleOptions": "neverOpen",
+//       "port": 9229
+//     }
+//   ]
