a68ce28ce278aafa986984f8fea34bc92e2b544b
Roksolana Tylipska <roksolana1407@gmail.com>
2023-05-11 15:27:44 +0300

    delete my comments in JS file

--- src/transformState.js
+++ src/transformState.js
@@ -4,37 +4,6 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-// function transformState(state, actions) {
-//   const newState = state;
-
-//   for (const action of actions) {
-//     const { type, keysToRemove, extraData } = action;
-
-//     switch (type) {
-//       case 'addProperties':
-//         Object.assign(newState, extraData);
-//         break;
-
-//       case 'removeProperties':
-//         for (const keyToRemove of keysToRemove) {
-//           delete newState[keyToRemove];
-//         }
-//         break;
-
-//       case 'clear':
-//         for (const keyToRemoveInState of state) {
-//           delete state[keyToRemoveInState];
-//         }
-//         break;
-
-//       default:
-//         return `Unknown type ${type} `;
-//     }
-//   }
-
-//   return newState;
-// }
-
 function transformState(state, actions) {
   for (const action of actions) {
     const { type, keysToRemove, extraData } = action;
