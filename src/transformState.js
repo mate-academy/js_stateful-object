5afc47a2267dd46c7fcf2c19d78d9628af4a3eab
iv4ek <ivaskovila@gmail.com>
2023-05-13 08:16:02 +0200

    State can be transform

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,28 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    switch (act.type) {
+      case 'addProperties':
+        Object.assign(state, act.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const prop of act.keysToRemove) {
+          delete state[prop];
+        }
+        break;
+
+      case 'clear':
+        for (const keys in state) {
+          delete state[keys];
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
