42a112a5a0e5c1b95d9606ec978e7fdb08890944
iv4ek <ivaskovila@gmail.com>
2023-05-14 20:50:54 +0200

    Add improvements

--- src/transformState.js
+++ src/transformState.js
@@ -5,21 +5,21 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const act of actions) {
-    switch (act.type) {
+  for (const action of actions) {
+    switch (action.type) {
       case 'addProperties':
-        Object.assign(state, act.extraData);
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        for (const prop of act.keysToRemove) {
+        for (const prop of action.keysToRemove) {
           delete state[prop];
         }
         break;
 
       case 'clear':
-        for (const keys in state) {
-          delete state[keys];
+        for (const key in state) {
+          delete state[key];
         }
         break;
 
