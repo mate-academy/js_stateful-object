8d51cd0df5f40a9da51e2ddc29df159edc491e6a
Volodymyr Shostak <shostak_1@proton.me>
2023-05-18 14:38:29 +0300

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      for (const givenProperty in actions[i].extraData) {
+        state[givenProperty] = actions[i].extraData[givenProperty];
+      }
+    } else if (actions[i].type === 'removeProperties') {
+      for (const removingProp in actions[i].keysToRemove) {
+        delete state[actions[i].keysToRemove[removingProp]];
+      }
+    } else if (actions[i].type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
