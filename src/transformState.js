97be6e04e8d3bf71b02fda2c94b9629d0e877f1e
Nazar Hotko <nazar.hotko@outlook.com>
2023-05-19 18:45:09 +0200

    task solved

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,21 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const num in actions) {
+    const action = actions[num];
+
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+    } else if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
