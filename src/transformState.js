5412cb5281cc361b4d946e80ff1438c7b1efdd4c
dima <dima98doroshenko@gmail.com>
2023-05-22 14:32:42 +0300

    added switchCase

--- src/transformState.js
+++ src/transformState.js
@@ -8,18 +8,26 @@ function transformState(state, actions) {
   for (const ob in actions) {
     const action = actions[ob];
 
-    if (action.type === 'addProperties') {
-      Object.assign(state, action.extraData);
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+
+        break;
+      default:
+        return {};
     }
   }
 }
-
 module.exports = transformState;
