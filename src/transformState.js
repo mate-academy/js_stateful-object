e3de28fe8ee5aa28d25966f25fc131e25ce746c5
Vitaly Maistrenko <maystrenko977@gmail.com>
2023-05-24 09:25:49 +0300

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,29 +4,32 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
  function transformState(state, actions) {
   return actions.reduce((newState, action) => {
     switch (action.type) {
       case 'addProperties':
         return Object.assign(newState, action.extraData);
-
+        
       case 'removeProperties':
         action.keysToRemove.forEach((key) => {
           if (newState.hasOwnProperty(key)) {
             delete newState[key];
           }
         });
-        return newState;
-
+        break;
+        
       case 'clear':
         Object.keys(newState).forEach((key) => {
           delete newState[key];
         });
-        return newState;
-
+        break;
+        
       default:
-        return newState;
+        break;
     }
+    
+    return newState;
   }, state);
 }
 
