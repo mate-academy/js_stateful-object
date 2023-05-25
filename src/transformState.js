d8975767c89a29e0005b6cca1f369da51b7540dd
Illya Myhalyk <ilamihalik@gmail.com>
2023-05-25 19:05:00 +0300

    commit task transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
-}
+  function transformState(state, actions) {
+    for (const action of actions) {
+      switch (action.type) {
+        case 'addProperties':
+          Object.assign(state, action.extraData);
+          break;
+  
+        case 'removeProperties':
+          for (const key of action.keysToRemove) {
+            delete state[key];
+          }
+          break;
+  
+        case 'clear':
+          for (const key in state) {
+            delete state[key];
+          }
+          break;
+  
+        default:
+          return 0;
+      }
+    }
+  
+    return state;
+  }
+  }
 
 module.exports = transformState;
