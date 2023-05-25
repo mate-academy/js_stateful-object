ef2ed5e155834bf83cfee7fe36ff3a139b87c7e5
Illya Myhalyk <ilamihalik@gmail.com>
2023-05-25 19:14:11 +0300

    removed duplicate code

--- src/transformState.js
+++ src/transformState.js
@@ -4,7 +4,6 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
   function transformState(state, actions) {
     for (const action of actions) {
       switch (action.type) {
@@ -30,7 +29,6 @@ function transformState(state, actions) {
     }
   
     return state;
-  }
   }
 
 module.exports = transformState;
