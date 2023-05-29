def298f71dae39a59bb7606dd6d0ed04ec2b54bf
Roma <roma150269@gmail.com>
2023-05-29 20:16:27 +0300

    decision

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      action.keysToRemove.forEach(element => {
+        delete state[element];
+      });
+    }
+
+    if (action.type === 'clear') {
+      for (const stat in state) {
+        delete state[stat];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
