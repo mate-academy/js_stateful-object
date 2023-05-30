f3cf8bf508ce15aa575635324ec5b5272f6d6052
Volodymyr <prostonadobobb@gmail.com>
2023-05-30 22:12:26 +0300

    add solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    if (act.type === 'addProperties') {
+      for (const addItem in act.extraData) {
+        state[addItem] = act.extraData[addItem];
+      }
+    }
+
+    if (act.type === 'removeProperties') {
+      for (const removeItem of act.keysToRemove) {
+        // eslint-disable-next-line no-console
+        console.log(state, state[removeItem], removeItem);
+        delete state[removeItem];
+      }
+    }
+
+    if (act.type === 'clear') {
+      for (const st in state) {
+        delete state[st];
+      }
+    }
+  }
+  // eslint-disable-next-line no-console
+
+  return state;
 }
 
 module.exports = transformState;
