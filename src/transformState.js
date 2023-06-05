c1e04c901ac0c9ee3aa91bcbf4b8a10b8ca3c254
Dima Astapenko <bonum_taurus@ukr.net>
2023-06-05 16:19:49 +0300

    task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,35 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const keys = Object.keys(action);
+    const type = action[keys[0]];
+
+    switch (type) {
+      case 'removeProperties':
+        const keysRemove = action[keys[1]];
+
+        for (const keyRemove of keysRemove) {
+          delete state[keyRemove];
+        }
+
+        break;
+
+      case 'addProperties':
+        const properties = action[keys[1]];
+
+        Object.assign(state, properties);
+
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
