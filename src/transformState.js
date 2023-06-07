5b299436da97eb5c7be35ce5fbcd7de892a90f56
Nikita Kravets <titech.contacts@gmail.com>
2023-06-07 20:54:09 +0300

    task implementation

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,28 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        const dataToExtract = action.extraData;
+
+        Object.assign(state, { ...dataToExtract });
+        break;
+
+      case 'removeProperties':
+        const dataToRemove = action.keysToRemove;
+
+        [ ...dataToRemove ].forEach(list => delete state[list]);
+        break;
+
+      case 'clear':
+        for (const member in state) {
+          delete state[member];
+        }
+
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
