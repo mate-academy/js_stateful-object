9af0c4582aaae12b8d9204e31931797733607048
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-06-07 12:18:07 +0300

    Added switch case , cahnged loop var names

--- src/transformState.js
+++ src/transformState.js
@@ -5,17 +5,22 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const a of actions) {
-    if (a.type === 'addProperties') {
-      Object.assign(state, a.extraData);
-    } else if (a.type === 'removeProperties') {
-      for (const b of a.keysToRemove) {
-        delete state[b];
-      }
-    } else {
-      for (const c in state) {
-        delete state[c];
-      }
+  for (const value of actions) {
+    switch (value.type) {
+      case 'addProperties':
+        Object.assign(state, value.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of value.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
     }
   }
 }
