acba147489a4da197250b45f4ab75f73271fbeb7
Leo <you@example.com>
2023-06-08 17:13:10 +0300

    create pr

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const comands of actions) {
+    if (comands.type === 'addProperties') {
+      for (const [key, value] of Object.entries(comands.extraData)) {
+        state[key] = value;
+      }
+    }
+
+    if (comands.type === 'removeProperties') {
+      for (const key of Object.values(comands.keysToRemove)) {
+        delete state[key];
+      }
+    }
+
+    if (comands.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
