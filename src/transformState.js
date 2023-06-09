5cd135db3901d02d8b984325f2c6e04e780c13f0
IlonaBorkovska <borkovska.io@gmail.com>
2023-06-09 10:59:34 +0300

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,35 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+
+        continue;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+
+        continue;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+
+        continue;
+
+      default:
+        throw new Error(`Action type "${action.type}" does not exist`);
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
