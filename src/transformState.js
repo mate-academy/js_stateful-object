111c53484addaab9737806a80b80ae1ec695f242
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-06-11 21:29:53 +0300

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      // eslint-disable-next-line
+      case 'addProperties': {
+        Object.assign(state, action.extraData);
+      } break;
+      // eslint-disable-next-line
+      case 'removeProperties': {
+        for (const key of action.keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+      } break;
+      // eslint-disable-next-line
+      case 'clear': {
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+      } break;
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
