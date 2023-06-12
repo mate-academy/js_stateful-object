8c63001c644887b1159fe9fcc98d9cc381b8a73d
Zoriana Melnychuk <zoryashka3002@gmail.com>
2023-06-12 12:45:28 +0200

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,30 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        Object.keys(state).forEach((key) => {
+          delete state[key];
+        });
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach((key) => {
+          delete state[key];
+        });
+        break;
+
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      default:
+        throw new Error(`Error`);
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
