656d669d5d8061dd6ecd3b3c85958a301ad1a646
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-06-12 21:25:33 +0300

    feat: transform state

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,41 @@
  */
 function transformState(state, actions) {
   // write code here
+  function addProperties(extraData) {
+    for (const [key, value] of Object.entries(extraData)) {
+      state[key] = value;
+    }
+  }
+
+  function clear() {
+    for (const key in state) {
+      delete state[key];
+    }
+  }
+
+  function removeProperties(keysToRemove) {
+    for (const key of keysToRemove) {
+      delete state[key];
+    }
+  }
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(action.extraData);
+        break;
+      case 'clear':
+        clear();
+        break;
+      case 'removeProperties':
+        removeProperties(action.keysToRemove);
+        break;
+      default:
+        throw Error;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
