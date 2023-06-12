c94efb05ea8a4e7f2cf55715cc747e01c0c5bd1f
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-12 15:36:40 +0300

    Finished task

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const allActions = Object.values(actions);
+
+  for (let i = 0; i < allActions.length; i++) {
+    const action = allActions[i];
+
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach((key) => {
+          delete state[key];
+        });
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
