2fae86278173f44e1224d7633ad17453b1876935
Diana Pinchuk <diana9360@gmail.com>
2023-06-13 20:25:41 +0300

    added types const

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,23 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  const ADD = 'addProperties';
+  const REMOVE = 'removeProperties';
+  const CLEAR = 'clear';
+
   for (const action of actions) {
     switch (action.type) {
-      case 'addProperties':
+      case ADD:
         Object.assign(state, action.extraData);
         break;
 
-      case 'removeProperties':
+      case REMOVE:
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
 
-      case 'clear':
+      case CLEAR:
         for (const key in state) {
           delete state[key];
         }
