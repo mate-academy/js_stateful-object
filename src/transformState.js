b1dd4762519d4d961ae0cc32913bd73467ce95bd
ihorhladkov <ihor.hladkov@gmail.com>
2023-06-13 17:00:22 +0300

    fixed

--- src/transformState.js
+++ src/transformState.js
@@ -22,6 +22,9 @@ function transformState(state, actions) {
           delete state[key];
         }
         break;
+
+      default:
+        throw new Error('This type doesn`t exist');
     }
   }
 }
