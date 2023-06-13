8560e23cd469c6e9b5180ce6cecb458b4eba0637
Bohdan Ivankov <bodanich600@gmail.com>
2023-06-13 14:43:48 +0100

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,35 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type } = action;
+
+    switch (type) {
+      case 'addProperties':
+        const { extraData } = action;
+
+        Object.assign(state, extraData);
+        break;
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        for (const key of keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        throw new Error(`Invalid action type: ${type}`);
+    }
+  }
 }
 
 module.exports = transformState;
