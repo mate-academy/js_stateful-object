307c2a10dd7844031e3decf4dda87bc2b9177a68
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-13 14:55:47 +0300

    adding default case

--- src/transformState.js
+++ src/transformState.js
@@ -5,11 +5,15 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  const ADD_PROPERTIES = 'addProperties';
+  const REMOVE_PROPERTIES = 'removeProperties';
+  const CLEAR = 'clear';
+
   for (const action of actions) {
     const { type } = action;
 
     switch (type) {
-      case 'addProperties': {
+      case ADD_PROPERTIES: {
         const { extraData } = action;
 
         for (const key in extraData) {
@@ -18,7 +22,7 @@ function transformState(state, actions) {
         break;
       }
 
-      case 'removeProperties': {
+      case REMOVE_PROPERTIES: {
         const { keysToRemove } = action;
 
         for (const key of keysToRemove) {
@@ -29,12 +33,15 @@ function transformState(state, actions) {
         break;
       }
 
-      case 'clear': {
+      case CLEAR: {
         for (const key in state) {
           delete state[key];
         }
         break;
       }
+
+      default:
+        throw new Error(`Action error with ${type}`);
     }
   }
 
