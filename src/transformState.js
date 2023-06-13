22890597a129178787f18299346e55820c01abdc
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-13 14:33:46 +0300

    create transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,40 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type } = action;
+
+    switch (type) {
+      case 'addProperties': {
+        const { extraData } = action;
+
+        for (const key in extraData) {
+          state[key] = extraData[key];
+        }
+        break;
+      }
+
+      case 'removeProperties': {
+        const { keysToRemove } = action;
+
+        for (const key of keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
