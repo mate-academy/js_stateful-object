6a2261f019f73877eb9c1ab7728ff52543ec79e6
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-06-14 16:19:33 +0300

    fix codestyle and error handling

--- src/transformState.js
+++ src/transformState.js
@@ -9,20 +9,17 @@ function transformState(state, actions) {
     switch (action.type) {
       case 'addProperties':
         Object.assign(state, action.extraData);
-
         break;
       case 'removeProperties':
         for (const removeItem of action.keysToRemove) {
           delete state[removeItem];
         }
-
         break;
       case 'clear':
         Object.keys(state).forEach(objectValue => delete state[objectValue]);
-
         break;
       default:
-        break;
+        throw new Error(`Incorrect value: ${action.type}`);
     }
   }
 }
