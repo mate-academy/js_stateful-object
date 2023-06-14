133802510ef8fb8ae15a83bdd3a09a0112bf0fb5
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-06-14 08:23:29 +0300

    fixes

--- src/transformState.js
+++ src/transformState.js
@@ -5,11 +5,8 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    const action = actions[i];
-    const type = action.type;
-    const extraData = action.extraData;
-    const keysToRemove = action.keysToRemove;
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
 
     switch (type) {
       case 'addProperties':
@@ -17,21 +14,21 @@ function transformState(state, actions) {
         break;
 
       case 'removeProperties':
-        for (const arrKey of keysToRemove) {
-          if (state.hasOwnProperty(arrKey)) {
-            delete state[arrKey];
+        for (const key of keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
           }
         }
         break;
 
       case 'clear':
-        for (const objKey in state) {
-          delete state[objKey];
+        for (const key in state) {
+          delete state[key];
         }
         break;
 
       default:
-        return 'Invalid action type';
+        throw new Error('Invalid action type');
     }
   }
 }
