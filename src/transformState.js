7c938f0751bca1d6e231d266e06a139bfb78ffe0
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-06-15 15:34:29 +0200

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,29 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Object} state
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+      continue;
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const keyToRemove of action.keysToRemove) {
+        delete state[keyToRemove];
+      }
+      continue;
+    }
+
+    if (action.type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+  }
 }
 
 module.exports = transformState;
