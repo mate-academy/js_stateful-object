aee283c14ff88a9a3a070689a5d3a5d7dfb14272
zipic <zpc23041988@gmail.com>
2023-06-15 17:52:27 +0300

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+    const { type } = action;
+
+    if (type === 'addProperties') {
+      Object.assign(state, action.extraData);
+    }
+
+    if (type === 'removeProperties') {
+      action.keysToRemove.forEach(key => {
+        delete state[key];
+      });
+    }
+
+    if (type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
