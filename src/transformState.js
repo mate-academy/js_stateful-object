299909d5d9795377b1791c2a321e65b1ddb6e218
Valera Viachalo <valeravyachalo@gmail.com>
2023-06-16 14:19:49 +0300

    stateful object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (true) {
+      case action.type === 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      case action.type === 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete state[keyToRemove];
+        }
+        break;
+
+      case action.type === 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
