228b5ea5ec159a6440f040c506240dcb1e8a67fd
Asya Yeromina <asyayeromina@gmail.com>
2023-06-17 16:12:24 +0200

    add my solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,46 @@
 'use strict';
 
-/**
- * @param {Object} state
- * @param {Object[]} actions
- */
+// /**
+//  * @param {Object} state
+//  * @param {Object[]} actions
+//  */
+
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const dataToAdd = action.extraData;
+    const keysToRemove = action.keysToRemove;
+    const allKeys = Object.keys(state);
+    const typeAdd = 'addProperties';
+    const typeRemove = 'removeProperties';
+    const typeClear = 'clear';
+
+    switch (action.type) {
+      case typeAdd:
+        addProperties(state, dataToAdd);
+        break;
+
+      case typeRemove:
+        removeValueByKey(state, keysToRemove);
+        break;
+
+      case typeClear:
+        removeValueByKey(state, allKeys);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return state;
+}
+
+function addProperties(obj, data) {
+  Object.assign(obj, data);
+}
+
+function removeValueByKey(obj, keys) {
+  keys.forEach(key => delete obj[key]);
 }
 
 module.exports = transformState;
