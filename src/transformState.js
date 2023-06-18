a60b75b49ba3d26824db529297809a520073f6f3
Lina Do <dykaya0916@gmail.com>
2023-06-19 00:01:54 +0300

    task was solved

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Object} state
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const obj of actions) {
+    if (obj.type === "addProperties") {
+      for (const keys in obj.extraData) {
+        state[keys] = obj.extraData[keys];
+      }
+    }
+
+    if (obj.type === "removeProperties") {
+      for (const key of obj.keysToRemove) {
+        delete state[key];
+      }
+    }
+
+    if (obj.type === "clear") {
+      for (const del in state) {
+        delete state[del];
+      }
+    }
+  }
+  return state;
 }
 
 module.exports = transformState;
