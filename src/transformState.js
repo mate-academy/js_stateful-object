676f4e5139eb8b570f98acfd2f5a038960beb4d1
Максим <78905554+F-u-n-H-o-u-s-e@users.noreply.github.com>
2023-06-19 20:43:50 +0300

    Make tasks

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,29 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    if (actions[i].type === 'addProperties') {
+      for (const keyAdd in actions[i].extraData) {
+        state[keyAdd] = actions[i].extraData[keyAdd];
+      }
+    } else if (actions[i].type === 'removeProperties') {
+      for (
+        let keyRemove = 0;
+        keyRemove < actions[i].keysToRemove.length;
+        keyRemove++
+      ) {
+        delete state[actions[i].keysToRemove[keyRemove]];
+      }
+    } else if (actions[i].type === 'clear') {
+      for (const keyClear in state) {
+        delete state[keyClear];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
