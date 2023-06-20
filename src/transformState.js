45a63780084adc20c644571bf9f61bc99c412d8b
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-06-20 17:32:47 +0200

    add tusk solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const extraData = action.extraData;
+
+      for (const key in extraData) {
+        state[key] = extraData[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      const keysToRemove = action.keysToRemove;
+
+      for (const key of keysToRemove) {
+        delete state[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
