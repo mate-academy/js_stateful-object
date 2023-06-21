550c662b7ab563b7178400a72a3667942ef60546
Andrii Solovei <solovey98.info@gmail.com>
2023-06-21 18:59:53 +0300

    fixed the variables

--- src/transformState.js
+++ src/transformState.js
@@ -5,23 +5,19 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  const changeState = state;
-
   for (const el of actions) {
     if (el.type === 'addProperties') {
-      Object.assign(changeState, el.extraData);
+      Object.assign(state, el.extraData);
     } else if (el.type === 'removeProperties') {
       for (const key of el.keysToRemove) {
-        delete changeState[key];
+        delete state[key];
       }
     } else if (el.type === 'clear') {
-      for (const key in changeState) {
-        delete changeState[key];
+      for (const key in state) {
+        delete state[key];
       }
     }
   }
-
-  return changeState;
 }
 
 module.exports = transformState;
