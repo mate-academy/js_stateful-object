97b429d4fd1b199221893a5a0c17b2c6be4e3f23
Максим <78905554+F-u-n-H-o-u-s-e@users.noreply.github.com>
2023-06-22 15:11:49 +0300

    chenge 'else if' on 'case'

--- src/transformState.js
+++ src/transformState.js
@@ -7,22 +7,26 @@
 
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      for (const keyToAdd in actions[i].extraData) {
-        state[keyToAdd] = actions[i].extraData[keyToAdd];
-      }
-    } else if (actions[i].type === 'removeProperties') {
-      for (
-        let keyToRemove = 0;
-        keyToRemove < actions[i].keysToRemove.length;
-        keyToRemove++
-      ) {
-        delete state[actions[i].keysToRemove[keyToRemove]];
-      }
-    } else if (actions[i].type === 'clear') {
-      for (const keyClear in state) {
-        delete state[keyClear];
-      }
+    switch (actions[i].type) {
+      case 'addProperties' : 
+        for (const keyToAdd in actions[i].extraData) {
+          state[keyToAdd] = actions[i].extraData[keyToAdd];
+        }
+        break;
+      case 'removeProperties' :
+        for (
+          let keyToRemove = 0;
+          keyToRemove < actions[i].keysToRemove.length;
+          keyToRemove++
+        ) {
+          delete state[actions[i].keysToRemove[keyToRemove]];
+        }
+        break;
+      case 'clear' :
+        for (const keyClear in state) {
+          delete state[keyClear];
+        }
+      break;
     }
   }
 
