479302fcbb313a47da6b689548c8342860f928c0
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-24 15:56:21 +0300

    second comit

--- src/transformState.js
+++ src/transformState.js
@@ -8,24 +8,34 @@ function transformState(state, actions) {
   actions.forEach(action => {
     const { type } = action;
 
-    if (type === 'addProperties') {
-      const { extraData } = action;
+    switch (type) {
+      case 'addProperties':
+        const { extraData } = action;
 
-      Object.entries(extraData).forEach(([key, value]) => {
-        state[key] = value;
-      });
-    } else if (type === 'removeProperties') {
-      const { keysToRemove } = action;
+        Object.entries(extraData).forEach(([key, value]) => {
+          state[key] = value;
+        });
+        break;
 
-      keysToRemove.forEach(key => {
-        if (state.hasOwnProperty(key)) {
+      case 'removeProperties':
+        const { keysToRemove } = action;
+
+        keysToRemove.forEach(key => {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        });
+        break;
+
+      case 'clear':
+        for (const key in state) {
           delete state[key];
         }
-      });
-    } else if (type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+        break;
+
+      default:
+
+        break;
     }
   });
 }
