8ef48fa2c2111e7e2410e8dc9e25cb1f454ac8c9
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-24 03:44:30 +0300

    first commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(action => {
+    const { type } = action;
+
+    if (type === 'addProperties') {
+      const { extraData } = action;
+
+      Object.entries(extraData).forEach(([key, value]) => {
+        state[key] = value;
+      });
+    } else if (type === 'removeProperties') {
+      const { keysToRemove } = action;
+
+      keysToRemove.forEach(key => {
+        if (state.hasOwnProperty(key)) {
+          delete state[key];
+        }
+      });
+    } else if (type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  });
 }
 
 module.exports = transformState;
