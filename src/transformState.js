c6c951d86f91aac82d8fc9753a393998cefe0afd
Nikita Orliak <nikitonorl@gmail.com>
2023-06-30 15:02:58 +0200

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,34 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const value = Object.values(action);
+    const type = value[0];
+
+    if (type === 'addProperties') {
+      const addData = value[1];
+
+      for (const key in addData) {
+        state[key] = addData[key];
+      }
+    }
+
+    if (type === 'removeProperties') {
+      const removeData = value[1];
+
+      for (const key of removeData) {
+        delete state[key];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const element in state) {
+        delete state[element];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
