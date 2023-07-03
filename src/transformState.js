0581e6397ad27fc1a1e02a47ed34ab94d5a51315
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-07-03 13:28:32 +0200

    made transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const REMOVE_ALL = 'clear';
+  const ADD = 'addProperties';
+  const REMOVE = 'removeProperties';
+
+  for (let i = 0; i < actions.length; i++) {
+    const objActions = actions[i];
+
+    if (objActions.type === ADD) {
+      for (const key in objActions.extraData) {
+        state[key] = objActions.extraData[key];
+      }
+    } else if (objActions.type === REMOVE) {
+      for (const key of objActions.keysToRemove) {
+        delete state[key];
+      }
+    } else if (objActions.type === REMOVE_ALL) {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
