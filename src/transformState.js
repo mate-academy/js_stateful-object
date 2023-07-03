edd35adf088f85cbf25e035944473cf898ae6f0e
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-07-03 17:07:00 +0200

    used switch instead of if

--- src/transformState.js
+++ src/transformState.js
@@ -12,18 +12,24 @@ function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
     const objActions = actions[i];
 
-    if (objActions.type === ADD) {
-      for (const key in objActions.extraData) {
-        state[key] = objActions.extraData[key];
-      }
-    } else if (objActions.type === REMOVE) {
-      for (const key of objActions.keysToRemove) {
-        delete state[key];
-      }
-    } else if (objActions.type === REMOVE_ALL) {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (objActions.type) {
+      case ADD:
+        for (const key in objActions.extraData) {
+          state[key] = objActions.extraData[key];
+        }
+        break;
+
+      case REMOVE:
+        for (const key of objActions.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case REMOVE_ALL:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
     }
   }
 
