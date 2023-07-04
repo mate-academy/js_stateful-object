d8cd62fea287c44f1de160d37a73d30e402d5ac2
Orchibald <orestkozak17@gmail.com>
2023-07-04 13:24:30 +0200

    transform state

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,39 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type } = action;
+
+    switch (type) {
+      case 'addProperties': {
+        const { extraData } = action;
+
+        Object.assign(state, extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        const { keysToRemove } = action;
+
+        for (const key of keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+      }
+
+      case 'clear': {
+        for (const key of Object.keys(state)) {
+          delete state[key];
+        }
+        break;
+      }
+
+      default:
+        throw new Error(`Unknown action type: ${type}`);
+    }
+  }
 }
 
 module.exports = transformState;
