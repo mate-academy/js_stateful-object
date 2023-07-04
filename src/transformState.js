25f3439ee40bd56f42018b05631896de3f4315f4
Serhii Dranuta <sirojka8@gmail.com>
2023-07-04 20:23:39 +0300

    add stateful_object solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => delete state[key]);
+        break;
+      case 'clear':
+        if (Object.keys(state).length > 0) {
+          Object.keys(state).forEach(key => delete state[key]);
+        }
+        break;
+      default:
+        return state;
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
