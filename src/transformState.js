e42f8cd36453d8e4a72d2d009dae69f7320efe07
Viktoriia Melnyk <melnychka1993@gmail.com>
2023-07-07 10:14:36 +0100

    task is done

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const object = actions[i];
+
+    if (object.type === 'addProperties') {
+      for (const key in object.extraData) {
+        state[key] = object.extraData[key];
+      }
+    }
+
+    if (object.type === 'removeProperties') {
+      for (const word of object.keysToRemove) {
+        delete state[word];
+      }
+    }
+
+    if (object.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
