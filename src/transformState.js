94b02df338a1e7067e4a3dc54c81aac60b8ec0bc
Viktoriia Melnyk <melnychka1993@gmail.com>
2023-07-07 11:03:46 +0100

    replaced the code with switch case construction

--- src/transformState.js
+++ src/transformState.js
@@ -5,25 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    const object = actions[i];
-
-    if (object.type === 'addProperties') {
-      for (const key in object.extraData) {
-        state[key] = object.extraData[key];
-      }
-    }
-
-    if (object.type === 'removeProperties') {
-      for (const word of object.keysToRemove) {
-        delete state[word];
-      }
-    }
-
-    if (object.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          state[key] = action.extraData[key];
+        };
+        break;
+      case 'removeProperties':
+        for (const word of action.keysToRemove) {
+          delete state[word];
+        };
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+      default:
+        return state;
     }
   }
 }
