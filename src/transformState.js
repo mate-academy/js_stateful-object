76357e9c4e679f9bbc5d05ad3f06cd5150137065
Anna Shevtsova <aniashevtsova@gmail.com>
2023-07-18 14:14:37 +0200

    all tests pass

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const actionType = actions[i].type;
+
+    if (actionType === 'addProperties') {
+      Object.assign(state, actions[i].extraData);
+    } else if (actionType === 'removeProperties') {
+      const keysToRemove = actions[i].keysToRemove;
+
+      for (const key of keysToRemove) {
+        if (state.hasOwnProperty(key)) {
+          delete state[key];
+        }
+      }
+    } else if (actionType === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
