3fb60a8e991e1bdb8c41550e9e0443afc9bbc850
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2023-07-20 09:50:36 +0300

    create transformState

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,32 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
-}
+const actionTypes = {
+  addProperties: 'addProperties',
+  removeProperties: 'removeProperties',
+  clear: 'clear',
+};
 
+function transformState(state, actions) {
+  for (const action of actions) {
+    switch (action.type) {
+      case actionTypes.addProperties:
+        Object.assign(state, action.extraData);
+        break;
+
+      case actionTypes.removeProperties:
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case actionTypes.clear:
+        for (const key in state) {
+          delete state[key];
+        };
+        break;
+    };
+  };
+
+  return state;
+};
 module.exports = transformState;
