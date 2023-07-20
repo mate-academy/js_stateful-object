3fdcc9363660585f3bde05acf517a44808fd8d3c
Anton Kuts <anton.a.kuts@gmail.com>
2023-07-20 21:50:14 +0300

    compleated all tasks and passed all tests

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,27 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  actions.forEach(howTransform => {
+    const { type, extraData, keysToRemove } = howTransform;
+
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      case 'removeProperties':
+        keysToRemove.forEach(propKey => {
+          if (state.hasOwnProperty(propKey)) {
+            delete state[propKey];
+          }
+        });
+        break;
+    }
+  });
 }
 
 module.exports = transformState;
