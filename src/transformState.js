0bbbeb649417de1c6ca93cc69a1e76d5c517d0fc
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-07-23 18:08:28 +0300

    add task Object

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const prop of action.keysToRemove) {
+          if (state[prop]) {
+            delete state[prop];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const prop in state) {
+          delete state[prop];
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
