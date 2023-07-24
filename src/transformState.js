6912b23935b22a35efb08181b7b10ebee7bce328
Vladyslav Korobka <vladseipa@gmail.com>
2023-07-24 18:16:50 +0300

    created object with actions types

--- src/transformState.js
+++ src/transformState.js
@@ -5,23 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  const ActionTypes = {};
+
+  actions.map(action => {
+    ActionTypes[action.type] = action.type;
+  });
+
   for (const action of actions) {
     switch (action.type) {
-      case 'addProperties':
+      case ActionTypes.addProperties:
         for (const data in action.extraData) {
           state[data] = action.extraData[data];
         };
 
         break;
 
-      case 'removeProperties':
+      case ActionTypes.removeProperties:
         for (const prop of action.keysToRemove) {
           delete state[prop];
         };
 
         break;
 
-      case 'clear':
+      case ActionTypes.clear:
         Object.keys(state).forEach(key => delete state[key]);
 
         break;
