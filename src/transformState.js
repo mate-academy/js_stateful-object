22abd3c9cd46fef0fc0a669ff4facba4409569b9
Vasylyna Loda <vasylynatsepak@gmail.com>
2023-07-25 01:35:22 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-param-reassign */
 'use strict';
 
 /**
@@ -6,6 +8,23 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const keys of actions) {
+    switch (keys.type) {
+      case 'addProperties':
+        Object.assign(state, keys.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of keys.keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        state = {};
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
