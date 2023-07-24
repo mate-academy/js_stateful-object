e75928d9e717e519614643dd09e17f5108a98f11
Max Oleksyuk <maksoleksyuk@gmail.com>
2023-07-24 14:44:38 +0300

    cr a func

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,42 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type } = action;
+
+    switch (type) {
+      case 'addProperties':
+        if ('extraData' in action) {
+          const { extraData } = action;
+
+          for (const key in extraData) {
+            state[key] = extraData[key];
+          }
+        };
+        break;
+
+      case 'removeProperties':
+        if ('keysToRemove' in action) {
+          const { keysToRemove } = action;
+
+          for (const key of keysToRemove) {
+            if (state.hasOwnProperty(key)) {
+              delete state[key];
+            }
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
