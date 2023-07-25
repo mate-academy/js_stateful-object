3e7a07e6dd8e0009999dc5014a100487ffc5131f
Andrzej Pietkiewicz <pietan@me.com>
2023-07-25 18:14:40 +0200

    fixed according to comments

--- src/transformState.js
+++ src/transformState.js
@@ -1,25 +1,20 @@
 'use strict';
 
 function transformState(state, actions) {
-  for (let i = 0; i < actions.length; i++) {
-    for (const key in actions[i]) {
-      if (actions[i][key] === 'addProperties') {
-        Object.assign(state, actions[i]['extraData']);
-      };
+  for (const key of actions) {
+    switch (key.type) {
+      case 'addProperties':
+        Object.assign(state, key.extraData);
+        break;
 
-      if (actions[i][key] === 'removeProperties') {
-        for (const key1 in actions[i]['keysToRemove']) {
-          delete state[actions[i]['keysToRemove'][key1]];
-        }
-      }
+      case 'removeProperties':
+        key.keysToRemove.forEach(el => delete state[el]);
+        break;
 
-      if (actions[i][key] === 'clear') {
-        for (const key1 in state) {
-          delete state[key1];
-        };
-      };
-    };
-  };
+      default:
+        Object.keys(state).forEach(el => delete state[el]);
+    }
+  }
 
   return state;
 }
