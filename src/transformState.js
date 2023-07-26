309f8576d7e5e35ea7adc52561346da0aee70f34
Kamil <majcherczak.kamil@protonmail.com>
2023-07-26 12:24:05 +0200

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,11 +1,33 @@
 'use strict';
 
+const ADD_PROPERTIES = 'addProperties';
+const REMOVE_PROPERTIES = 'removeProperties';
+const CLEAR = 'clear';
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const act of actions) {
+    switch (act.type) {
+      case ADD_PROPERTIES:
+        Object.assign(state, act.extraData);
+        break;
+      case REMOVE_PROPERTIES:
+        act.keysToRemove.forEach((element) => {
+          delete state[element];
+        });
+        break;
+      case CLEAR:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        throw new Error('Wrong type');
+    }
+  }
 }
 
 module.exports = transformState;
