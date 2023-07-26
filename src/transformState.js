ccdfc48bd0e058a905ffbf37fad92d08107292f9
Konrad <konrad.domalewski@gmail.com>
2023-07-26 12:09:56 +0200

    adding review changes

--- src/transformState.js
+++ src/transformState.js
@@ -10,20 +10,25 @@ function transformState(state, actions) {
       case 'addProperties':
         Object.assign(state, action.extraData);
         break;
+
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
+
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
         break;
+
       default:
         throw Error('Something went wrong');
     }
   }
+
+  return state;
 }
 
 module.exports = transformState;
