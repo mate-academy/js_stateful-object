270c3b9597e28ed7d4dc94d9f9a2d734f9237e75
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2023-07-26 12:48:41 +0200

    update solution

--- src/transformState.js
+++ src/transformState.js
@@ -1,31 +1,39 @@
 'use strict';
 
+const ACTION_ADD_PROPERTIES = 'addProperties';
+const REMOVE_PROPERTIES = 'removeProperties';
+const CLEAR = 'clear';
+
 /**
  * @param {Object} state
  * @param {Object[]} actions
  */
+
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
-      case 'addProperties':
+      case ACTION_ADD_PROPERTIES:
         Object.assign(state, action.extraData);
         break;
-      case 'removeProperties':
+
+      case REMOVE_PROPERTIES:
         for (const key of action.keysToRemove) {
-          if (state.hasOwnProperty(key)) {
-            delete state[key];
-          }
+          delete state[key];
         }
         break;
-      case 'clear':
+
+      case CLEAR:
         for (const key in state) {
           delete state[key];
         }
         break;
+
       default:
         throw new Error(`Unknown action type: ${action.type}`);
     }
   }
+
+  return state;
 }
 
 module.exports = transformState;
