483a104e4a604002910ff369f0d411461b12bf35
Valerie <valeriebryksina@gmail.com>
2023-07-26 13:38:00 +0200

    Rename variables

--- src/transformState.js
+++ src/transformState.js
@@ -8,8 +8,8 @@ function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
       case 'addProperties':
-        for (const key in action.extraData) {
-          state[key] = action.extraData[key];
+        for (const data in action.extraData) {
+          state[data] = action.extraData[data];
         }
         break;
       case 'removeProperties':
