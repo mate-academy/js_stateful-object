2666236dcab1f6133b7eabfec00b1c7c6ae85e03
Maciej Kujawa <maciekujawa@gmail.com>
2023-07-26 18:23:43 +0200

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,35 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+
+const ACTION_ADD_PROPERTIES = 'addProperties';
+const ACTION_REMOVE_PROPERTIES = 'removeProperties';
+const ACTION_CLEAR = 'clear';
+
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case ACTION_ADD_PROPERTIES:
+        Object.assign(state, action.extraData);
+        break;
+
+      case ACTION_REMOVE_PROPERTIES:
+        action.keysToRemove.forEach((key) => {
+          delete state[key];
+        });
+        break;
+      case ACTION_CLEAR:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        Error('unsupported value');
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
