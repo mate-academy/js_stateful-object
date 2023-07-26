2225dd7a2df9eec6ee378548515678e3e891e188
Andrzej Pec <pimatstudio@gmail.com>
2023-07-26 17:14:52 +0200

    must use object assign

--- src/transformState.js
+++ src/transformState.js
@@ -5,31 +5,26 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  let newState = state;
-
   actions.forEach((action) => {
     switch (action.type) {
       case 'addProperties':
-        newState = {
-          ...state,
-          ...action.extraData,
-        };
+        Object.assign(state, action.extraData);
         break;
 
       case 'removeProperties':
-        action.keysToRemove.forEach((key) => delete newState[key]);
+        action.keysToRemove.forEach((key) => delete state[key]);
         break;
 
       case 'clear':
-        Object.keys(newState).forEach((key) => delete newState[key]);
+        Object.keys(state).forEach((key) => delete state[key]);
         break;
 
       default:
         throw new Error('Unknown action type: ' + action.type);
     }
-
-    return newState;
   });
+
+  return state;
 }
 
 module.exports = transformState;
