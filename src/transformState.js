3dcd6937f2ff3112db4d053800548e7ba45cf9cf
Valerie <valeriebryksina@gmail.com>
2023-07-26 13:33:01 +0200

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,23 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  for (const obj of actions) {
-    if (obj.type === 'addProperties') {
-      for (const key in obj.extraData) {
-        state[key] = obj.extraData[key];
-      }
-    }
-
-    if (obj.type === 'removeProperties') {
-      for (const key of obj.keysToRemove) {
-        delete state[key];
-      }
-    }
-
-    if (obj.type === 'clear') {
-      for (const key of Object.keys(state)) {
-        delete state[key];
-      }
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          state[key] = action.extraData[key];
+        }
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key of Object.keys(state)) {
+          delete state[key];
+        }
+        break;
+      default:
+        throw new Error('Unexpected option');
     }
   }
 
