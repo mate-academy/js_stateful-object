b68e9c776bc60ed954345a9b5d7ebe5c5da37f58
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-07-27 16:07:00 +0200

    adding const

--- src/transformState.js
+++ src/transformState.js
@@ -6,17 +6,21 @@
  */
 function transformState(state, actions) {
   actions.forEach((obj) => {
-    if (obj.type === 'addProperties') {
+    const ADD_PROPERTIES = obj.type === 'addProperties';
+    const REMOVE_PROERTIES = obj.type === 'removeProperties';
+    const CLEAR_ALL = obj.type === 'clear';
+
+    if (ADD_PROPERTIES) {
       Object.assign(state, obj.extraData);
     }
 
-    if (obj.type === 'removeProperties') {
+    if (REMOVE_PROERTIES) {
       for (const i of obj.keysToRemove) {
         delete state[i];
       }
     }
 
-    if (obj.type === 'clear') {
+    if (CLEAR_ALL) {
       for (const i in state) {
         delete state[i];
       }
