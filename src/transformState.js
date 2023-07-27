418e19aa732c1428ae8362f73ce9ba01d7bc58e9
Wave <www.dandelion.ru@gmail.com>
2023-07-27 21:45:36 +0300

    fix

--- src/transformState.js
+++ src/transformState.js
@@ -7,30 +7,32 @@
 function transformState(state, actions) {
   for (const action of actions) {
     switch (action.type) {
-      case "addProperties": {
+      case 'addProperties': {
         Object.assign(state, action.extraData);
         break;
       }
 
-      case "removeProperties": {
+      case 'removeProperties': {
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
       }
 
-      case "clear": {
-        for (const key in state) {
+      case 'clear': {
+        for (const key of Object.keys(state)) {
           delete state[key];
         }
         break;
       }
 
       default: {
-        throw new Error("Incorrect action type");
+        throw new Error('Incorrect action type');
       }
     }
   }
+
+  return state;
 }
 
 module.exports = transformState;
