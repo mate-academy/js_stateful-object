3c46116a1d5b998287423a5e610ee04d29d2f25b
Vova Zinchenko <zincenkomain17@gmail.com>
2023-07-27 23:40:10 +0300

    task transfrom

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,44 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete state[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+
+      default:
+        break;
+    };
+  };
+
+  return state;
 }
+
+const a = {
+  foo: 'bar',
+  bar: 'foo',
+};
+
+const b = [
+  {
+    type: 'addProperties',
+    extraData: {
+      name: 'Jim', hello: 'world',
+    },
+  },
+];
+
+transformState(a, b);
 
 module.exports = transformState;
