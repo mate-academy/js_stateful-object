a89b97916f898ae4b7bbdff9c0d8b169236fac78
Klaudia <kklis@interia.pl>
2023-07-28 12:28:49 +0300

    Add empty line after each break statement

--- src/transformState.js
+++ src/transformState.js
@@ -10,16 +10,19 @@ function transformState(state, actions) {
       case 'addProperties':
         Object.assign(state, action.extraData);
         break;
+
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
+
       case 'clear':
         for (const key in state) {
           delete state[key];
         }
         break;
+
       default:
         return 'Error';
     }
