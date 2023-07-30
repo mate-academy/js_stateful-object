8d5536056d2c6703c9dec0c6ba908c97a164902a
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-07-30 23:42:52 +0300

    initial commit

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,29 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (let i = 0; i < actions.length; i++) {
+    const actionData = actions[i];
+
+    if (actionData.type === 'addProperties') {
+      for (const key in actionData.extraData) {
+        state[key] = actionData.extraData[key];
+      }
+    }
+
+    if (actionData.type === 'removeProperties') {
+      for (const key of actionData.keysToRemove) {
+        delete state[key];
+      }
+    }
+
+    if (actionData.type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
