10b310c9d4d9190255eaa0ccc1119e1a08df7ed0
MateuszSlezal <M4teks93@gmail.com>
2023-07-31 00:45:27 +0200

    changes

--- src/transformState.js
+++ src/transformState.js
@@ -4,33 +4,35 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
+const ADD_PROPERTIES = 'addProperties';
+const REMOVE_PROPERTIES = 'removeProperties';
+const CLEAR_ALL = 'clear';
+
 function transformState(state, actions) {
   const stateHistory = [];
 
   for (const order of actions) {
-    const ADD_PROPERTIES = order.type === 'addProperties';
-    const REMOVE_PROPERTIES = order.type === 'removeProperties';
-    const CLEAR_ALL = order.type === 'clear';
+    switch (order.type) {
+      case ADD_PROPERTIES:
+        Object.assign(state, order.extraData);
+        stateHistory.push(Object.assign({}, state));
+        break;
 
-    if (ADD_PROPERTIES) {
-      Object.assign(state, order.extraData);
-      stateHistory.push(Object.assign({}, state));
-    }
+      case REMOVE_PROPERTIES:
+        for (const remove of order.keysToRemove) {
+          delete state[remove];
+        }
 
-    if (REMOVE_PROPERTIES) {
-      for (const remove of order.keysToRemove) {
-        delete state[remove];
-      }
+        stateHistory.push(Object.assign({}, state));
+        break;
 
-      stateHistory.push(Object.assign({}, state));
-    }
+      case CLEAR_ALL:
+        for (const key in state) {
+          delete state[key];
+        }
 
-    if (CLEAR_ALL) {
-      for (const key in state) {
-        delete state[key];
-      }
-
-      stateHistory.push(Object.assign({}, state));
+        stateHistory.push(Object.assign({}, state));
+        break;
     }
   }
 
