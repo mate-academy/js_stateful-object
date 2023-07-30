fba04595f16e765fbc0eb54f11a79b3406e2038f
KatOlista^^! <olya.katyushina@gmail.com>
2023-07-31 00:00:22 +0200

    add solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,19 +6,34 @@
  */
 function transformState(state, actions) {
   for (let i = 0; i < actions.length; i++) {
-    if (actions[i].type === 'addProperties') {
-      Object.assign(state, actions[i].extraData);
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(state, actions[i].extraData);
+        break;
+      case 'removeProperties':
+        for (const action of actions[i].keysToRemove) {
+          delete state[action];
+        };
+        break;
+      case 'clear':
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+      default:
+        return state;
     }
+    // if (actions[i].type === 'addProperties') {
+    //   Object.assign(state, actions[i].extraData);
+    // }
 
-    if (actions[i].type === 'removeProperties') {
-      for (const item of actions[i].keysToRemove) {
-        delete state[item];
-      }
-    }
+    // if (actions[i].type === 'removeProperties') {
+    //   for (const action of actions[i].keysToRemove) {
+    //     delete state[action];
+    //   }
+    // }
 
-    if (actions[i].type === 'clear') {
-      Object.keys(state).forEach(key => delete state[key]);
-    }
+    // if (actions[i].type === 'clear') {
+    //   Object.keys(state).forEach(key => delete state[key]);
+    // }
   }
 }
 
