3009c675471a6483e5dfcabc84814ec5c21fc4ff
Anton Tsurkan <wesses1999@gmail.com>
2023-07-31 19:33:45 +0300

    add fix according to the checklist

--- src/transformState.js
+++ src/transformState.js
@@ -10,20 +10,26 @@ function transformState(state, actions) {
   const TYPE_CLEAR = 'clear';
 
   for (const action of actions) {
-    if (action.type === TYPE_ADD_PROPERTIES) {
-      Object.assign(state, action.extraData);
-    }
-
-    if (action.type === TYPE_REMOVE_PROPERTIES) {
-      for (const key of action.keysToRemove) {
-        delete state[key];
+    switch (action.type) {
+      case TYPE_ADD_PROPERTIES: {
+        Object.assign(state, action.extraData);
+        break;
       }
-    }
 
-    if (action.type === TYPE_CLEAR) {
-      for (const key in state) {
-        delete state[key];
+      case TYPE_REMOVE_PROPERTIES: {
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
       }
+
+      case TYPE_CLEAR: {
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      }
+      default: return 'undefined type';
     }
   }
 }
