ebe5d33acec9fe8ab48793f8ba2bd984a0081208
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-07-31 11:37:01 +0300

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,27 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const indexAction in actions) {
+    switch (actions[indexAction].type) {
+      case 'addProperties':
+        for (const key in actions[indexAction].extraData) {
+          state[key] = actions[indexAction].extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of actions[indexAction].keysToRemove) {
+          delete state[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+    }
+  }
 }
 
 module.exports = transformState;
