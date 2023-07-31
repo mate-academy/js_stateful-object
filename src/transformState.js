823a6bc8c33e1ebda670b26934fb1be00fa67615
AndrMar1939 <advo737@gmail.com>
2023-07-31 14:44:51 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,34 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const finiteStateMachine = ({ type, extraData, keysToRemove }) => {
+    switch (type) {
+      case 'addProperties':
+        Object.assign(state, extraData);
+        break;
+
+      case 'removeProperties':
+        keysToRemove.forEach(keyToRemove => {
+          delete state[keyToRemove];
+        });
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  actions.forEach(action => {
+    finiteStateMachine(action);
+  });
+
+  return state;
 }
 
 module.exports = transformState;
