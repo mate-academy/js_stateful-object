112c0084a62f7500998bec1ae4eda3ee2e9c2093
Bahtijar <no4kar@gmail.com>
2023-08-01 17:40:03 +0200

    task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,37 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const removeKeyFromStateObj = (key) => {
+    delete state[key];
+  };
+
+  for (const action of actions) {
+    if (Object.hasOwnProperty.call(action, 'type')) {
+      switch (action.type) {
+        case 'addProperties':
+          Object.keys(action.extraData).forEach(
+            (key) => {
+              state[key] = action.extraData[key];
+            });
+          continue;
+
+        case 'removeProperties':
+          action.keysToRemove.forEach(removeKeyFromStateObj);
+          continue;
+
+        case 'clear':
+          Object.keys(state).forEach(removeKeyFromStateObj);
+          continue;
+
+        default:
+          throw new Error(
+            'unknown "type" of "action" from "@param {Object[]} actions"');
+      }
+    }
+
+    throw new Error(
+      '"type" is not key of "action" from "@param {Object[]} actions"');
+  }
 }
 
 module.exports = transformState;
