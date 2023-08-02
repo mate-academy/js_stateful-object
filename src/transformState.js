1fe9a3ad08b9af9d4c00134c04a829eac97ba1e6
Dmytro Hrynchyi <desevoker@gmail.com>
2023-08-02 13:38:35 +0300

    Solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,41 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  const ACTION_ADD = 'addProperties';
+  const ACTION_REMOVE = 'removeProperties';
+  const ACTION_CLEAR = 'clear';
+  const ERROR_MSG = 'Unknown action!';
+
+  for (const action of actions) {
+    switch (action.type) {
+      case ACTION_ADD:
+        Object.assign(state, action.extraData);
+        break;
+
+      case ACTION_REMOVE:
+        deleteProperties(state, action.keysToRemove);
+        break;
+
+      case ACTION_CLEAR:
+        deleteProperties(state, Object.keys(state));
+        break;
+
+      default:
+        throw new Error(ERROR_MSG);
+    }
+  }
+}
+
+/**
+ * @param {Object} object
+ * @param {String[]} properties
+ */
+function deleteProperties(object, properties) {
+  for (const name of properties) {
+    if (object.hasOwnProperty(name)) {
+      delete object[name];
+    }
+  }
 }
 
 module.exports = transformState;
