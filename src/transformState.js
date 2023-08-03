4be48761d6323be6731b36c4285ca54a4cb0042e
Andrii Korobkin <flouzeek@gmail.com>
2023-08-03 16:21:22 +0300

    add task solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,32 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key of Object.keys(state)) {
+          if (state.hasOwnProperty(key)) {
+            delete state[key];
+          }
+        }
+        break;
+
+      default:
+        return new Error('Oops, something went wrong!');
+    }
+  }
 }
 
 module.exports = transformState;
