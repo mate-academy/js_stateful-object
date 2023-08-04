8d7dfc7be7f7e6b3fb13235d5bbcb8e0c61a7a39
Stoianova Ivanna <ctoyanova13@gmail.com>
2023-08-04 14:19:29 +0300

    Solution 2

--- src/transformState.js
+++ src/transformState.js
@@ -16,16 +16,19 @@ function transformState(state, actions) {
       case ACTIONS.ADD_PROPERTIES:
         Object.assign(state, action.extraData);
         break;
+
       case ACTIONS.REMOVE_PROPERTIES:
         for (const key of action.keysToRemove) {
           delete state[key];
         }
         break;
+
       case ACTIONS.CLEAR:
         for (const key in state) {
           delete state[key];
         }
         break;
+        
       default:
         throw new Error('Error handing');
     }
