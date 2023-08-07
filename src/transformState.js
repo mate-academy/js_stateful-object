561c80cc742ad40d33a70d680ab00480c5981de2
Nikita <cool.nikita734@gmail.com>
2023-08-07 16:39:15 +0200

    solution

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,36 @@
  */
 function transformState(state, actions) {
   // write code here
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case 'addProperties':
+        for (const key in extraData) {
+          state[key] = extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        if (keysToRemove) {
+          for (const key of keysToRemove) {
+            if (state.hasOwnProperty(key)) {
+              delete state[key];
+            }
+          }
+        }
+        break;
+
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${type}`);
+    }
+  }
 }
 
 module.exports = transformState;
