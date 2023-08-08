d7c453ce967d7968fa4ae430ce3e2d9990ddb29e
Artem <gavr.artem.s@gmail.com>
2023-08-08 13:00:59 +0300

    add my ugly solution. i think so, maybe not))

--- src/transformState.js
+++ src/transformState.js
@@ -4,8 +4,36 @@
  * @param {Object} state
  * @param {Object[]} actions
  */
-function transformState(state, actions) {
-  // write code here
+function transformState(state, action) {
+  for (const obj of action) {
+    // to iterate all objects in array.
+    for (const [key, value] of Object.entries(obj)) {
+      // to iterate every object and split on keys and values.
+      //
+      // conditions check
+      if (obj[key] === obj.type && value === 'clear') {
+        for (const ke of Object.keys(state)) {
+          delete state[ke];
+        }
+      }
+
+      if (obj[key] === obj.extraData) {
+        // add key and value to the object
+        for (const [k, v] of Object.entries(obj[key])) {
+          state[k] = v;
+        }
+      }
+
+      if (obj[key] === obj.keysToRemove) {
+        // delete properies
+        for (const k of obj[key]) {
+          delete state[k];
+        }
+      }
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
