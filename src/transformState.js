73ff2fcd90dd6361f403c62a349c7d34ad236160
Oleksandr Voloshyn <voloshyn244@gmail.com>
2023-08-11 00:03:35 +0200

    jsStatefullObject V1

--- src/transformState.js
+++ src/transformState.js
@@ -6,6 +6,31 @@
  */
 function transformState(state, actions) {
   // write code here
+  const _addProperties = 'addProperties';
+  const _removeProperties = 'removeProperties';
+  const _clear = 'clear';
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    switch (type) {
+      case _addProperties:
+        Object.assign(state, extraData);
+        break;
+      case _removeProperties:
+        for (const removeKey of keysToRemove) {
+          delete state[removeKey];
+        }
+        break;
+      case _clear:
+        Object.keys(state).forEach(key => delete state[key]);
+        break;
+      default:
+        return 'Type error!';
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
