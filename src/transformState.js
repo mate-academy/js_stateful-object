d698c8d616ac825ce77bc2d89746db86f6227359
Ruslan Hrynchuk <r.momos@gmail.com>
2023-08-10 13:54:07 +0300

    Stateful object. Tests passed.

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,35 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    switch (action.type) {
+      case ('addProperties'):
+        Object.assign(state, action.extraData);
+
+        break;
+
+      case ('removeProperties'):
+        for (const keyToRemove of action.keysToRemove) {
+          if (state.hasOwnProperty(keyToRemove)) {
+            delete state[keyToRemove];
+          }
+        }
+
+        break;
+
+      case ('clear'):
+        for (const stateKey in state) {
+          delete state[stateKey];
+        }
+
+        break;
+
+      default:
+        return 'ERROR';
+    }
+  }
+
+  return state;
 }
 
 module.exports = transformState;
