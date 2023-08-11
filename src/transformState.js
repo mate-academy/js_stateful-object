a067a3985f5434ead2440cbe759609af9fb041e5
Oleksandr Voloshyn <voloshyn244@gmail.com>
2023-08-11 17:25:34 +0200

    jsStatefulObject v2

--- src/transformState.js
+++ src/transformState.js
@@ -10,23 +10,24 @@ function transformState(state, actions) {
   const _removeProperties = 'removeProperties';
   const _clear = 'clear';
 
-  for (const action of actions) {
-    const { type, extraData, keysToRemove } = action;
-
+  for (const { type, extraData, keysToRemove } of actions) {
     switch (type) {
       case _addProperties:
         Object.assign(state, extraData);
         break;
+
       case _removeProperties:
         for (const removeKey of keysToRemove) {
           delete state[removeKey];
         }
         break;
+
       case _clear:
         Object.keys(state).forEach(key => delete state[key]);
         break;
+
       default:
-        return 'Type error!';
+        throw new Error(`Unknown action type: ${type}`);
     }
   }
 
