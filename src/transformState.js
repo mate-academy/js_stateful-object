a0db87b4d63f84f7322d0f30028fc7ebdfb01ae6
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2023-08-16 22:40:04 +0300

    first add

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,31 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  let STATE = { ...state };
+
+  for (let i = 0; i < actions.length; i++) {
+    const el = actions[i];
+    const EXTRA_DATA_CONTENT = el.extraData;
+    const KEYS_TO_REMOVE_CONTENT = el.keysToRemove;
+
+    if (el.type === 'clear') {
+      STATE = {};
+    }
+
+    if (el.type === 'addProperties') {
+      Object.assign(STATE, EXTRA_DATA_CONTENT);
+    }
+
+    if (el.type === 'removeProperties') {
+      for (let a = 0; i < KEYS_TO_REMOVE_CONTENT.length; i++) {
+        const elKr = KEYS_TO_REMOVE_CONTENT[a];
+
+        delete STATE[elKr];
+      }
+    }
+  }
+
+  return STATE;
 }
 
 module.exports = transformState;
