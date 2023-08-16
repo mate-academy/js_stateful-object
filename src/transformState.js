dbd1f4ff0cba4bba48991dcaab4f5d2dc2be1193
Mykhailo <eglet777@ukr.net>
2023-08-16 21:55:40 +0300

    removed variables

--- src/transformState.js
+++ src/transformState.js
@@ -5,17 +5,13 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  const ADD_PROPERTIES = 'addProperties';
-  const REMOVE_PROPERTIES = 'removeProperties';
-  const CLEAR_PROPERTIES = 'clear';
-
   for (const action of actions) {
     switch (action.type) {
-      case ADD_PROPERTIES:
+      case 'addProperties':
         Object.assign(state, action.extraData);
         break;
 
-      case REMOVE_PROPERTIES:
+      case 'removeProperties':
         for (const key of action.keysToRemove) {
           if (state.hasOwnProperty(key)) {
             delete state[key];
@@ -23,7 +19,7 @@ function transformState(state, actions) {
         }
         break;
 
-      case CLEAR_PROPERTIES:
+      case 'clear':
         for (const key in state) {
           delete state[key];
         }
