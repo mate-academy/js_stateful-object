085f705ac982c541a5de336b2b16f2ea6c688b28
Evhen Teneta <tenetaevgen@gmail.com>
2023-08-28 17:36:40 +0300

    replaced for on switch

--- src/transformState.js
+++ src/transformState.js
@@ -5,19 +5,30 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
+  const addPropertyExtraData = 'addProperties';
+  const removePropertyKeys = 'removeProperties';
+  const clearState = 'clear';
+
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        state[key] = action.extraData[key];
-      }
-    } else if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-    } else if (action.type === 'clear') {
-      for (const key in state) {
-        delete state[key];
-      }
+    switch (action.type) {
+      case addPropertyExtraData:
+        for (const key in action.extraData) {
+          state[key] = action.extraData[key];
+        }
+        break;
+      case removePropertyKeys:
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case clearState:
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+
+      default:
+        break;
     }
   }
 
