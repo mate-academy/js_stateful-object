c29a213ebc2b628699c368835fa3627650b874ad
Yaroslav220890 <126674531+Yaroslav220890@users.noreply.github.com>
2023-09-12 11:24:15 +0300

    transformState

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  for (const action of actions) {
+    const { type } = action;
+
+    if (type === 'addProperties') {
+      const { extraData } = action;
+
+      Object.assign(state, extraData);
+    } else if (type === 'removeProperties') {
+      const { keysToRemove } = action;
+
+      for (const key of keysToRemove) {
+        delete state[key];
+      }
+    } else if (type === 'clear') {
+      for (const key in state) {
+        delete state[key];
+      }
+    }
+  }
 }
 
 module.exports = transformState;
