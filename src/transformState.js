058c958694f229afef827c4513fdc907a7e14b02
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-10 16:40:18 +0100

    feat: provides with solution

--- src/transformState.js
+++ src/transformState.js
@@ -5,7 +5,34 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  // write code here
+  if (Array.isArray(actions)) {
+    for (const action of actions) {
+      actionsObserver(state, action);
+    }
+
+    return;
+  }
+  actionsObserver(state, actions);
 }
 
 module.exports = transformState;
+
+const actionsObserver = (state, action) => {
+  switch (action.type) {
+    case 'addProperties':
+      Object.assign(state, action.extraData);
+      break;
+    case 'removeProperties':
+      for (const key of action.keysToRemove) {
+        delete state[key];
+      }
+      break;
+    case 'clear':
+      for (const key in state) {
+        delete state[key];
+      }
+      break;
+    default:
+      break;
+  }
+};
