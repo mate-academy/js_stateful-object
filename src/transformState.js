861f800cda9f1378fca326261279434a5262e82a
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-10 17:09:07 +0100

    fix: my horrible logics mistake, somehow thought we could also receive a single object as `action` not onlu an array

--- src/transformState.js
+++ src/transformState.js
@@ -5,34 +5,25 @@
  * @param {Object[]} actions
  */
 function transformState(state, actions) {
-  if (Array.isArray(actions)) {
-    for (const action of actions) {
-      actionsObserver(state, action);
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(state, action.extraData);
+        break;
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete state[key];
+        }
+        break;
+      case 'clear':
+        for (const key in state) {
+          delete state[key];
+        }
+        break;
+      default:
+        break;
     }
-
-    return;
   }
-  actionsObserver(state, actions);
 }
 
 module.exports = transformState;
-
-const actionsObserver = (state, action) => {
-  switch (action.type) {
-    case 'addProperties':
-      Object.assign(state, action.extraData);
-      break;
-    case 'removeProperties':
-      for (const key of action.keysToRemove) {
-        delete state[key];
-      }
-      break;
-    case 'clear':
-      for (const key in state) {
-        delete state[key];
-      }
-      break;
-    default:
-      break;
-  }
-};
